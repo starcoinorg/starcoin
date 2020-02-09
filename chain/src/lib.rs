43297814988ac6a3444292556da8c59596450d6a
jolestar <jolestar@gmail.com>
2020-02-10 05:45:34 +0800

    [scaffold] Add chain module.

---
+++ chain/src/lib.rs
@@ -0,0 +1,14 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+mod chain;
+
+pub struct ChainActor {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
