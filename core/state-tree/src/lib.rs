f532aacadadebae796b3d011fc8f6d6229401854
jolestar <jolestar@gmail.com>
2020-02-14 07:40:14 +0800

    [accumulator] Define state tree.

---
+++ core/state-tree/src/lib.rs
@@ -0,0 +1,45 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{Error, Result};
+use crypto::{hash::CryptoHash, HashValue};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+#[derive(Default, Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct StateProof {}
+
+#[derive(Default, Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct StateNode {}
+
+pub trait StateStore {
+    fn get_node(&self, hash: HashValue) -> Result<StateNode>;
+    fn save_node(&self, node: StateNode) -> Result<()>;
+}
+
+pub struct SparseMerkleTree {
+    store: Arc<dyn StateStore>,
+    root_hash: HashValue,
+}
+
+impl SparseMerkleTree {
+    pub fn new(store: Arc<dyn StateStore>, root_hash: HashValue) -> Self {
+        Self { store, root_hash }
+    }
+
+    pub fn update(hash: HashValue, value: Vec<u8>) -> Result<Self> {
+        unimplemented!()
+    }
+
+    pub fn root_hash(&self) -> HashValue {
+        self.root_hash
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_state_tree() {}
+}
