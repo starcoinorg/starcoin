b0a4651902806dd36a060e0434c1e4c035d18bf8
jolestar <jolestar@gmail.com>
2020-02-20 05:48:53 +0800

    [traits] Add Chain traits.

---
+++ core/traits/src/chain.rs
@@ -0,0 +1,16 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use crypto::HashValue;
+use types::block::{Block, BlockHeader, BlockNumber, BlockTemplate};
+
+#[async_trait::async_trait]
+pub trait Chain: Clone + std::marker::Unpin {
+    async fn current_header(self) -> BlockHeader;
+    async fn get_header(self, hash: &HashValue) -> BlockHeader;
+    async fn get_header_by_number(self, number: BlockNumber) -> BlockHeader;
+    async fn create_block_template(self) -> Result<BlockTemplate>;
+    async fn get_block_by_hash(self, hash: &HashValue) -> Result<Option<Block>>;
+    async fn try_connect(self, block: Block) -> Result<()>;
+}
--- core/traits/src/lib.rs
+++ core/traits/src/lib.rs
@@ -1,9 +1,11 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+mod chain;
 pub mod mock;
 mod txpool;
 
+pub use chain::Chain;
 pub use txpool::TxPool;
 
 #[cfg(test)]
