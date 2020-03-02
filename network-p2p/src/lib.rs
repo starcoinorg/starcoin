563226e235094b0f4d3b234d7fbd1258948ada09
fanngyuan <fanngyuan@gmail.com>
2020-03-02 20:30:46 +0800

    update lipp2p

---
+++ network-p2p/src/lib.rs
@@ -0,0 +1,14 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+#[macro_use]
+extern crate log;
+
+mod config;
+mod debug_info;
+mod discovery;
+mod error;
+mod network_state;
+mod protocol;
+mod transport;
+mod utils;
