ca8a5df38985a7966f83f41e906f352ebc100b46
nonsense <nonsense0202@pm.me>
2020-03-12 09:51:45 +0800

    [config-generator]: update to generate config in dir
    Signed-off-by: nonsense <nonsense0202@pm.me>

--- config/config-generator/src/main.rs
+++ config/config-generator/src/main.rs
@@ -1,15 +1,15 @@
-use starcoin_config::{save_config, NodeConfig};
+use starcoin_config::NodeConfig;
 
 use clap::Clap;
 #[derive(Clap)]
 #[clap(version = "1.0", author = "starcoin")]
 struct Opts {
-    #[clap(short = "o", long = "output", default_value = "config.template.toml")]
+    #[clap(short = "o", long = "output_dir", default_value = "starcoin")]
     output_file: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    let config_template = NodeConfig::default();
-    save_config(&config_template, opts.output_file).expect("write file should ok");
+    NodeConfig::load(&opts.output_file).expect("write file should ok");
+    println!("generated config.toml in dir {}", opts.output_file);
 }
--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,10 +1,10 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use std::convert::TryFrom;
-use anyhow::Result;
 use anyhow::ensure;
-use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use anyhow::Result;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::convert::TryFrom;
 mod miner_config;
 mod network_config;
 mod rpc_config;
@@ -19,42 +19,31 @@ pub use vm_config::VMConfig;
 
 use crypto::ed25519::{Ed25519PrivateKey, Ed25519PublicKey};
 use crypto::{test_utils::KeyPair, Uniform};
+use dirs;
+use once_cell::sync::Lazy;
 use rand::prelude::*;
-use std::fs::File;
 use std::fs::create_dir;
+use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use dirs;
-use once_cell::sync::Lazy;
 
 /// Default data dir
-pub static DEFAULT_DATA_DIR: Lazy<PathBuf> =
-    Lazy::new(|| dirs::home_dir().expect("read home dir should ok").join(".starcoin"));
+pub static DEFAULT_DATA_DIR: Lazy<PathBuf> = Lazy::new(|| {
+    dirs::home_dir()
+        .expect("read home dir should ok")
+        .join(".starcoin")
+});
 pub static CONFIG_FILE_PATH: &str = "config.toml";
 
-pub fn load_config_from_dir<P>(data_dir: P) -> Result<NodeConfig>  where P: AsRef<Path> {
-    if !data_dir.as_ref().exists() {
-        create_dir(data_dir.as_ref())?;
-    }
-    ensure!(data_dir.as_ref().is_dir(), "pelase pass in a dir as data_dir");
-
-    let base_dir = PathBuf::from(data_dir.as_ref());
-    let config_file_path = base_dir.join(CONFIG_FILE_PATH);
-
-    let mut node_config: NodeConfig = if config_file_path.exists() {
-        load_config(&config_file_path)?
-    } else {
-        let default_config = NodeConfig::default();
-        save_config(&default_config, &config_file_path)?;
-        default_config
-    };
-    node_config.network.load(&base_dir)?;
-    // NOTICE: if there is more load case, make it here.
-    // such as: node_config.storage.load(&base_dir)?;
-    Ok(node_config)
+pub fn load_config_from_dir<P>(data_dir: P) -> Result<NodeConfig>
+where
+    P: AsRef<Path>,
+{
+    NodeConfig::load(data_dir)
 }
+
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct NodeConfig {
@@ -77,42 +66,42 @@ impl NodeConfig {
         config
     }
 
-    pub fn load<P: AsRef<Path>>(input_path: P) -> Result<Self> {
-        let mut file = File::open(&input_path)?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        Self::deserialize(&contents)
+    pub fn load<P: AsRef<Path>>(data_dir: P) -> Result<Self> {
+        if !data_dir.as_ref().exists() {
+            create_dir(data_dir.as_ref())?;
+        }
+        ensure!(
+            data_dir.as_ref().is_dir(),
+            "pelase pass in a dir as data_dir"
+        );
+
+        let base_dir = PathBuf::from(data_dir.as_ref());
+        let config_file_path = base_dir.join(CONFIG_FILE_PATH);
+
+        let mut node_config: NodeConfig = if config_file_path.exists() {
+            load_config(&config_file_path)?
+        } else {
+            let default_config = NodeConfig::default();
+            save_config(&default_config, &config_file_path)?;
+            default_config
+        };
+        node_config.network.load(&base_dir)?;
+        // NOTICE: if there is more load case, make it here.
+        // such as: node_config.storage.load(&base_dir)?;
+        Ok(node_config)
     }
 
-    pub fn deserialize(serialized: &str) -> Result<Self> {
-        Ok(toml::from_str(&serialized)?)
-    }
-
-    pub fn serialize(&self) -> Result<String> {
-        let config_str = toml::to_string(self)?;
-        return Ok(config_str);
-    }
-
-    pub fn load_or_default(config_path: Option<&Path>) -> Self {
+    pub fn load_or_random(config_path: Option<&Path>) -> Self {
         // Load the config
         let node_config = match config_path {
-            Some(path) => {
-                println!("Loading node config from: {}", path.display());
-                NodeConfig::load(path).expect("Failed to load node config.")
-            }
-            None => {
-                println!("Loading test configs");
-                NodeConfig::default()
-            }
+            Some(path) => NodeConfig::load(path).expect("Failed to load node config."),
+            None => NodeConfig::random_for_test(),
         };
-
-        println!("Using node config {:?}", &node_config);
-
         node_config
     }
 }
 
-pub fn save_config<T, P>(c: &T, output_file: P) -> Result<()>
+pub(crate) fn save_config<T, P>(c: &T, output_file: P) -> Result<()>
 where
     T: Serialize + DeserializeOwned,
     P: AsRef<Path>,
@@ -122,31 +111,42 @@ where
     file.write_all(&contents)?;
     Ok(())
 }
-pub fn load_config<T, P>(path: P) -> Result<T>   where T: Serialize + DeserializeOwned, P: AsRef<Path> {
+pub(crate) fn load_config<T, P>(path: P) -> Result<T>
+where
+    T: Serialize + DeserializeOwned,
+    P: AsRef<Path>,
+{
     let mut file = File::open(&path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     parse(&contents)
 }
 
-fn parse<T>(serialized: &str) -> Result<T> where T: Serialize + DeserializeOwned {
+fn parse<T>(serialized: &str) -> Result<T>
+where
+    T: Serialize + DeserializeOwned,
+{
     Ok(toml::from_str(&serialized)?)
 }
 
-pub fn save_key<P>(key: &[u8], output_file: P) -> Result<()>  where P: AsRef<Path> {
+pub(crate) fn save_key<P>(key: &[u8], output_file: P) -> Result<()>
+where
+    P: AsRef<Path>,
+{
     let contents: String = hex::encode(key);
     let mut file = File::create(output_file)?;
     file.write_all(contents.as_bytes())?;
     Ok(())
 }
 
-pub fn load_key<P: AsRef<Path>>(path: P) -> Result<KeyPair<Ed25519PrivateKey, Ed25519PublicKey>> {
+pub(crate) fn load_key<P: AsRef<Path>>(
+    path: P,
+) -> Result<KeyPair<Ed25519PrivateKey, Ed25519PublicKey>> {
     let content = std::fs::read_to_string(path)?;
     let bytes_out: Vec<u8> = hex::decode(&content)?;
     let pri_key = Ed25519PrivateKey::try_from(bytes_out.as_slice())?;
     Ok(KeyPair::from(pri_key))
 }
-
 
 pub fn gen_keypair() -> Arc<KeyPair<Ed25519PrivateKey, Ed25519PublicKey>> {
     let mut seed_rng = rand::rngs::OsRng::new().expect("can't access OsRng");
@@ -183,18 +183,4 @@ fn get_ephemeral_port() -> ::std::io::Result<u16> {
     let _incoming = listener.accept()?;
 
     Ok(addr.port())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_default_config_serialize() {
-        let config = NodeConfig::default();
-        let config_str = config.serialize().expect("config serialize must success.");
-        let config2 =
-            NodeConfig::deserialize(config_str.as_str()).expect("config deserialize must success.");
-        assert_eq!(config, config2);
-    }
 }
