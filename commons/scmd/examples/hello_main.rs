9a77d6bad2d0173cf4084710c4446a717355a7a1
jolestar <jolestar@gmail.com>
2020-03-25 00:37:07 +0800

    [cmd] Scmd framework support state and opt parse.

--- commons/scmd/examples/hello_main.rs
+++ commons/scmd/examples/hello_main.rs
@@ -2,23 +2,121 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
-use scmd::CmdContext;
+use scmd::{CmdContext, Command, CommandAction, ExecContext};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
+#[structopt(name = "hello_app")]
+struct GlobalOpts {
+    #[structopt(short = "t")]
+    test: bool,
+}
+
+#[derive(Debug, StructOpt)]
 #[structopt(name = "alpha")]
 struct AlphaOpts {
-    #[structopt(short = "n")]
+    #[structopt(short = "n", default_value = "alpha_default")]
     name: String,
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "alpha_sub1")]
+struct AlphaSub1Opts {
+    #[structopt(short = "n", default_value = "alpha_sub1_default")]
+    name: String,
+}
+
+struct Counter(AtomicUsize);
+
+impl Counter {
+    pub fn new() -> Self {
+        Self(AtomicUsize::default())
+    }
+
+    pub fn incr(&self) -> usize {
+        self.0.fetch_add(1, Ordering::Relaxed)
+    }
+
+    pub fn get(&self) -> usize {
+        self.0.load(Ordering::Relaxed)
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "beta")]
+struct BetaOpts {
+    #[structopt(short = "n", default_value = "beta_default")]
+    name: String,
+}
+
+struct BetaCommand {}
+
+impl CommandAction for BetaCommand {
+    type State = Counter;
+    type GlobalOpt = GlobalOpts;
+    type Opt = BetaOpts;
+
+    fn run(&self, ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()> {
+        println!(
+            "BetaCommand hello global_opts:{:?} {:?} state:{:?}",
+            ctx.global_opt(),
+            ctx.opt(),
+            ctx.state().get()
+        );
+        Ok(())
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "beta_sub1")]
+struct BetaSub1Opts {
+    #[structopt(short = "n", default_value = "beta_default")]
+    name: String,
+}
+
+struct BetaSub1Command {}
+
+impl CommandAction for BetaSub1Command {
+    type State = Counter;
+    type GlobalOpt = GlobalOpts;
+    type Opt = BetaSub1Opts;
+
+    fn run(&self, ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()> {
+        ctx.state().incr();
+        println!(
+            "BetaSub1Command hello global_opts:{:?} {:?} state:{:?}",
+            ctx.global_opt(),
+            ctx.opt(),
+            ctx.state().get()
+        );
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
-    let context = CmdContext::new("hello_app");
+    let state = Counter::new();
+
+    let context = CmdContext::new(state);
     context
-        .with_command::<AlphaOpts>(Box::new(|args| -> Result<()> {
-            println!("hello alpha {:?}", args.name);
-            Ok(())
-        }))
-        .execute()?;
+        .add_command(
+            Command::with_name("alpha").subcommand(Command::with_action_fn(Box::new(
+                |ctx: &ExecContext<Counter, GlobalOpts, AlphaSub1Opts>| -> Result<()> {
+                    println!(
+                        "hello global_opts:{:?} {:?} state:{:?}",
+                        ctx.global_opt(),
+                        ctx.opt(),
+                        ctx.state().get()
+                    );
+                    Ok(())
+                },
+            ))),
+        )
+        .add_command(
+            BetaCommand {}
+                .into_cmd()
+                .subcommand(BetaSub1Command {}.into_cmd()),
+        )
+        .exec()?;
     Ok(())
 }
--- commons/scmd/src/lib.rs
+++ commons/scmd/src/lib.rs
@@ -5,69 +5,259 @@ use anyhow::Result;
 use clap::{App, ArgMatches};
 use serde::export::PhantomData;
 use std::collections::HashMap;
+use std::sync::Arc;
 use structopt::StructOpt;
 
-pub trait CommandRunnable {
-    fn run(&self, arg_matches: &ArgMatches<'_>) -> Result<()>;
+#[derive(Debug, Clone, Default, StructOpt)]
+pub struct EmptyOpt {}
+
+pub trait CommandAction {
+    type State;
+    type GlobalOpt: StructOpt;
+    type Opt: StructOpt;
+
+    fn run(&self, ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()>;
+
+    fn into_cmd(self) -> Command<Self::State, Self::GlobalOpt, Self::Opt, Self>
+    where
+        Self: std::marker::Sized,
+    {
+        self.into()
+    }
 }
 
-pub struct CmdContext<'a, 'b> {
-    app: App<'a, 'b>,
-    commands: HashMap<String, Box<dyn CommandRunnable>>,
+pub struct FnCommandAction<State, GlobalOpt, Opt>
+where
+    State: 'static,
+    GlobalOpt: StructOpt + 'static,
+    Opt: StructOpt + 'static,
+{
+    action: Box<dyn Fn(&ExecContext<State, GlobalOpt, Opt>) -> Result<()>>,
 }
 
-impl<'a, 'b> CmdContext<'a, 'b> {
-    pub fn new(app_name: &str) -> Self {
+impl<State, GlobalOpt, Opt> FnCommandAction<State, GlobalOpt, Opt>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+{
+    pub fn new(action: Box<dyn Fn(&ExecContext<State, GlobalOpt, Opt>) -> Result<()>>) -> Self {
+        Self { action }
+    }
+}
+
+impl<State, GlobalOpt, Opt> CommandAction for FnCommandAction<State, GlobalOpt, Opt>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+{
+    type State = State;
+    type GlobalOpt = GlobalOpt;
+    type Opt = Opt;
+
+    fn run(&self, ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()> {
+        self.action.as_ref()(ctx)
+    }
+}
+
+pub struct NoneAction<State, GlobalOpt>
+where
+    State: 'static,
+    GlobalOpt: StructOpt + 'static,
+{
+    state_type: PhantomData<State>,
+    global_opt_type: PhantomData<GlobalOpt>,
+}
+
+impl<State, GlobalOpt> CommandAction for NoneAction<State, GlobalOpt>
+where
+    GlobalOpt: StructOpt,
+{
+    type State = State;
+    type GlobalOpt = GlobalOpt;
+    type Opt = EmptyOpt;
+
+    fn run(&self, _ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()> {
+        Ok(())
+    }
+}
+
+pub(crate) trait CommandExec<State, GlobalOpt>
+where
+    GlobalOpt: StructOpt + 'static,
+    State: 'static,
+{
+    fn exec(
+        &mut self,
+        state: Arc<State>,
+        global_opt: Arc<GlobalOpt>,
+        arg_matches: &ArgMatches<'_>,
+    ) -> Result<()>;
+}
+
+pub struct ExecContext<State, GlobalOpt, Opt>
+where
+    State: 'static,
+    GlobalOpt: StructOpt + 'static,
+    Opt: StructOpt + 'static,
+{
+    state: Arc<State>,
+    global_opt: Arc<GlobalOpt>,
+    opt: Arc<Opt>,
+}
+
+impl<State, GlobalOpt, Opt> ExecContext<State, GlobalOpt, Opt>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+{
+    pub fn new(state: Arc<State>, global_opt: Arc<GlobalOpt>, opt: Arc<Opt>) -> Self {
         Self {
-            app: App::new(app_name),
+            state,
+            global_opt,
+            opt,
+        }
+    }
+    pub fn global_opt(&self) -> &GlobalOpt {
+        self.global_opt.as_ref()
+    }
+
+    pub fn state(&self) -> &State {
+        self.state.as_ref()
+    }
+
+    pub fn opt(&self) -> &Opt {
+        self.opt.as_ref()
+    }
+}
+
+pub struct CmdContext<State, GlobalOpt>
+where
+    State: 'static,
+    GlobalOpt: StructOpt + 'static,
+{
+    app: App<'static, 'static>,
+    commands: HashMap<String, Box<dyn CommandExec<State, GlobalOpt>>>,
+    state: Arc<State>,
+}
+
+impl<State, GlobalOpt> CmdContext<State, GlobalOpt>
+where
+    GlobalOpt: StructOpt,
+{
+    pub fn new(state: State) -> Self {
+        Self {
+            app: GlobalOpt::clap(),
             commands: HashMap::new(),
+            state: Arc::new(state),
         }
     }
 
-    pub fn with_command<Arg>(mut self, action: Box<dyn Fn(Arg) -> Result<()>>) -> Self
+    pub fn add_command<Opt, Action>(
+        mut self,
+        command: Command<State, GlobalOpt, Opt, Action>,
+    ) -> Self
     where
-        Arg: StructOpt + 'static,
+        Opt: StructOpt + 'static,
+        Action: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = Opt> + 'static,
     {
-        let command = Command::new(action);
         let name = command.name().to_string();
         self.app = self.app.subcommand(command.app().clone());
         self.commands.insert(name, Box::new(command));
         self
     }
 
-    pub fn execute(self) -> Result<()> {
-        let matches = self.app.get_matches();
+    pub fn print_help(&mut self) -> Result<()> {
+        self.app.print_help()?;
+        Ok(())
+    }
 
+    pub fn exec(mut self) -> Result<()> {
+        let matches = self.app.clone().get_matches();
+        let global_opt = Arc::new(GlobalOpt::from_clap(&matches));
         let (cmd, arg_matches) = matches.subcommand();
-        let cmd = self.commands.get(cmd);
+        let cmd = self.commands.get_mut(cmd);
         match (cmd, arg_matches) {
             (Some(cmd), Some(arg_matches)) => {
-                cmd.run(arg_matches)?;
+                cmd.exec(self.state.clone(), global_opt, arg_matches)?;
             }
-            _ => {}
+            _ => {
+                self.print_help()?;
+            }
         };
         Ok(())
     }
 }
 
-pub struct Command<'a, 'b, Arg>
+pub struct Command<State, GlobalOpt, Opt, Action>
 where
-    Arg: StructOpt,
+    GlobalOpt: StructOpt + 'static,
+    State: 'static,
+    Opt: StructOpt + 'static,
+    Action: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = Opt> + 'static,
 {
-    pub app: App<'a, 'b>,
-    pub action: Box<dyn Fn(Arg) -> Result<()>>,
-    pub arg: PhantomData<Arg>,
+    app: App<'static, 'static>,
+    action: Option<Action>,
+    subcommands: HashMap<String, Box<dyn CommandExec<State, GlobalOpt>>>,
+    global_opt: PhantomData<GlobalOpt>,
+    opt_type: PhantomData<Opt>,
 }
 
-impl<'a, 'b, Arg> Command<'a, 'b, Arg>
+impl<State, GlobalOpt> Command<State, GlobalOpt, EmptyOpt, NoneAction<State, GlobalOpt>>
 where
-    Arg: StructOpt,
+    GlobalOpt: StructOpt,
 {
-    pub fn new(action: Box<dyn Fn(Arg) -> Result<()>>) -> Self {
+    pub fn with_name(name: &str) -> Self {
         Self {
-            app: Arg::clap(),
-            action,
-            arg: PhantomData,
+            app: App::new(name),
+            action: None,
+            subcommands: HashMap::new(),
+            global_opt: PhantomData,
+            opt_type: PhantomData,
+        }
+    }
+}
+
+impl<State, GlobalOpt, Opt> Command<State, GlobalOpt, Opt, FnCommandAction<State, GlobalOpt, Opt>>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+{
+    pub fn with_action_fn(
+        action: Box<dyn Fn(&ExecContext<State, GlobalOpt, Opt>) -> Result<()>>,
+    ) -> Self {
+        Self {
+            app: Opt::clap(),
+            action: Some(FnCommandAction::new(action)),
+            subcommands: HashMap::new(),
+            global_opt: PhantomData,
+            opt_type: PhantomData,
+        }
+    }
+}
+
+impl<State, GlobalOpt, Opt, Action> Command<State, GlobalOpt, Opt, Action>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+    Action: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = Opt>,
+{
+    pub fn new() -> Self {
+        Self {
+            app: Opt::clap(),
+            action: None,
+            subcommands: HashMap::new(),
+            global_opt: PhantomData,
+            opt_type: PhantomData,
+        }
+    }
+
+    pub fn with_action(action: Action) -> Self {
+        Self {
+            app: Opt::clap(),
+            action: Some(action),
+            subcommands: HashMap::new(),
+            global_opt: PhantomData,
+            opt_type: PhantomData,
         }
     }
 
@@ -75,18 +265,81 @@ where
         self.app.get_name()
     }
 
-    pub fn app(&self) -> &App<'a, 'b> {
+    pub fn app(&self) -> &App<'static, 'static> {
         &self.app
+    }
+
+    pub fn subcommand<SubOpt, SubAction>(
+        mut self,
+        subcommand: Command<State, GlobalOpt, SubOpt, SubAction>,
+    ) -> Self
+    where
+        SubOpt: StructOpt + 'static,
+        SubAction: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = SubOpt>,
+    {
+        self.app = self.app.subcommand(subcommand.app().clone());
+        self.subcommands
+            .insert(subcommand.name().to_string(), Box::new(subcommand));
+        self
+    }
+
+    pub fn has_subcommand(&self) -> bool {
+        !self.subcommands.is_empty()
+    }
+
+    fn exec_action(&mut self, ctx: &ExecContext<State, GlobalOpt, Opt>) -> Result<()> {
+        match &self.action {
+            Some(action) => {
+                action.run(ctx)?;
+            }
+            None => {
+                self.app.print_help()?;
+            }
+        }
+        Ok(())
     }
 }
 
-impl<'a, 'b, Arg> CommandRunnable for Command<'a, 'b, Arg>
+impl<State, GlobalOpt, Opt, Action> CommandExec<State, GlobalOpt>
+    for Command<State, GlobalOpt, Opt, Action>
 where
-    Arg: StructOpt,
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+    Action: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = Opt>,
 {
-    fn run(&self, arg_matches: &ArgMatches<'_>) -> Result<()> {
-        let arg = Arg::from_clap(arg_matches);
-        self.action.as_ref()(arg)
-        //Ok(())
+    fn exec(
+        &mut self,
+        state: Arc<State>,
+        global_opt: Arc<GlobalOpt>,
+        arg_matches: &ArgMatches<'_>,
+    ) -> Result<()> {
+        let opt = Arc::new(Opt::from_clap(arg_matches));
+        let ctx = ExecContext::new(state, global_opt, opt);
+        if self.has_subcommand() {
+            let (subcmd_name, subcmd_matches) = arg_matches.subcommand();
+            let subcmd = self.subcommands.get_mut(subcmd_name);
+            match (subcmd, subcmd_matches) {
+                (Some(subcmd), Some(subcmd_matches)) => {
+                    subcmd.exec(ctx.state, ctx.global_opt, subcmd_matches)?;
+                }
+                _ => {
+                    self.exec_action(&ctx)?;
+                }
+            }
+        } else {
+            self.exec_action(&ctx)?;
+        }
+        Ok(())
+    }
+}
+
+impl<C, State, GlobalOpt, Opt> From<C> for Command<C::State, C::GlobalOpt, C::Opt, C>
+where
+    GlobalOpt: StructOpt,
+    Opt: StructOpt,
+    C: CommandAction<State = State, GlobalOpt = GlobalOpt, Opt = Opt> + 'static,
+{
+    fn from(action: C) -> Self {
+        Command::with_action(action)
     }
 }
