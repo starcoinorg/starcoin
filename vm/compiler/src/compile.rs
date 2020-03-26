3531e729de28615079f7adac7e848620e912476a
Guangyu Zhu <guangyu.zhu@aliyun.com>
2020-03-26 12:22:22 +0800

    [vm] update libra compiler version

--- vm/compiler/src/compile.rs
+++ vm/compiler/src/compile.rs
@@ -5,9 +5,10 @@ pub struct StarcoinCompiler {}
 impl StarcoinCompiler {
     /// Compile script with account address 0x0
     pub fn compile_script(code: &str) -> Vec<u8> {
-        let compiler = Compiler {
-            ..Compiler::default()
-        };
-        compiler.into_script_blob(code).unwrap()
+        unimplemented!()
+//        let compiler = Compiler {
+//            ..Compiler::default()
+//        };
+//        compiler.into_script_blob(code).unwrap()
     }
 }
