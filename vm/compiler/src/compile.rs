174544cb731df1cd1ed34e8c2aadd9c6f5dc74ce
Guangyu Zhu <guangyu.zhu@aliyun.com>
2020-03-26 12:26:08 +0800

    [vm] cargo fmt

--- vm/compiler/src/compile.rs
+++ vm/compiler/src/compile.rs
@@ -6,9 +6,9 @@ impl StarcoinCompiler {
     /// Compile script with account address 0x0
     pub fn compile_script(code: &str) -> Vec<u8> {
         unimplemented!()
-//        let compiler = Compiler {
-//            ..Compiler::default()
-//        };
-//        compiler.into_script_blob(code).unwrap()
+        //        let compiler = Compiler {
+        //            ..Compiler::default()
+        //        };
+        //        compiler.into_script_blob(code).unwrap()
     }
 }
