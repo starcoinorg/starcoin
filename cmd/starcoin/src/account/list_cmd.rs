b0d70c46f79fd7200eac151a6f1d85d9468f98da
jolestar <jolestar@gmail.com>
2020-03-27 15:34:47 +0800

    [cmd] Implements account command.

--- cmd/starcoin/src/account/list_cmd.rs
+++ cmd/starcoin/src/account/list_cmd.rs
@@ -8,22 +8,22 @@ use scmd::{CommandAction, ExecContext};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "new")]
-pub struct NewOpt {
-    #[structopt(short = "p")]
-    password: String,
-}
+#[structopt(name = "list")]
+pub struct ListOpt {}
 
-pub struct NewCommand {}
+pub struct ListCommand {}
 
-impl CommandAction for NewCommand {
+impl CommandAction for ListCommand {
     type State = CliState;
     type GlobalOpt = StarcoinOpt;
-    type Opt = NewOpt;
+    type Opt = ListOpt;
 
     fn run(&self, ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>) -> Result<()> {
         let client = ctx.state().client();
-        println!("account new command, node: status: {:?}", client.status()?);
+        let accounts = client.account_list()?;
+        for account in accounts {
+            println!("{} {}", account.address, account.is_default);
+        }
         Ok(())
     }
 }
