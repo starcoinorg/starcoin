849de7235648921881c591938ff915e6aff14867
jolestar <jolestar@gmail.com>
2020-03-29 05:37:07 +0800

    [test] fix mock txn gas.

--- miner/src/mock_txn_generator.rs
+++ miner/src/mock_txn_generator.rs
@@ -68,7 +68,7 @@ impl MockTxnGenerator {
                     .sequence_number();
             // add some randomness too.
             let mut rng = rand::thread_rng();
-            let amount_to_transfer = rng.gen_range(5000u64, 10000u64);
+            let amount_to_transfer = rng.gen_range(500000000u64, 1000000000u64);
 
             // transfer will create account if it not exists.
             let txn = <E as TransactionExecutor>::build_mint_txn(
@@ -104,7 +104,7 @@ impl MockTxnGenerator {
                                  a_seq_number: u64| {
             // add some randomness.
             let mut rng = rand::thread_rng();
-            let amount_to_transfer = rng.gen_range(10000000u64, 50000000u64);
+            let amount_to_transfer = rng.gen_range(1000u64, 5000u64);
             let transfer_txn = <E as TransactionExecutor>::build_transfer_txn(
                 a.0,
                 AuthenticationKey::from_public_key(&a.1.public_key).to_vec(),
