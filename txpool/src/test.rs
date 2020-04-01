434704b070301b347d0bf4badd18ccc337f1fbf7
lerencao <funfriendcjf@gmail.com>
2020-04-01 16:08:50 +0800

    [txpool]: fix test, use real storage and executor
    Signed-off-by: lerencao <funfriendcjf@gmail.com>

--- txpool/src/test.rs
+++ txpool/src/test.rs
@@ -1,53 +1,36 @@
-use crate::{pool::AccountSeqNumberClient, TxPoolRef};
-use common_crypto::hash::{CryptoHash, HashValue};
-use forkable_jellyfish_merkle::node_type::Node;
-use parking_lot::RwLock;
+use crate::TxPoolRef;
+use anyhow::Result;
+use common_crypto::ed25519;
+use common_crypto::hash::CryptoHash;
 use starcoin_bus::BusActor;
-use starcoin_state_tree::StateNodeStore;
-use std::{collections::HashMap, sync::Arc};
+use starcoin_config::{NodeConfig, TxPoolConfig};
+use starcoin_consensus::argon_consensus::ArgonConsensus;
+use starcoin_executor::executor::Executor;
+use starcoin_executor::TransactionExecutor;
+use starcoin_genesis::Genesis;
+use std::sync::Arc;
 use storage::cache_storage::CacheStorage;
 use storage::db_storage::DBStorage;
 use storage::StarcoinStorage;
 use traits::TxPoolAsyncService;
-use types::{
-    account_address::AccountAddress, block::BlockHeader, transaction::SignedUserTransaction,
-};
-
-#[derive(Clone, Debug)]
-struct MockNonceClient {
-    cache: Arc<RwLock<HashMap<AccountAddress, u64>>>,
-}
-
-impl Default for MockNonceClient {
-    fn default() -> Self {
-        Self {
-            cache: Arc::new(RwLock::new(HashMap::new())),
-        }
-    }
-}
-
-impl AccountSeqNumberClient for MockNonceClient {
-    fn account_seq_number(&self, address: &AccountAddress) -> u64 {
-        let cached = self.cache.read().get(address).cloned();
-        match cached {
-            Some(v) => v,
-            None => {
-                self.cache.write().insert(address.clone(), 0);
-                0
-            }
-        }
-    }
-}
+use types::account_address::AccountAddress;
 
 #[actix_rt::test]
-async fn test_tx_pool() {
+async fn test_tx_pool() -> Result<()> {
     let pool = gen_pool_for_test();
-    let txn = SignedUserTransaction::mock();
+    let (_private_key, public_key) = ed25519::compat::generate_keypair(None);
+    let account_address = AccountAddress::from_public_key(&public_key);
+    let auth_prefix = AccountAddress::authentication_key(&public_key)
+        .prefix()
+        .to_vec();
+    let txn = Executor::build_mint_txn(account_address, auth_prefix, 1, 10000);
+    let txn = txn.as_signed_user_txn()?.clone();
     let txn_hash = txn.crypto_hash();
-    let mut result = pool.clone().add_txns(vec![txn]).await.unwrap();
+    let mut result = pool.clone().add_txns(vec![txn]).await?;
     assert!(result.pop().unwrap().is_ok());
-    let mut pending_txns = pool.clone().get_pending_txns(Some(10)).await.unwrap();
+    let mut pending_txns = pool.clone().get_pending_txns(Some(10)).await?;
     assert_eq!(pending_txns.pop().unwrap().crypto_hash(), txn_hash);
+    Ok(())
 }
 
 #[actix_rt::test]
@@ -57,33 +40,63 @@ async fn test_subscribe_txns() {
 }
 
 #[actix_rt::test]
-async fn test_rollback() {
+async fn test_rollback() -> Result<()> {
     let pool = gen_pool_for_test();
-    let txn = SignedUserTransaction::mock();
-    let _txn_hash = txn.crypto_hash();
-    let _result = pool.clone().add_txns(vec![txn.clone()]).await.unwrap();
-    let new_txn = SignedUserTransaction::mock();
+    let txn = {
+        let (_private_key, public_key) = ed25519::compat::generate_keypair(None);
+        let account_address = AccountAddress::from_public_key(&public_key);
+        let auth_prefix = AccountAddress::authentication_key(&public_key)
+            .prefix()
+            .to_vec();
+        let txn = Executor::build_mint_txn(account_address, auth_prefix, 1, 10000);
+        let txn = txn.as_signed_user_txn()?.clone();
+        txn
+    };
+    let _ = pool.clone().add_txns(vec![txn.clone()]).await?;
+    let new_txn = {
+        let (_private_key, public_key) = ed25519::compat::generate_keypair(None);
+        let account_address = AccountAddress::from_public_key(&public_key);
+        let auth_prefix = AccountAddress::authentication_key(&public_key)
+            .prefix()
+            .to_vec();
+        let txn = Executor::build_mint_txn(account_address, auth_prefix, 1, 20000);
+        let txn = txn.as_signed_user_txn()?.clone();
+        txn
+    };
     pool.clone()
         .rollback(vec![txn], vec![new_txn.clone()])
-        .await
-        .unwrap();
-    let txns = pool.clone().get_pending_txns(Some(100)).await.unwrap();
+        .await?;
+    let txns = pool.clone().get_pending_txns(Some(100)).await?;
     assert_eq!(txns.len(), 1);
     let pending = txns.into_iter().next().unwrap();
     assert_eq!(
         CryptoHash::crypto_hash(&pending),
         CryptoHash::crypto_hash(&new_txn)
     );
+    Ok(())
 }
 
 fn gen_pool_for_test() -> TxPoolRef {
     let cache_storage = Arc::new(CacheStorage::new());
-    let tmpdir = libra_temppath::TempPath::new();
+    let tmpdir = tempfile::tempdir().unwrap();
     let db_storage = Arc::new(DBStorage::new(tmpdir.path()));
     let storage = Arc::new(StarcoinStorage::new(cache_storage, db_storage).unwrap());
-    let _ = storage.put(HashValue::zero(), Node::new_null().into());
-    let header = BlockHeader::genesis_block_header(HashValue::random(), HashValue::zero(), vec![]);
+    let node_config = NodeConfig::random_for_test();
+
+    let genesis = Genesis::new::<Executor, ArgonConsensus, StarcoinStorage>(
+        Arc::new(node_config),
+        storage.clone(),
+    )
+    .expect("init gensis fail");
+    let startup_info = genesis.startup_info().clone();
+
     let bus = BusActor::launch();
-    let pool = TxPoolRef::start_with_best_block_header(storage, header, bus);
+    let pool = TxPoolRef::start(
+        TxPoolConfig::default(),
+        storage.clone(),
+        startup_info.head.get_head(),
+        bus,
+    );
+
     pool
 }
