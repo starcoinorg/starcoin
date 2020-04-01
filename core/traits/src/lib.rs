cd2b3de69a754ab9b9bf93e5a946cbdada40ed14
lerencao <funfriendcjf@gmail.com>
2020-04-01 13:05:23 +0800

    [txpool]: first step- extract core trait to api
    Signed-off-by: lerencao <funfriendcjf@gmail.com>

--- core/traits/src/lib.rs
+++ core/traits/src/lib.rs
@@ -4,8 +4,7 @@
 mod chain;
 mod chain_service;
 pub mod mock;
-mod txpool_service;
 pub use chain::{Chain, ChainReader, ChainWriter};
 pub use chain_service::{ChainAsyncService, ChainService};
 
-pub use txpool_service::TxPoolAsyncService;
+pub use starcoin_txpool_api::TxPoolAsyncService;
