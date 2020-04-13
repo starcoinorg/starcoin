387da6b36eff4ea03d2918b14d17cf995b2edb05
lerencao <funfriendcjf@gmail.com>
2020-04-13 16:05:19 +0800

    [rpc]: try to reconnect rpc connection when some kind of errors happen
    Signed-off-by: lerencao <funfriendcjf@gmail.com>

--- rpc/client/src/lib.rs
+++ rpc/client/src/lib.rs
@@ -19,8 +19,9 @@ use starcoin_types::transaction::{RawUserTransaction, SignedUserTransaction};
 use starcoin_wallet_api::WalletAccount;
 use std::cell::RefCell;
 use std::ops::Deref;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::time::Duration;
+use thiserror::Error;
 use tokio01::reactor::Reactor;
 use tokio_compat::prelude::*;
 use tokio_compat::runtime::Runtime;
@@ -29,22 +30,57 @@ mod remote_state_reader;
 
 pub use crate::remote_state_reader::RemoteStateReader;
 
+#[derive(Debug, Clone)]
+enum ConnSource {
+    Ipc(PathBuf),
+    Http,
+    Local,
+}
+
 pub struct RpcClient {
-    inner: RpcClientInner,
+    inner: RefCell<Option<RpcClientInner>>,
     rt: RefCell<Runtime>,
+    conn_source: ConnSource,
+}
+
+struct ConnectionProvider {
+    conn_source: ConnSource,
+}
+
+#[derive(Error, Debug)]
+pub enum ConnError {
+    #[error("io error, {0}")]
+    Io(#[from] std::io::Error),
+    #[error("rpc error, {0}")]
+    RpcError(jsonrpc_client_transports::RpcError),
+}
+
+impl ConnectionProvider {
+    async fn get_rpc_channel(&self) -> anyhow::Result<RpcChannel, ConnError> {
+        let reactor = Reactor::new().unwrap();
+        match &self.conn_source {
+            ConnSource::Ipc(sock_path) => {
+                let conn_fut = ipc::connect(sock_path, &reactor.handle())?;
+                conn_fut.compat().await.map_err(|e| ConnError::RpcError(e))
+            }
+            // only have ipc impl for now
+            _ => unreachable!(),
+        }
+    }
 }
 
 impl RpcClient {
-    pub(crate) fn new(inner: RpcClientInner, rt: Runtime) -> Self {
+    pub(crate) fn new(conn_source: ConnSource, inner: RpcClientInner, rt: Runtime) -> Self {
         Self {
-            inner,
+            inner: RefCell::new(Some(inner)),
             rt: RefCell::new(rt),
+            conn_source,
         }
     }
     pub fn connect_http(url: &str) -> anyhow::Result<Self> {
         let mut rt = Runtime::new().unwrap();
         let client_inner = rt.block_on(http::connect(url).map_err(map_err))?;
-        Ok(Self::new(client_inner, rt))
+        Ok(Self::new(ConnSource::Http, client_inner, rt))
     }
 
     pub fn connect_local<THandler, TMetadata>(handler: THandler) -> Self
@@ -72,87 +108,57 @@ impl RpcClient {
                 };
             }
         });
-        Self::new(client, rt)
+        Self::new(ConnSource::Local, client, rt)
     }
 
     pub fn connect_ipc<P: AsRef<Path>>(sock_path: P) -> anyhow::Result<Self> {
         let mut rt = Runtime::new().unwrap();
         let reactor = Reactor::new().unwrap();
-
+        let path = sock_path.as_ref().to_path_buf();
         let fut = ipc::connect(sock_path, &reactor.handle())?;
         let client_inner = rt.block_on(fut.map_err(map_err))?;
 
-        Ok(Self::new(client_inner, rt))
+        Ok(Self::new(ConnSource::Ipc(path), client_inner, rt))
     }
 
     pub fn node_status(&self) -> anyhow::Result<bool> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .node_client
-                .status()
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(|inner| async move { inner.node_client.status().compat().await })
+            .map_err(map_err)
     }
 
     pub fn submit_transaction(&self, txn: SignedUserTransaction) -> anyhow::Result<bool> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .txpool_client
-                .submit_transaction(txn)
-                .map_err(map_err)
-                .compat()
-                .await
+        self.block_call_rpc(|inner| async move {
+            inner.txpool_client.submit_transaction(txn).compat().await
         })
+        .map_err(map_err)
     }
     //TODO should split client for different api ?
     // such as  RpcClient().account().create()
     pub fn account_create(&self, password: String) -> anyhow::Result<WalletAccount> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .account_client
-                .create(password)
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(
+            |inner| async move { inner.account_client.create(password).compat().await },
+        )
+        .map_err(map_err)
     }
 
     pub fn account_list(&self) -> anyhow::Result<Vec<WalletAccount>> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .account_client
-                .list()
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(|inner| async move { inner.account_client.list().compat().await })
+            .map_err(map_err)
     }
 
     pub fn account_get(&self, address: AccountAddress) -> anyhow::Result<Option<WalletAccount>> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .account_client
-                .get(address)
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(|inner| async move { inner.account_client.get(address).compat().await })
+            .map_err(map_err)
     }
 
     pub fn account_sign_txn(
         &self,
         raw_txn: RawUserTransaction,
     ) -> anyhow::Result<SignedUserTransaction> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .account_client
-                .sign_txn(raw_txn)
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(
+            |inner| async move { inner.account_client.sign_txn(raw_txn).compat().await },
+        )
+        .map_err(map_err)
     }
 
     pub fn account_unlock(
@@ -161,28 +167,28 @@ impl RpcClient {
         password: String,
         duration: std::time::Duration,
     ) -> anyhow::Result<()> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
+        self.block_call_rpc(|inner| async move {
+            inner
                 .account_client
                 .unlock(address, password, duration)
-                .map_err(map_err)
                 .compat()
                 .await
         })
+        .map_err(map_err)
     }
     pub fn account_export(
         &self,
         address: AccountAddress,
         password: String,
     ) -> anyhow::Result<Vec<u8>> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
+        self.block_call_rpc(|inner| async move {
+            inner
                 .account_client
                 .export(address, password)
-                .map_err(map_err)
                 .compat()
                 .await
         })
+        .map_err(map_err)
     }
     pub fn account_import(
         &self,
@@ -190,70 +196,105 @@ impl RpcClient {
         private_key: Vec<u8>,
         password: String,
     ) -> anyhow::Result<WalletAccount> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
+        self.block_call_rpc(|inner| async move {
+            inner
                 .account_client
                 .import(address, private_key, password)
-                .map_err(map_err)
                 .compat()
                 .await
         })
+        .map_err(map_err)
     }
 
     pub fn state_get(&self, access_path: AccessPath) -> anyhow::Result<Option<Vec<u8>>> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .state_client
-                .get(access_path)
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(
+            |inner| async move { inner.state_client.get(access_path).compat().await },
+        )
+        .map_err(map_err)
     }
 
     pub fn state_get_with_proof(&self, access_path: AccessPath) -> anyhow::Result<StateWithProof> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
+        self.block_call_rpc(|inner| async move {
+            inner
                 .state_client
                 .get_with_proof(access_path)
-                .map_err(map_err)
                 .compat()
                 .await
         })
+        .map_err(map_err)
     }
 
     pub fn state_get_state_root(&self) -> anyhow::Result<HashValue> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .state_client
-                .get_state_root()
-                .map_err(map_err)
-                .compat()
-                .await
-        })
+        self.block_call_rpc(
+            |inner| async move { inner.state_client.get_state_root().compat().await },
+        )
+        .map_err(map_err)
     }
 
     pub fn state_get_account_state(
         &self,
         address: AccountAddress,
     ) -> anyhow::Result<Option<AccountState>> {
-        self.rt.borrow_mut().block_on_std(async {
-            self.inner
-                .state_client
-                .get_account_state(address)
-                .map_err(map_err)
-                .compat()
-                .await
+        self.block_call_rpc(|inner| async move {
+            inner.state_client.get_account_state(address).compat().await
         })
+        .map_err(map_err)
+    }
+
+    fn block_call_rpc<F, T>(
+        &self,
+        f: impl FnOnce(RpcClientInner) -> F,
+    ) -> Result<T, jsonrpc_client_transports::RpcError>
+    where
+        F: std::future::Future<Output = Result<T, jsonrpc_client_transports::RpcError>>,
+    {
+        let inner_opt = self.inner.borrow().as_ref().cloned();
+        let inner = match inner_opt {
+            Some(inner) => inner,
+            None => {
+                let new_inner: RpcClientInner = self.rt.borrow_mut().block_on_std(async {
+                    Self::get_rpc_channel(self.conn_source.clone())
+                        .await
+                        .map(|c| c.into())
+                })?;
+                *self.inner.borrow_mut() = Some(new_inner.clone());
+                new_inner
+            }
+        };
+
+        let result = self.rt.borrow_mut().block_on_std(async { f(inner).await });
+
+        if let Err(rpc_error) = &result {
+            if let jsonrpc_client_transports::RpcError::Other(e) = rpc_error {
+                error!("rpc error due to {:?}", e);
+                *self.inner.borrow_mut() = None;
+            }
+        }
+
+        result
+    }
+
+    async fn get_rpc_channel(
+        conn_source: ConnSource,
+    ) -> anyhow::Result<RpcChannel, jsonrpc_client_transports::RpcError> {
+        let conn_provider = ConnectionProvider { conn_source };
+        match conn_provider.get_rpc_channel().await {
+            Ok(channel) => Ok(channel),
+            Err(ConnError::RpcError(e)) => Err(e),
+            Err(ConnError::Io(e)) => Err(jsonrpc_client_transports::RpcError::Other(
+                failure::Error::from(e),
+            )),
+        }
     }
 }
 
-impl AsRef<RpcClientInner> for RpcClient {
-    fn as_ref(&self) -> &RpcClientInner {
-        &self.inner
-    }
-}
+// impl AsRef<RpcClientInner> for RpcClient {
+//     fn as_ref(&self) -> &RpcClientInner {
+//         &self.inner
+//     }
+// }
 
+#[derive(Clone)]
 pub(crate) struct RpcClientInner {
     node_client: NodeClient,
     txpool_client: TxPoolClient,
