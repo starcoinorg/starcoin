4a64c6b6bbc9425431e177784770272557e52cb9
fikgol <hit.cs.lijun@gmail.com>
2020-04-16 17:15:33 +0800

    Update tests with miner

--- miner/src/miner_client/test.rs
+++ miner/src/miner_client/test.rs
@@ -1,7 +1,6 @@
 mod test {
     use crate::miner::{MineCtx, Miner};
-    use crate::miner_client::miner::{Miner as MinerClient, MinerClientActor};
-    use crate::miner_client::stratum::StratumClient;
+    use crate::miner_client::miner::MinerClientActor;
     use crate::stratum::StratumManager;
     use actix::Actor;
     use actix_rt::System;
@@ -9,7 +8,6 @@ mod test {
     use config::MinerConfig;
     use config::NodeConfig;
     use consensus::argon::ArgonConsensusHeader;
-    use futures::stream::StreamExt;
     use futures_timer::Delay;
     use logger::prelude::*;
     use sc_stratum::{PushWorkHandler, Stratum};
@@ -24,7 +22,8 @@ mod test {
         let mut system = System::new("test");
         system.block_on(async {
             let mut miner_config = MinerConfig::default();
-            miner_config.consensus_strategy = config::ConsensusStrategy::Argon;
+            miner_config.consensus_strategy = config::ConsensusStrategy::Dummy;
+            miner_config.thread_num = 4;
             let conf = Arc::new(NodeConfig::random_for_test());
             let mut miner = Miner::<ArgonConsensusHeader>::new(BusActor::launch(), conf);
             let stratum = {
@@ -37,17 +36,16 @@ mod test {
                 let header = BlockHeader::default();
                 let body = BlockBody::default();
                 let block = Block::new(header, body);
-                let mut block_template = BlockTemplate::from_block(block);
-                block_template.difficult = U256::max_value() / 1000.into();
-                MineCtx::new(block_template)
+                let block_template = BlockTemplate::from_block(block);
+                let difficult: U256 = 2000.into();
+                MineCtx::new(block_template, difficult)
             };
-            let addr = MinerClientActor::new(miner_config).start();
+            let _addr = MinerClientActor::new(miner_config).start();
             miner.set_mint_job(mine_ctx);
             for _ in 1..10 {
-                Delay::new(Duration::from_millis(3000)).await;
                 stratum.push_work_all(miner.get_mint_job()).unwrap();
+                Delay::new(Duration::from_millis(2000)).await;
             }
-            Delay::new(Duration::from_millis(3000)).await;
         });
     }
 }
