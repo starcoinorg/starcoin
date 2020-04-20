b7146744a5b18fb5a473ac00c83804e8521a5aec
jolestar <jolestar@gmail.com>
2020-04-20 23:34:19 +0800

    [cmd] panic command

---
+++ cmd/starcoin/src/debug/panic_cmd.rs
@@ -0,0 +1,44 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::cli_state::CliState;
+use crate::StarcoinOpt;
+use anyhow::{bail, Result};
+use scmd::{CommandAction, ExecContext};
+use structopt::StructOpt;
+
+///Manual trigger panic, only work for dev network.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "panic")]
+pub struct PanicOpt {
+    /// if set remote, panic at Backend, otherwise panic at cli.
+    #[structopt(short = "r")]
+    remote: bool,
+}
+
+pub struct PanicCommand;
+
+impl CommandAction for PanicCommand {
+    type State = CliState;
+    type GlobalOpt = StarcoinOpt;
+    type Opt = PanicOpt;
+    type ReturnItem = String;
+
+    fn run(
+        &self,
+        ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>,
+    ) -> Result<Self::ReturnItem> {
+        let opt = ctx.opt();
+        let client = ctx.state().client();
+        let config = ctx.state().config();
+        if !config.net().is_dev() {
+            bail!("This command only work for dev network");
+        }
+        if opt.remote {
+            client.debug_panic()?;
+        } else {
+            panic!("Debug command panic.");
+        }
+        Ok("Remote node is panic.".to_string())
+    }
+}
