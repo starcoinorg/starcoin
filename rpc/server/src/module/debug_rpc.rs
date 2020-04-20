a705d178e89d64fcfdf4d8d85e803566ee13f2fc
jolestar <jolestar@gmail.com>
2020-04-20 23:16:54 +0800

    [rpc] add a panic rpc api.

--- rpc/server/src/module/debug_rpc.rs
+++ rpc/server/src/module/debug_rpc.rs
@@ -3,6 +3,7 @@
 
 use crate::module::to_invalid_param_err;
 use jsonrpc_core::Result;
+use starcoin_config::NodeConfig;
 use starcoin_logger::prelude::LevelFilter;
 use starcoin_logger::LoggerHandle;
 use starcoin_rpc_api::debug::DebugApi;
@@ -10,12 +11,13 @@ use std::str::FromStr;
 use std::sync::Arc;
 
 pub struct DebugRpcImpl {
+    config: Arc<NodeConfig>,
     log_handle: Arc<LoggerHandle>,
 }
 
 impl DebugRpcImpl {
-    pub fn new(log_handle: Arc<LoggerHandle>) -> Self {
-        Self { log_handle }
+    pub fn new(config: Arc<NodeConfig>, log_handle: Arc<LoggerHandle>) -> Self {
+        Self { config, log_handle }
     }
 }
 
@@ -24,5 +26,12 @@ impl DebugApi for DebugRpcImpl {
         self.log_handle
             .update_level(LevelFilter::from_str(level.as_str()).map_err(to_invalid_param_err)?);
         Ok(())
+    }
+
+    fn panic(&self) -> Result<()> {
+        if !self.config.net().is_dev() {
+            return Err(jsonrpc_core::Error::invalid_request());
+        }
+        panic!("DebugApi.panic")
     }
 }
