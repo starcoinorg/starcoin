eabb2eaf90f7bc01b1e277431bbfb88fc95e8acc
lerencao <funfriendcjf@gmail.com>
2020-04-23 12:57:44 +0800

    [metric]: fix metric usage
    Signed-off-by: lerencao <funfriendcjf@gmail.com>

--- txpool/src/counters.rs
+++ txpool/src/counters.rs
@@ -1,12 +1,16 @@
 use once_cell::sync::Lazy;
-use prometheus::IntCounterVec;
+use prometheus::{IntCounterVec, IntGauge};
 
 /// Counter of txn status in tx_pool
 pub static TXN_STATUS_COUNTERS: Lazy<IntCounterVec> = Lazy::new(|| {
     register_int_counter_vec!(
         "txpool_txn_stats",
         "Counters of how many txn's stats in tx pool",
-        &["added", "rejected", "dropped", "invaid", "canceled", "culled"]
+        &["status"]
     )
     .unwrap()
+});
+
+pub static TXPOOL_TXNS_GAUGE: Lazy<IntGauge> = Lazy::new(|| {
+    register_int_gauge!("txpool_txn_nums", "Counter of how many txns in txpool").unwrap()
 });
