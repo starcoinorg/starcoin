2db5dfe8f2b0ba71b54c0b540e7655d1b8b3e117
lerencao <funfriendcjf@gmail.com>
2020-04-26 17:23:35 +0800

    [txpool]: add namespace
    Signed-off-by: lerencao <funfriendcjf@gmail.com>

--- txpool/src/counters.rs
+++ txpool/src/counters.rs
@@ -1,24 +1,29 @@
 use once_cell::sync::Lazy;
-use prometheus::{HistogramVec, IntCounterVec, IntGauge, IntGaugeVec};
+use prometheus::{HistogramOpts, HistogramVec, IntCounterVec, IntGauge, IntGaugeVec, Opts};
 
 /// Counter of txn status in tx_pool
 pub static TXN_STATUS_COUNTERS: Lazy<IntCounterVec> = Lazy::new(|| {
-    register_int_counter_vec!(
+    let opts = Opts::new(
         "txpool_txn_stats",
         "Counters of how many txn's stats in tx pool",
-        &["status"]
     )
-    .unwrap()
+    .namespace("starcoin");
+    register_int_counter_vec!(opts, &["status"]).unwrap()
 });
 
 pub static TXPOOL_TXNS_GAUGE: Lazy<IntGauge> = Lazy::new(|| {
-    register_int_gauge!("txpool_txn_nums", "Counter of how many txns in txpool").unwrap()
+    let opts =
+        Opts::new("txpool_txn_nums", "Counter of how many txns in txpool").namespace("starcoin");
+    register_int_gauge!(opts).unwrap()
 });
 
 pub static TXPOOL_STATUS_GAUGE_VEC: Lazy<IntGaugeVec> = Lazy::new(|| {
-    register_int_gauge_vec!("txpool_status", "Gauge of pool status", &["name"]).unwrap()
+    let opts = Opts::new("txpool_status", "Gauge of pool status").namespace("starcoin");
+    register_int_gauge_vec!(opts, &["name"]).unwrap()
 });
 
 pub static TXPOOL_SERVICE_HISTOGRAM: Lazy<HistogramVec> = Lazy::new(|| {
-    register_histogram_vec!("txpool_service", "Histogram of txpool service", &["api"]).unwrap()
+    let opts =
+        HistogramOpts::new("txpool_service", "Histogram of txpool service").namespace("starcoin");
+    register_histogram_vec!(opts, &["api"]).unwrap()
 });
