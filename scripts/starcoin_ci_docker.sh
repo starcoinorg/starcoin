24a1c75413cb09d2a35413131a1e273f1bde7ec3
fikgol <hit.cs.lijun@gmail.com>
2020-04-29 15:45:53 +0800

    Expose metrics ports

--- scripts/starcoin_ci_docker.sh
+++ scripts/starcoin_ci_docker.sh
@@ -12,35 +12,31 @@ cfg_root=/mnt/volume_01/starcoin_cfg
 function docker_rebuild(){
     echo  -e "*\n"'!'"starcoin"> $DIR/../target/debug/.dockerignore
     docker build -f $DIR/DockerfileCi -t starcoin:latest  $DIR/../target/debug/
-    
+
 }
 
 function start_starcoin(){
     local host_name=$1
     local name=$2
     local port=$3
-    shift 3;
+    local m_port=$4
+    shift 4;
     eval $(docker-machine env $host_name)
     docker_rebuild
     docker rm -f $name 1>/dev/null
     docker-machine ssh $host_name rm -f $cfg_root/$name/*/starcoin.ipc
-    docker run -td --restart=on-failure:1 -p $port:9840 -v $cfg_root/$name:/.starcoin --name $name starcoin -d /.starcoin $@
+    docker run -td --restart=on-failure:1 -p $port:9840 -p $m_port:9101 -v $cfg_root/$name:/.starcoin --name $name starcoin -d /.starcoin $@
 }
 
 function start_halley_seed(){
-    start_starcoin $1 $2 $3 -n halley -s full --node-key $SEED_NODE_KEY
+    start_starcoin $1 $2 $3 $4 -n halley -s full --node-key $SEED_NODE_KEY
 }
 
 function start_halley_node(){
-    start_starcoin $1 $2 $3 -n halley -s full --seed $SEED
+    start_starcoin $1 $2 $3 $4 -n halley -s full --seed $SEED
 }
-
-function clean_all(){
-    rm -rf /root/workspaces/starcoin_cfg
-}
-
 
 #TODO: start failed, clean all env and restart
-start_halley_seed starcoin-0 starcoin-0 $SEED_PORT
-start_halley_node starcoin-0 starcoin-1 9841
-start_halley_node starcoin-0 starcoin-2 9842
+start_halley_seed starcoin-0 starcoin-0 $SEED_PORT 9101
+start_halley_node starcoin-0 starcoin-1 9841 9102
+start_halley_node starcoin-0 starcoin-2 9842 9103
