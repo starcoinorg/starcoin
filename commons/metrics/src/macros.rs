5509f2c41bc4714b57e4ef7e9ac71e2cdeda2a4d
jolestar <jolestar@gmail.com>
2020-04-29 16:16:26 +0800

    [metrics] Define register_uint_gauge

--- commons/metrics/src/macros.rs
+++ commons/metrics/src/macros.rs
@@ -11,6 +11,18 @@ macro_rules! register_uint_gauge_vec {
 
 #[macro_export]
 #[doc(hidden)]
+macro_rules! register_uint_gauge {
+    ($OPTS:expr) => {{
+        let gauge = $crate::UIntGauge::with_opts($OPTS).unwrap();
+        $crate::register(Box::new(gauge.clone())).map(|_| gauge)
+    }};
+    ($NAME:expr, $HELP:expr) => {{
+        register_uint_gauge!($crate::opts!($NAME, $HELP))
+    }};
+}
+
+#[macro_export]
+#[doc(hidden)]
 macro_rules! __register_gauge_vec {
     ($TYPE:ident, $OPTS:expr, $LABELS_NAMES:expr) => {{
         let gauge_vec = $crate::$TYPE::new($OPTS, $LABELS_NAMES).unwrap();
