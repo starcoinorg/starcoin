8e652bc3c03e8251db4516be80f93247f58b3d37
fikgol <lijunchina@foxmail.com>
2020-05-08 05:16:26 +0000

    ci: support create different cluster

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cfg_path="/.starcoin"
-net="halley"
+net="$1"
+shift;
 rm -f $cfg_path/$net/starcoin.ipc &>/dev/null
 rm -f $cfg_path/$net/peers.json &>/dev/null
-./starcoin -d /.starcoin -n halley -s full $@
+./starcoin -d /.starcoin -n $net $@
