6a024df805c48540261de0a62b632d15f31b5039
fikgol <lijunchina@foxmail.com>
2020-05-12 06:47:48 +0000

    ci:start.sh: Fix exit clean check bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,8 @@ rm -f $cfg_path/$net/starcoin.ipc &>/dev/null
 rm -f $cfg_path/$net/peers.json &>/dev/null
 ./starcoin -d /.starcoin -n $net $@
 ret=$?
-if [ $ret -ne 120 ]; then
-    echo "Start failed with gensis mismatch code 120,clean data..."
+if [ $ret -eq 120 ]; then
+    echo "Start failed with gensis mismatch code 120, clean data..."
     rm -rf $cfg_path/$net/ &>/dev/null
     exit $ret
 fi
