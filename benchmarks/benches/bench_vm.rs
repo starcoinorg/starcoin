73fc8a1b1f257b858c0f28f27b3c03701f4dc392
chengsuoyuan <chengsuoyuan@teddymobile.cn>
2020-05-18 18:47:22 +0800

    [benchmark] modify sync bench size and vm add sample size

--- benchmarks/benches/bench_vm.rs
+++ benchmarks/benches/bench_vm.rs
@@ -1,12 +1,17 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use executor_benchmark::run_benchmark;
 
 pub fn transaction_execution(c: &mut Criterion) {
-    c.bench_function("transaction_execution", |b| {
-        b.iter(|| run_benchmark(20, 1000000, 20, 1))
-    });
+    let mut group = c.benchmark_group("vm");
+    group.sample_size(10);
+    let bench_id = "transaction_execution";
+    for i in vec![1u64, 5, 10, 20, 50].into_iter() {
+        group.bench_function(BenchmarkId::new(bench_id, i), |b| {
+            b.iter(|| run_benchmark(20, 1000000, i as usize, 1))
+        });
+    }
 }
 
 criterion_group!(starcoin_vm_benches, transaction_execution);
