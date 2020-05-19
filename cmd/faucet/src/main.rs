c28e4ce9b40c9eb2703c7932acd782311ceab4ec
fikgol <hit.cs.lijun@gmail.com>
2020-05-19 17:30:41 +0800

    cmd:faucet: cargo clippy

--- cmd/faucet/src/main.rs
+++ cmd/faucet/src/main.rs
@@ -23,8 +23,8 @@ fn main() {
     let opts: FaucetOpt = FaucetOpt::from_args();
     let account_address =
         AccountAddress::from_str(&opts.faucet_address).expect("Invalid faucet address");
-    let server =
-        Server::http(&opts.server_addr).expect(&format!("Faild to serve on {}", opts.server_addr));
+    let server = Server::http(&opts.server_addr)
+        .unwrap_or_else(|_| panic!("Failed to serve on {}", opts.server_addr));
     let client = RpcClient::connect_ipc(opts.ipc_path).expect("Failed to connect ipc");
     let account = client
         .wallet_get(account_address)
