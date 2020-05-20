d40fbb1e8a2682a5c3427e2fcce027678d61fe2a
chengsuoyuan <chengsuoyuan@teddymobile.cn>
2020-05-21 00:49:00 +0800

    [smoke-test] add node scenario implement

---
+++ testsuite/tests/src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+extern crate starcoin_node;
+
+use actix::clock::Duration;
+use starcoin_config::NodeConfig;
+use starcoin_node::run_dev_node;
+use std::sync::Arc;
+use std::thread;
+
+#[test]
+fn test_run_node() {
+    let config = Arc::new(NodeConfig::random_for_test());
+    let handle = run_dev_node(config);
+    thread::sleep(Duration::from_secs(10));
+    handle.stop().unwrap()
+}
