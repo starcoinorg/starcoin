aa54ab01c18c6af538ef29ab2515608cfa6b7109
chengsuoyuan <chengsuoyuan@teddymobile.cn>
2020-05-21 00:49:00 +0800

    [smoke-test] add node scenario implement

--- testsuite/tests/steps/mod.rs
+++ testsuite/tests/steps/mod.rs
@@ -1,5 +1,4 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod node;
 pub mod transaction;
--- testsuite/tests/steps/transaction.rs
+++ testsuite/tests/steps/transaction.rs
@@ -1,71 +1,14 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
-
 use crate::MyWorld;
-use anyhow::Error;
-use cucumber::{Steps, StepsBuilder};
-use starcoin_executor::executor::Executor;
-use starcoin_executor::TransactionExecutor;
-use starcoin_rpc_client::{RemoteStateReader, RpcClient};
-use starcoin_state_api::AccountStateReader;
-use starcoin_types::account_address::AccountAddress;
-use starcoin_types::account_config;
-use starcoin_types::transaction::authenticator::AuthenticationKey;
-use starcoin_types::transaction::helpers::TransactionSigner;
-use starcoin_wallet_api::WalletAccount;
+use cucumber::steps;
 
-pub fn steps() -> Steps<MyWorld> {
-    let mut builder: StepsBuilder<MyWorld> = Default::default();
-    builder
-        .then("charge money to account", |world: &mut MyWorld, _step| {
-            let client = world.rpc_client.as_ref().take().unwrap();
-            let to = world.default_account.as_ref().take().unwrap();
-            let pre_mine_address = account_config::association_address();
-            let result = transfer_txn(client, to, pre_mine_address, None);
-            assert!(result.is_ok());
-        })
-        .then(
-            "execute transfer transaction",
-            |world: &mut MyWorld, _step| {
-                let client = world.rpc_client.as_ref().take().unwrap();
-                let from_account = world.default_account.as_ref().take().unwrap();
-                let to_account = world.txn_account.as_ref().take().unwrap();
-                let result = transfer_txn(client, to_account, from_account.address, Some(100));
-                assert!(result.is_ok());
-            },
-        );
-    builder.build()
-}
+steps!(MyWorld => {
+    when regex "^a project with slug \"(.*)\" is submitted$" |world, matches, _| {
 
-fn transfer_txn(
-    client: &RpcClient,
-    to: &WalletAccount,
-    from: AccountAddress,
-    amount: Option<u64>,
-) -> Result<bool, Error> {
-    let chain_config = client.node_info().unwrap().net.get_config();
-    let pre_mine_config = chain_config
-        .pre_mine_config
-        .as_ref()
-        .expect("Dev net pre mine config must exist.");
+    };
 
-    let to_auth_key_prefix = AuthenticationKey::ed25519(&to.public_key).prefix();
+    then regex "^a project with slug \"(.*)\" is created$" |world, matches, _| {
 
-    let chain_state_reader = RemoteStateReader::new(client);
-    let account_state_reader = AccountStateReader::new(&chain_state_reader);
-    let account_resource = account_state_reader
-        .get_account_resource(&from)
-        .unwrap()
-        .unwrap();
-    let balance = account_state_reader.get_balance(&from).unwrap().unwrap();
-    let amount = amount.unwrap_or(balance * 20 / 100);
-    let raw_txn = Executor::build_transfer_txn(
-        from,
-        to.address,
-        to_auth_key_prefix.to_vec(),
-        account_resource.sequence_number(),
-        amount,
-    );
-    let txn = pre_mine_config.sign_txn(raw_txn).unwrap();
-    client.submit_transaction(txn.clone())
-}
+    };
+});
