b64eaf4dd19c7e1148064ad00e744156d9c612b9
fikgol <hit.cs.lijun@gmail.com>
2020-06-11 21:19:35 +0800

    ci: Clean data when start node return 134

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,8 @@ rm -f $cfg_path/$net/starcoin.ipc &>/dev/null
 rm -f $cfg_path/$net/peers.json &>/dev/null
 ./starcoin -d /.starcoin -n $net $@
 ret=$?
-if [ $ret -eq 120 ]; then
-    echo "Start failed with gensis mismatch code 120, clean data..."
+if [ $ret -eq 120 ] | [ $ret -eq 134 ]; then
+    echo "Start failed with gensis mismatch code 120 or 134, clean data..."
     rm -rf $cfg_path/$net/ &>/dev/null
 fi
 exit $ret
