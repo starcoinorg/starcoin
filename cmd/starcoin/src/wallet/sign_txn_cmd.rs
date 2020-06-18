7db8059568691f108956fbd5a0b413208d409d4e
caojiafeng <funfriendcjf@gmail.com>
2020-06-18 20:55:22 +0800

    cli: update filename
    Signed-off-by: caojiafeng <funfriendcjf@gmail.com>

--- cmd/starcoin/src/wallet/sign_txn_cmd.rs
+++ cmd/starcoin/src/wallet/sign_txn_cmd.rs
@@ -86,11 +86,7 @@ impl CommandAction for SignTxnCommand {
         let output_file_path = {
             let mut output_dir = opt.output_dir.clone().unwrap_or(current_dir()?);
             // use txn id's short str and signer as the file name
-            let file_name = format!(
-                "{}.{}",
-                txn.raw_txn().crypto_hash().short_str(),
-                signer_address.short_str()
-            );
+            let file_name = txn.raw_txn().crypto_hash().short_str();
             output_dir.push(file_name);
             output_dir.set_extension("multisig-txn.partial");
             output_dir
