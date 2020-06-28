26af4238a768696c50aa32896303fad1af0189da
fikgol <hit.cs.lijun@gmail.com>
2020-06-28 21:31:40 +0800

    release: Use 7zip for archive release asset

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,4 +4,9 @@ mkdir -p starcoin-artifacts/
 cp -v target/release/starcoin starcoin-artifacts/starcoin
 cp -v target/release/starcoin_miner starcoin-artifacts/starcoin_miner
 cp -v target/release/faucet starcoin-artifacts/faucet
-zip -r starcoin-$1.zip  starcoin-artifacts
+
+if [ "$1" == "windows-latest" ]; then
+  7z a -r starcoin-$1.zip starcoin-artifacts
+else
+  zip -r starcoin-$1.zip starcoin-artifacts
+fi
