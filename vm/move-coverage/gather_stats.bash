c52546eebdc466b001aeba2491b815fb3fce1301
chengsuoyuan <suoyuan@gmail.com>
2020-07-15 17:28:20 +0800

    [vm] update stdlib base test

--- vm/move-coverage/gather_stats.bash
+++ vm/move-coverage/gather_stats.bash
@@ -11,6 +11,11 @@ pushd ../stdlib || exit 1
 cargo run
 popd || exit 1
 
+echo "Running executor testsuite..."
+pushd ../../executor || exit 1
+cargo test
+popd || exit 1
+
 echo "Running functional testsuite..."
 pushd ../functional-tests || exit 1
 cargo test
@@ -20,7 +25,6 @@ echo "Converting trace file..."
 cargo run --bin move-trace-conversion -- -f "$TRACE_PATH" -o trace.mvcov
 echo "Producing coverage summaries..."
 cargo run --bin coverage-summaries -- -t trace.mvcov -s ../stdlib/staged/stdlib.mv -o "$1"
-cat ./baseline/coverage_report > "$2"
 
 unset MOVE_VM_TRACE
 
