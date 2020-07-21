b7422190b808ad13980206756e73a2d7062a13f6
jolestar <jolestar@gmail.com>
2020-07-21 18:22:52 +0800

    [cmd & example] Fix token example and cmd accpet_coin -> accept_token, coin_type -> token-type. (#912)

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -4,8 +4,8 @@
 pub use block_executor::block_execute;
 pub use executor::*;
 pub use starcoin_transaction_builder::{
-    build_accept_coin_txn, build_transfer_from_association, build_transfer_txn,
-    build_transfer_txn_by_coin_type, create_signed_txn_with_association_account,
+    build_accept_token_txn, build_transfer_from_association, build_transfer_txn,
+    build_transfer_txn_by_token_type, create_signed_txn_with_association_account,
     encode_create_account_script, encode_transfer_script, peer_to_peer_txn_sent_as_association,
     DEFAULT_MAX_GAS_AMOUNT,
 };
