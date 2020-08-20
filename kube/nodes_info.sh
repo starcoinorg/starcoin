cf451fe620847a7e6606655245c57ad542c71671
fikgol <hit.cs.lijun@gmail.com>
2020-08-20 10:37:03 +0800

    kube: Fix node keys issue  && Add loadbalance

--- kube/nodes_info.sh
+++ kube/nodes_info.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+set -e
 PORT=31440
 nodes=$(kubectl get pod  -l app=starcoin --field-selector=status.phase=Running  --no-headers=true|awk '{print $1}')
 addresses=$(for node in $nodes;do
 	      kubectl exec  $node --stdin --tty -- /starcoin/starcoin -n proxima -d /data/ -o json node info  |grep -v INFO |jq -r .ok.self_address
 	    done)
-f1=$(mktemp)
-f2=$(mktemp)
-kubectl get node -o wide --no-headers=true | awk '{print $1,$7}'| sort >f1
-kubectl get pod -o wide --no-headers=true | awk '{print $7,$1}' | sort >f2
-exips=($(join f1 f2 |awk '{print $2}'));rm f1 f2
+exips=($(kubectl get service -o wide|grep LoadBalance|sort -k 1|awk '{print $4}'))
 
 green=`tput setaf 2`
 echo -e "${green} Starcoin nodes in this cluster:"
@@ -18,5 +15,3 @@ for add in $addresses;do
     ip=${exips[$((counter++))]};
     echo "$add"|sed -e  "s/127.0.0.1/$ip/"|sed -e  "s/9840/$PORT/"
 done
-
-
