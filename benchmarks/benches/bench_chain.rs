97f05794ba1008cb7f39a32871cedfc6aeee4b24
chengsuoyuan <suoyuan@gmail.com>
2020-08-21 00:32:21 +0800

    [benchmark] modify bench test  parameter

--- benchmarks/benches/bench_chain.rs
+++ benchmarks/benches/bench_chain.rs
@@ -6,7 +6,7 @@ use criterion::{criterion_group, criterion_main, Benchmark, Criterion};
 
 fn block_apply(c: &mut Criterion) {
     ::logger::init();
-    for i in vec![100u64, 500].into_iter() {
+    for i in vec![20u64, 50, 100].into_iter() {
         c.bench(
             "block_try_connect",
             Benchmark::new(format!("connect_branch_{:?}", i), move |b| {
@@ -19,9 +19,9 @@ fn block_apply(c: &mut Criterion) {
 }
 
 fn query_block(c: &mut Criterion) {
-    for i in vec![100u64, 500].into_iter() {
+    for i in vec![20u64, 50, 100].into_iter() {
         for j in vec![2u64, 5, 10].into_iter() {
-            for k in vec![100u64, 500].into_iter() {
+            for k in vec![20u64, 50, 100].into_iter() {
                 for id in [format!("query_block_{:?}_{:?}_{:?}", i, j, k)].iter() {
                     c.bench(
                         "query_block",
