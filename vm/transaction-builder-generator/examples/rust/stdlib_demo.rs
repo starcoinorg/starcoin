104725ec0c0be4562a2146a3df41fdb79334e10e
chengsuoyuan <suoyuan@gmail.com>
2020-09-08 19:43:17 +0800

    [stdlib] update genesis and libra dependence

--- vm/transaction-builder-generator/examples/rust/stdlib_demo.rs
+++ vm/transaction-builder-generator/examples/rust/stdlib_demo.rs
@@ -4,7 +4,6 @@
 use lcs;
 use libra_stdlib::encode_peer_to_peer_with_metadata_script;
 use starcoin_types::{AccountAddress, Identifier, StructTag, TypeTag};
-
 fn main() {
     let token = TypeTag::Struct(StructTag {
         address: AccountAddress([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
@@ -12,13 +11,14 @@ fn main() {
         name: Identifier("LBR".into()),
         type_params: Vec::new(),
     });
-    let payee = AccountAddress([
+    let key_vec = [
+        0x22u8, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
         0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
-        0x22,
-    ]);
+        0x22, 0x22,
+    ];
     let amount = 1234567;
     let script =
-        encode_peer_to_peer_with_metadata_script(token, payee, Vec::new(), amount, Vec::new());
+        encode_peer_to_peer_with_metadata_script(token, key_vec.to_vec(), amount, Vec::new());
 
     let output = lcs::to_bytes(&script).unwrap();
     for o in output {
