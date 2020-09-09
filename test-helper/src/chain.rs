b2fb15196c50309ff28f109e2110224a96ce6814
tiangong3624749 <tiangong3624749@163.com>
2020-09-09 21:57:10 +0800

    Test block sync (#1255)
    * [sync]add test_block_sync_inner.
    
    * test-helper: Fix DummyNetworkService implement
    
    The dummy implement of rpc should encode Result
    
    * [sync]test sync block.
    
    Co-authored-by: fikgol <hit.cs.lijun@gmail.com>

--- test-helper/src/chain.rs
+++ test-helper/src/chain.rs
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
+use starcoin_account_api::AccountInfo;
 use starcoin_chain::BlockChain;
+use starcoin_consensus::Consensus;
 use starcoin_genesis::Genesis;
+use starcoin_traits::{ChainReader, ChainWriter};
 use starcoin_types::genesis_config::ChainNetwork;
 
 pub fn gen_blockchain_for_test(net: &ChainNetwork) -> Result<BlockChain> {
@@ -11,5 +14,26 @@ pub fn gen_blockchain_for_test(net: &ChainNetwork) -> Result<BlockChain> {
         Genesis::init_storage_for_test(net).expect("init storage by genesis fail.");
 
     let block_chain = BlockChain::new(net.consensus(), *startup_info.get_master(), storage)?;
+    Ok(block_chain)
+}
+
+pub fn gen_blockchain_with_blocks_for_test(count: u64, net: &ChainNetwork) -> Result<BlockChain> {
+    let mut block_chain = gen_blockchain_for_test(net)?;
+    let miner_account = AccountInfo::random();
+    for _i in 0..count {
+        let (block_template, _) = block_chain
+            .create_block_template(
+                *miner_account.address(),
+                Some(miner_account.get_auth_key().prefix().to_vec()),
+                None,
+                Vec::new(),
+                vec![],
+                None,
+            )
+            .unwrap();
+        let block = net.consensus().create_block(&block_chain, block_template)?;
+        block_chain.apply(block)?;
+    }
+
     Ok(block_chain)
 }
