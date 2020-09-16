4ce4e9227a2888eaee53c06acdbc5d145ab70aaa
fanngyuan <fanngyuan@gmail.com>
2020-09-16 18:26:39 +0800

    BalanceResource abi config

--- account/lib/src/account_test.rs
+++ account/lib/src/account_test.rs
@@ -9,7 +9,9 @@ use starcoin_types::account_address::AccountAddress;
 use starcoin_types::genesis_config::ChainId;
 use starcoin_types::identifier::{IdentStr, Identifier};
 use starcoin_types::language_storage::{StructTag, CORE_CODE_ADDRESS};
-use starcoin_types::transaction::{RawUserTransaction, Script, TransactionPayload};
+use starcoin_types::transaction::{
+    RawUserTransaction, Script, SignedUserTransaction, TransactionPayload,
+};
 use std::time::Duration;
 
 #[test]
@@ -98,7 +100,10 @@ pub fn test_libra_wallet() -> Result<()> {
     use core::convert::{From, TryFrom};
     use starcoin_canonical_serialization::SCSCodec;
     use starcoin_crypto::ed25519::{Ed25519PrivateKey, Ed25519PublicKey, Ed25519Signature};
-    use starcoin_crypto::{hash::CryptoHash, HashValue};
+    use starcoin_crypto::{
+        hash::{CryptoHash, PlainCryptoHash},
+        HashValue,
+    };
     use starcoin_types::transaction::authenticator::AuthenticationKey;
 
     let bytes = hex::decode("2c78c6fd8829de80451cda02310250b27307360ddc972d614fa0c8462ae41b3e")?;
@@ -169,5 +174,27 @@ pub fn test_libra_wallet() -> Result<()> {
     println!("path hash is {:?}", path.hash());
     let access_path = AccessPath::new(address, path.access_vector());
     println!("access path is {:?}", access_path);
+    let stxn_bytes = vec![
+        125, 67, 213, 38, 157, 219, 137, 205, 183, 247, 184, 18, 104, 155, 241, 53, 7, 0, 0, 0, 0,
+        0, 0, 0, 0, 161, 1, 161, 28, 235, 11, 1, 0, 0, 0, 6, 1, 0, 2, 3, 2, 17, 4, 19, 4, 5, 23,
+        24, 7, 47, 42, 8, 89, 16, 0, 0, 0, 1, 0, 1, 1, 1, 0, 2, 2, 3, 0, 0, 3, 4, 1, 1, 1, 0, 6, 2,
+        6, 2, 5, 10, 2, 0, 1, 5, 1, 1, 3, 6, 12, 5, 4, 4, 6, 12, 5, 10, 2, 4, 1, 9, 0, 7, 65, 99,
+        99, 111, 117, 110, 116, 14, 99, 114, 101, 97, 116, 101, 95, 97, 99, 99, 111, 117, 110, 116,
+        9, 101, 120, 105, 115, 116, 115, 95, 97, 116, 8, 112, 97, 121, 95, 102, 114, 111, 109, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 5, 1, 13, 10, 1, 17, 1, 32, 3, 5, 5, 8,
+        10, 1, 10, 2, 56, 0, 11, 0, 10, 1, 10, 3, 56, 1, 2, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 3, 83, 84, 67, 3, 83, 84, 67, 0, 3, 3, 170, 98, 21, 247, 38, 8, 228, 209,
+        97, 153, 20, 39, 180, 155, 110, 103, 4, 16, 112, 48, 56, 223, 253, 244, 219, 3, 173, 17,
+        252, 117, 207, 222, 197, 149, 2, 200, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 78,
+        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 13, 48, 120, 49, 58, 58, 83, 84, 67, 58, 58, 83,
+        84, 67, 5, 226, 96, 95, 0, 0, 0, 0, 254, 0, 32, 130, 108, 242, 253, 81, 233, 250, 135, 55,
+        141, 56, 92, 52, 117, 153, 246, 9, 69, 123, 70, 107, 203, 151, 216, 30, 34, 96, 130, 71,
+        68, 12, 143, 64, 6, 102, 250, 227, 98, 221, 129, 136, 197, 243, 79, 206, 201, 57, 0, 57,
+        163, 216, 146, 36, 227, 205, 214, 21, 85, 200, 71, 42, 155, 16, 207, 204, 134, 183, 87, 89,
+        253, 28, 178, 254, 244, 28, 94, 129, 152, 49, 111, 118, 238, 236, 36, 49, 239, 179, 197,
+        211, 150, 199, 7, 37, 161, 6, 202, 7,
+    ];
+    let stxn = SignedUserTransaction::decode(&stxn_bytes)?;
+    println!("txn hash is {:?}", stxn.crypto_hash());
     Ok(())
 }
