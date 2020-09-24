67c988fbef67c7c7b4f2b1b37cc6c02021fba3f2
fikgol <hit.cs.lijun@gmail.com>
2020-09-24 11:43:47 +0800

    uint: human_readable serialize

--- types/uint/src/lib.rs
+++ types/uint/src/lib.rs
@@ -19,14 +19,18 @@ macro_rules! impl_uint_serde {
     ($name: ident, $len: expr) => {
         impl Serialize for $name {
             fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-                use ser::SerializeTuple;
                 let mut bytes = [0u8; $len * 8];
                 self.to_big_endian(&mut bytes);
-                let mut seq = serializer.serialize_tuple($len * 8)?;
-                for byte in &bytes[..] {
-                    seq.serialize_element(byte)?;
+                if serializer.is_human_readable() {
+                    serializer.serialize_str(&hex::encode(&bytes))
+                } else {
+                    use ser::SerializeTuple;
+                    let mut seq = serializer.serialize_tuple($len * 8)?;
+                    for byte in &bytes[..] {
+                        seq.serialize_element(byte)?;
+                    }
+                    seq.end()
                 }
-                seq.end()
             }
         }
 
@@ -58,10 +62,31 @@ macro_rules! impl_uint_serde {
                         Ok($name::from_big_endian(&arr))
                     }
                 }
+                struct HexVisitor;
 
-                deserializer
-                    .deserialize_tuple($len * 8, ByteArrayVisitor)
-                    .map(|bytes| bytes.into())
+                impl<'de> de::Visitor<'de> for HexVisitor {
+                    type Value = $name;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                        write!(formatter, "A hex string")
+                    }
+
+                    fn visit_str<E: de::Error>(self, v: &str) -> Result<Self::Value, E> {
+                        let s = hex::decode(v).map_err(E::custom)?;
+                        Ok($name::from_big_endian(&s))
+                    }
+
+                    fn visit_string<E: de::Error>(self, v: String) -> Result<Self::Value, E> {
+                        self.visit_str(&v)
+                    }
+                }
+                if deserializer.is_human_readable() {
+                    deserializer.deserialize_str(HexVisitor)
+                } else {
+                    deserializer
+                        .deserialize_tuple($len * 8, ByteArrayVisitor)
+                        .map(|bytes| bytes.into())
+                }
             }
         }
     };
@@ -118,7 +143,6 @@ mod tests {
         let encoded = u.encode().unwrap();
         let u_decoded = U256::decode(&encoded).unwrap();
         assert_eq!(u, u_decoded);
-
         let max_hash: HashValue = U256::max_value().into();
         let max_u256: U256 = max_hash.into();
         assert_eq!(max_u256, U256::max_value());
@@ -126,5 +150,8 @@ mod tests {
         let h_encode = h.encode().unwrap();
         let h_decode = BlockHeader::decode(&h_encode).unwrap();
         assert_eq!(h, h_decode);
+        let human_encode = serde_json::to_string_pretty(&U256::max_value()).unwrap();
+        let human_decode: U256 = serde_json::from_str(&human_encode).unwrap();
+        assert_eq!(human_decode, U256::max_value());
     }
 }
