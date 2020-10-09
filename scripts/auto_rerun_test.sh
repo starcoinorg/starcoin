9526dbcce873d97db90320a57d395540353f997d
jolestar <jolestar@gmail.com>
2020-10-09 17:21:57 +0800

    [script] Set --no-fail-fast to test.

--- scripts/auto_rerun_test.sh
+++ scripts/auto_rerun_test.sh
@@ -2,20 +2,22 @@
 
 STARCOIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
-set -e
-
 TEST_RESULT_FILE="$STARCOIN_DIR/target/debug/test_result.txt"
 TEST_RESULT_FAILED_FILE="$STARCOIN_DIR/target/debug/test_result_failed.txt"
 
-RUST_LOG=OFF RUST_BACKTRACE=0 cargo test -q -- --color never --format pretty |tee "$TEST_RESULT_FILE" ||true
+RUST_LOG=OFF RUST_BACKTRACE=0 cargo test -q --no-fail-fast -- --color never --format pretty |tee "$TEST_RESULT_FILE" ||true
 grep -e '^test[[:space:]][^[:space:]]*[[:space:]]\.\.\.[[:space:]]FAILED' "$TEST_RESULT_FILE" >"$TEST_RESULT_FAILED_FILE" ||true
 
+status=0
 IFS=' '
 while read -r -a testcase; do
   case_name=${testcase[1]}
   echo "rerun failed test $case_name"
   RUST_LOG=DEBUG RUST_BACKTRACE=full cargo test "$case_name" -- --nocapture
-  status=$?
+  case_status=$?
+  if [ $case_status -ne 0 ]; then
+    status=$case_status
+  fi
 done < "$TEST_RESULT_FAILED_FILE"
 
 exit $status
