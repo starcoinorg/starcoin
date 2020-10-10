5ac5e82344b02a30177fcca510da3ac077648dfa
jolestar <jolestar@gmail.com>
2020-10-10 11:19:36 +0800

    [stest] Optimize stest error message report.

--- commons/stest/src/lib.rs
+++ commons/stest/src/lib.rs
@@ -3,10 +3,10 @@
 
 //! The stest lib enhances the rust test framework with some useful functions.
 
+use anyhow::{format_err, Result};
 use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
 use futures::stream::StreamExt;
 use futures::Future;
-
 use std::sync::mpsc::{Receiver, Sender};
 use std::time::Duration;
 pub use stest_macro::test;
@@ -20,62 +20,57 @@ pub fn init_test_logger() {
     starcoin_logger::init_for_test();
 }
 
-pub fn timeout<F, T>(timeout: u64, f: F, tx: Sender<Option<T>>)
+pub fn timeout<F, T>(timeout: u64, f: F, tx: Sender<Result<T>>)
 where
     F: FnOnce() -> T,
     F: Send + 'static,
     T: Send + 'static,
 {
-    let tx_clone = tx.clone();
-    let tx_cl = tx.clone();
-    std::thread::spawn(move || {
-        std::thread::sleep(std::time::Duration::from_secs(timeout));
-        let _ = tx.send(None);
-    });
-
-    let t = std::thread::spawn(move || {
-        let t = f();
-        let _ = tx_clone.send(Some(t));
-    });
-    std::thread::spawn(move || {
-        if t.join().is_err() {
-            let _ = tx_cl.send(None);
-        };
-    });
+    let handle = timeout_join_handler::spawn(f);
+    let _ = tx.send(
+        handle
+            .join(Duration::from_secs(timeout))
+            .map_err(|_| format_err!("test timeout for wait {} seconds", timeout)),
+    );
 }
 
-pub fn wait_channel<T>(rx: Receiver<Option<T>>) -> T {
+pub fn wait_channel<T>(rx: Receiver<Result<T>>) -> T {
     let result = rx.recv();
     match result {
-        Ok(Some(t)) => t,
-        _ => panic!("test timeout"),
+        Ok(Ok(t)) => t,
+        Ok(Err(e)) => panic!("test failed: {:?}", e),
+        _ => panic!("test receiver error"),
     }
 }
 
-pub fn make_channel<T>() -> (UnboundedSender<Option<T>>, UnboundedReceiver<Option<T>>) {
+pub fn make_channel<T>() -> (UnboundedSender<Result<T>>, UnboundedReceiver<Result<T>>) {
     unbounded()
 }
 
-pub async fn timeout_future<T>(timeout: u64, tx: UnboundedSender<Option<T>>) {
+pub async fn timeout_future<T>(timeout: u64, tx: UnboundedSender<Result<T>>) {
     actix::clock::delay_for(Duration::from_secs(timeout)).await;
-    let _ = tx.unbounded_send(None);
+    let _ = tx.unbounded_send(Err(format_err!(
+        "test timeout for wait {} seconds",
+        timeout
+    )));
 }
 
-pub async fn test_future<F, T>(f: F, tx: UnboundedSender<Option<T>>)
+pub async fn test_future<F, T>(f: F, tx: UnboundedSender<Result<T>>)
 where
     F: Future<Output = T> + Send + 'static,
     T: Send + 'static,
 {
     let join = tokio::task::spawn_local(f);
-    let t = join.await.ok();
-    let _ = tx.unbounded_send(t);
+    let t = join.await;
+    let _ = tx.unbounded_send(t.map_err(Into::<anyhow::Error>::into));
 }
 
-pub async fn wait_result<T>(mut rx: UnboundedReceiver<Option<T>>) -> T {
+pub async fn wait_result<T>(mut rx: UnboundedReceiver<Result<T>>) -> T {
     let result = rx.next().await;
     actix_rt::System::current().stop();
     match result {
-        Some(Some(t)) => t,
-        _ => panic!("test timeout"),
+        Some(Ok(t)) => t,
+        Some(Err(e)) => panic!("test fail: {:?}", e),
+        None => panic!("test timeout for wait result"),
     }
 }
