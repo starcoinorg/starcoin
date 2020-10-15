6670c7758c80f25e9c322fd03670205db56f0b4f
fikgol <hit.cs.lijun@gmail.com>
2020-10-15 11:22:02 +0800

    cnr: Add CryptoNightR hash support

---
+++ consensus/cryptonight-rs/src/lib.rs
@@ -0,0 +1,43 @@
+use libc::{c_void, c_char, size_t, c_int};
+
+#[link(name = "cryptonight")]
+extern "C" {
+    fn cn_slow_hash(data: *const c_void, length: size_t, hash: *const c_char, variant: c_int, prehashed: c_int, height: u64) -> c_void;
+}
+
+const VARIANT: i32 = 4;
+const PREHASHED: i32 = 0;
+const HEIGHT: u64 = 0;
+
+pub fn cryptonight_r(data: &[u8], size: usize) -> Vec<u8> {
+    let hash: Vec<i8> = vec![0i8; 32];
+    let data_ptr: *const c_void = data.as_ptr() as *const c_void;
+    let hash_ptr: *const c_char = hash.as_ptr() as *const c_char;
+    unsafe {
+        cn_slow_hash(data_ptr, size, hash_ptr, VARIANT, PREHASHED, HEIGHT);
+        std::mem::transmute::<Vec<i8>, Vec<u8>>(hash)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rustc_serialize as serialize;
+    use serialize::hex::FromHex;
+
+    struct TestCase {
+        input: Vec<u8>,
+        output: Vec<u8>,
+    }
+
+    #[test]
+    fn test_slow4() {
+        let data = TestCase {
+            input: "5468697320697320612074657374205468697320697320612074657374205468697320697320612074657374".from_hex().unwrap(),
+            output: "56bbeaee6ff36e4cd22a3bef0458c57d1bce74f392b5dac62da1bc2c20fabe94".from_hex().unwrap(),
+        };
+        let hash = cryptonight_r(&data.input[..], data.input.len());
+        assert_eq!(hash, data.output);
+    }
+}
