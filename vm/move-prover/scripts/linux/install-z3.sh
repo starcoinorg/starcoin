1fdd0c1377c2ef8b249bad712d92999417d5dc09
fikgol <lijunchina@foxmail.com>
2020-10-15 08:44:24 +0000

    update boogie and z3 version

--- vm/move-prover/scripts/linux/install-z3.sh
+++ vm/move-prover/scripts/linux/install-z3.sh
@@ -3,8 +3,8 @@
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-curl -LO https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-ubuntu-16.04.zip
-unzip z3-4.8.8-x64-ubuntu-16.04.zip
-sudo cp z3-4.8.8-x64-ubuntu-16.04/bin/z3 /usr/local/bin/
-rm -rf z3-4.8.8-x64-ubuntu-16.04
-rm -rf z3-4.8.8-x64-ubuntu-16.04.zip
+curl -LO https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-ubuntu-16.04.zip
+unzip z3-4.8.9-x64-ubuntu-16.04.zip
+sudo cp z3-4.8.9-x64-ubuntu-16.04/bin/z3 /usr/local/bin/
+rm -rf z3-4.8.9-x64-ubuntu-16.04
+rm -rf z3-4.8.9-x64-ubuntu-16.04.zip
