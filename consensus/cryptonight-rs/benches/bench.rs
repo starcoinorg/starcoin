bb62ba4c837ff68992b3ac887a25686ac58c0641
fikgol <hit.cs.lijun@gmail.com>
2020-10-19 12:28:55 +0800

    Update gensis for cnr config && fix hash rate info

--- consensus/cryptonight-rs/benches/bench.rs
+++ consensus/cryptonight-rs/benches/bench.rs
@@ -1,11 +1,12 @@
 #[macro_use]
 extern crate bencher;
 extern crate cryptonight;
+
 use bencher::Bencher;
 use cryptonight::cryptonight_r;
 
-fn bench_slow4_256(b: &mut Bencher) {
-    let bytes = [1u8; 256];
+fn bench_slow4_32(b: &mut Bencher) {
+    let bytes = [1u8; 32];
     b.iter(|| cryptonight_r(&bytes[..], bytes.len()))
 }
 
@@ -14,5 +15,5 @@ fn bench_slow4_1024(b: &mut Bencher) {
     b.iter(|| cryptonight_r(&bytes[..], bytes.len()))
 }
 
-benchmark_group!(benches, bench_slow4_256, bench_slow4_1024);
+benchmark_group!(benches, bench_slow4_32, bench_slow4_1024);
 benchmark_main!(benches);
