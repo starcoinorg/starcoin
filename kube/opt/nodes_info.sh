20ec1bd1fea00fa208976cc09c79837b63b1bc27
fikgol <hit.cs.lijun@gmail.com>
2020-11-13 19:15:06 +0800

    kube: Clean some useless codes

--- kube/opt/nodes_info.sh
+++ kube/opt/nodes_info.sh
@@ -3,7 +3,7 @@ set -e
 PORT=31440
 nodes=$(kubectl get pod  -l app=starcoin --field-selector=status.phase=Running  --no-headers=true|awk '{print $1}')
 addresses=$(for node in $nodes;do
-	      kubectl exec  $node --stdin --tty -- /starcoin/starcoin -n proxima -d /data/ -o json node info  |grep -v INFO |jq -r .ok.self_address
+	      kubectl exec  $node --stdin --tty -- /starcoin/starcoin -n proxima -d /sc-data/ -o json node info  |grep -v INFO |jq -r .ok.self_address
 	    done)
 exips=($(kubectl get service -o wide|grep LoadBalance|sort -k 1|awk '{print $4}'))
 
