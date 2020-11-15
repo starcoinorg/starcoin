8d5087792cc6418bf355ce23bfe256a368f79dec
tiangong3624749 <tiangong3624749@163.com>
2020-11-15 12:10:17 +0800

    [vm]Check strategy when upgrade module. (#1644)
    * [vm]Check strategy when upgrade module.
    
    * Remove todo.

--- vm/types/src/account_config/resources/mod.rs
+++ vm/types/src/account_config/resources/mod.rs
@@ -4,10 +4,12 @@
 pub mod account;
 pub mod balance;
 pub mod key_rotation_capability;
+pub mod module_upgrade_strategy;
 pub mod withdraw_capability;
 
 pub use crate::token::token_info::*;
 pub use account::*;
 pub use balance::*;
 pub use key_rotation_capability::*;
+pub use module_upgrade_strategy::*;
 pub use withdraw_capability::*;
