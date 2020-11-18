edc2d025b7246311c3e158b2e7e038e5e58bd629
Guangyu Zhu <guangyuz@gmail.com>
2020-11-18 19:56:41 +0800

    [cli] explain move error code even if there is no location

--- cmd/starcoin/src/debug/move_explain.rs
+++ cmd/starcoin/src/debug/move_explain.rs
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::cli_state::CliState;
+use crate::view::MoveExplainView;
 use crate::StarcoinOpt;
 use anyhow::Result;
-use errmapgen::ErrorContext;
 use scmd::{CommandAction, ExecContext};
 use starcoin_move_explain::get_explanation;
 use starcoin_vm_types::account_address::AccountAddress;
@@ -18,7 +18,7 @@ use structopt::StructOpt;
 pub struct MoveExplainOpt {
     /// The location (module id) returned with a `MoveAbort` error
     #[structopt(short = "l")]
-    location: String,
+    location: Option<String>,
     /// The abort code returned with a `MoveAbort` error
     #[structopt(short = "a")]
     abort_code: u64,
@@ -30,28 +30,49 @@ impl CommandAction for MoveExplain {
     type State = CliState;
     type GlobalOpt = StarcoinOpt;
     type Opt = MoveExplainOpt;
-    type ReturnItem = Option<ErrorContext>;
+    type ReturnItem = Option<MoveExplainView>;
 
     fn run(
         &self,
         ctx: &ExecContext<Self::State, Self::GlobalOpt, Self::Opt>,
     ) -> Result<Self::ReturnItem> {
         let opt = ctx.opt();
-        let mut location = opt.location.trim().split("::");
-        let mut address_literal = location.next().expect("Could not find address").to_string();
-        let module_name = location
-            .next()
-            .expect("Could not find module name")
-            .to_string();
-        if !address_literal.starts_with("0x") {
-            address_literal = format!("0x{}", address_literal);
-        }
-        let module_id = ModuleId::new(
-            AccountAddress::from_hex_literal(&address_literal)
-                .expect("Unable to parse module address"),
-            Identifier::new(module_name).expect("Invalid module name encountered"),
-        );
+        let category = opt.abort_code & 0xFFu64;
+        let reason_code = opt.abort_code >> 8;
+        match opt.location {
+            Some(_) => {
+                let mut location = opt.location.as_ref().unwrap().trim().split("::");
+                let mut address_literal =
+                    location.next().expect("Could not find address").to_string();
+                let module_name = location
+                    .next()
+                    .expect("Could not find module name")
+                    .to_string();
+                if !address_literal.starts_with("0x") {
+                    address_literal = format!("0x{}", address_literal);
+                }
+                let module_id = ModuleId::new(
+                    AccountAddress::from_hex_literal(&address_literal)
+                        .expect("Unable to parse module address"),
+                    Identifier::new(module_name).expect("Invalid module name encountered"),
+                );
 
-        Ok(get_explanation(&module_id, opt.abort_code))
+                let error_ctx =
+                    get_explanation(&module_id, opt.abort_code).expect("Failed to get explain");
+
+                Ok(Some(MoveExplainView {
+                    category_code: category,
+                    category_name: Some(error_ctx.category.code_name),
+                    reason_code,
+                    reason_name: Some(error_ctx.reason.code_name),
+                }))
+            }
+            None => Ok(Some(MoveExplainView {
+                category_code: category,
+                category_name: None,
+                reason_code,
+                reason_name: None,
+            })),
+        }
     }
 }
