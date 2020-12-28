f9b4944f4e6f98d828c4d334ebf4a21a5eab32fe
jolestar <jolestar@gmail.com>
2020-12-28 15:33:52 +0800

    [benchmark & ci] use env CARGO_TARGET_DIR to set target dir.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -9,4 +9,6 @@ if [ -z "$1" ]
     target_dir="$1"
 fi
 echo "benchmark use target_dir ${target_dir}"
+# cargo bench criterion use env to detect target dir https://github.com/bheisler/criterion.rs/issues/192
+export CARGO_TARGET_DIR="${target_dir}"
 cargo bench --bench bench_storage --bench bench_chain --bench bench_state_tree --bench bench_vm --target-dir "$target_dir"
