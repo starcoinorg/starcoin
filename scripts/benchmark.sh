907db48260dadebd7d1df12118e9e6a8001ac6c9
jolestar <jolestar@gmail.com>
2020-12-28 14:13:00 +0800

    [benchmark & ci] benchmark script reuse target_dir for measure change.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cp -rf benchmarks/target/criterion /mnt/volume_01/starcoin_benchmarks
-echo "benchmark result publish ok!"
+
+STARCOIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+if [ -z "$1" ]
+  then
+    target_dir="benchmarks/target/"
+  else
+    target_dir="$1"
+fi
+echo "benchmark use target_dir ${target_dir}"
+cargo bench --bench bench_storage --bench bench_chain --bench bench_state_tree --bench bench_vm --target-dir "$target_dir"
