a4b85dd63b06474cc8bb7a72b8084cfb026a0d14
jolestar <jolestar@gmail.com>
2020-12-29 09:06:37 +0800

    [benchmark] fix benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,13 +2,14 @@
 
 STARCOIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
-if [ -z "$1" ]
+cmd="cargo bench --bench bench_storage --bench bench_chain --bench bench_state_tree --bench bench_vm"
+# shellcheck disable=SC2236
+if [ -n "$1" ]
   then
-    target_dir="benchmarks/target/"
-  else
     target_dir="$1"
+    # cargo bench criterion use env to detect target dir https://github.com/bheisler/criterion.rs/issues/192
+    export CARGO_TARGET_DIR="${target_dir}"
+    cmd="${cmd} --target-dir ${target_dir}"
 fi
-echo "benchmark use target_dir ${target_dir}"
-# cargo bench criterion use env to detect target dir https://github.com/bheisler/criterion.rs/issues/192
-export CARGO_TARGET_DIR="${target_dir}"
-cargo bench --bench bench_storage --bench bench_chain --bench bench_state_tree --bench bench_vm --target-dir "$target_dir"
+echo "run benchmark with cmd: ${cmd}"
+eval "$cmd"
