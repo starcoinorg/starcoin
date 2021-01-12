4bfad3f54f949f68938a406ec24e8de9bcaf5696
jolestar <jolestar@gmail.com>
2021-01-12 09:29:55 +0800

    [ci]Deploy halley to kubernetes (#1987)
    * [deploy] Deploy halley to kubernetes, and define halley/proxima namespace.
    
    * [cmd] Fix logger option bug.
    
    * [ci] Define runner service account and update runner image.
    
    * [ci] Update runner images.
    
    * [ci] Builder docker and deploy
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup

---
+++ scripts/ops_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
