5e53868774b2f402d4eec72c3ba7526d949d732b
jolestar <jolestar@gmail.com>
2021-02-04 16:17:41 +0800

    [network-p2p] Fix handshake message max length bug. (#2106)
    Co-authored-by: 0xa <hit.cs.lijun@gmail.com>

--- network-p2p/src/protocol/generic_proto/upgrade/notifications.rs
+++ network-p2p/src/protocol/generic_proto/upgrade/notifications.rs
@@ -48,7 +48,7 @@ use std::{
 use unsigned_varint::codec::UviBytes;
 
 /// Maximum allowed size of the two handshake messages, in bytes.
-const MAX_HANDSHAKE_SIZE: usize = 1024;
+pub const MAX_HANDSHAKE_SIZE: usize = 2048;
 
 /// Upgrade that accepts a substream, sends back a status message, then becomes a unidirectional
 /// stream of messages.
