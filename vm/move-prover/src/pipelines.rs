e12da2f978a2f387f173b69e5a5285d084652f2f
Guangyu Zhu <guangyuz@gmail.com>
2021-03-08 10:31:06 +0800

    [vm] upgrade to vm_03_08

--- vm/move-prover/src/pipelines.rs
+++ vm/move-prover/src/pipelines.rs
@@ -5,13 +5,12 @@
 
 use crate::cli::Options;
 use bytecode::{
-    borrow_analysis::BorrowAnalysisProcessor, borrow_analysis_v2,
-    clean_and_optimize::CleanAndOptimizeProcessor, eliminate_imm_refs::EliminateImmRefsProcessor,
+    borrow_analysis::BorrowAnalysisProcessor, clean_and_optimize::CleanAndOptimizeProcessor,
+    eliminate_imm_refs::EliminateImmRefsProcessor,
     function_target_pipeline::FunctionTargetProcessor, livevar_analysis::LiveVarAnalysisProcessor,
     loop_analysis::LoopAnalysisProcessor, memory_instrumentation::MemoryInstrumentationProcessor,
-    memory_instrumentation_v2, mut_ref_instrumentation::MutRefInstrumenter,
-    reaching_def_analysis::ReachingDefProcessor, usage_analysis::UsageProcessor,
-    verification_analysis::VerificationAnalysisProcessor,
+    mut_ref_instrumentation::MutRefInstrumenter, reaching_def_analysis::ReachingDefProcessor,
+    usage_analysis::UsageProcessor, verification_analysis::VerificationAnalysisProcessor,
 };
 
 /// Allows client to decide between one of two pipelines for ease of benchmarking
@@ -22,7 +21,7 @@ pub fn pipelines(options: &Options) -> Vec<Box<dyn FunctionTargetProcessor>> {
             MutRefInstrumenter::new(),
             ReachingDefProcessor::new(),
             LiveVarAnalysisProcessor::new(),
-            BorrowAnalysisProcessor::new(),
+            BorrowAnalysisProcessor::new(options.strong_edges),
             MemoryInstrumentationProcessor::new(),
             CleanAndOptimizeProcessor::new(),
             UsageProcessor::new(),
@@ -31,17 +30,6 @@ pub fn pipelines(options: &Options) -> Vec<Box<dyn FunctionTargetProcessor>> {
         ]
     } else {
         // Enter your pipeline here
-        vec![
-            EliminateImmRefsProcessor::new(),
-            MutRefInstrumenter::new(),
-            ReachingDefProcessor::new(),
-            LiveVarAnalysisProcessor::new(),
-            borrow_analysis_v2::BorrowAnalysisProcessor::new(),
-            memory_instrumentation_v2::MemoryInstrumentationProcessor::new(),
-            CleanAndOptimizeProcessor::new(),
-            UsageProcessor::new(),
-            VerificationAnalysisProcessor::new(),
-            LoopAnalysisProcessor::new(),
-        ]
+        panic!("No experimental pipeline set");
     }
 }
