135abed0bf89ba00f313f7b74315fc84065036f9
jolestar <jolestar@gmail.com>
2021-03-15 22:49:11 +0800

    [network-p2p] remove request future timeout.

--- network-p2p/src/service.rs
+++ network-p2p/src/service.rs
@@ -53,7 +53,6 @@ use crate::{
     config::{parse_addr, parse_str_addr, NonReservedPeerMode},
     transport,
 };
-use async_std::future;
 use futures::channel::oneshot::Canceled;
 use futures::{
     channel::{mpsc, oneshot},
@@ -76,7 +75,6 @@ use starcoin_metrics::{Histogram, HistogramVec};
 use starcoin_types::startup_info::ChainStatus;
 use std::collections::HashMap;
 use std::num::NonZeroUsize;
-use std::time::Duration;
 
 /// Minimum Requirements for a Hash within Networking
 pub trait ExHashT: std::hash::Hash + Eq + std::fmt::Debug + Clone + Send + Sync + 'static {}
@@ -718,13 +716,12 @@ impl NetworkService {
             request,
             pending_response: tx,
         });
-        match future::timeout(Duration::from_secs(5), rx).await {
-            Ok(Ok(v)) => v,
+        match rx.await {
+            Ok(v) => v,
             // The channel can only be closed if the network worker no longer exists. If the
             // network worker no longer exists, then all connections to `target` are necessarily
             // closed, and we legitimately report this situation as a "ConnectionClosed".
             Err(_) => Err(RequestFailure::Network(OutboundFailure::ConnectionClosed)),
-            Ok(Err(_)) => Err(RequestFailure::Network(OutboundFailure::Timeout)),
         }
     }
 
