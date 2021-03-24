fcef2c4cc0ba93a90629d712e8197b1719e23ff8
jolestar <jolestar@gmail.com>
2021-03-24 12:57:52 +0800

    [cleanup] move core/genesis to genesis.

--- miner/src/create_block_template/test_create_block_template.rs
+++ miner/src/create_block_template/test_create_block_template.rs
@@ -5,11 +5,11 @@ use crate::create_block_template::{
     CreateBlockTemplateRequest, CreateBlockTemplateService, EmptyProvider, Inner,
 };
 use anyhow::Result;
-use chain::BlockChain;
 use consensus::Consensus;
 use logger::prelude::*;
 use starcoin_account_api::AccountInfo;
 use starcoin_account_service::AccountService;
+use starcoin_chain::BlockChain;
 use starcoin_chain::{ChainReader, ChainWriter};
 use starcoin_config::ChainNetworkID;
 use starcoin_config::{temp_path, NodeConfig, StarcoinOpt};
