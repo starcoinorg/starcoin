9e8d59322979e6f38b66ba2b9a8b2949b801e3eb
lerencao <funfriendcjf@gmail.com>
2021-04-14 14:32:34 +0800

    move: impl starcoin merkle verfier (#2418)
    * move: impl starcoin merkle verfier
    
    * fix on comments

---
+++ contrib-contracts/modules/StarcoinVerifier.move
@@ -0,0 +1,87 @@
+address 0xA550C18 {
+    module StarcoinVerifierScripts {
+        use 0xA550C18::StarcoinVerifier;
+        public(script) fun create(signer: signer, merkle_root: vector<u8>) {
+            StarcoinVerifier::create(&signer, merkle_root);
+        }
+    }
+    module StarcoinVerifier {
+        use 0x1::Vector;
+        use 0xA550C18::Bit;
+        use 0xA550C18::StructuredHash;
+        use 0x1::Hash;
+
+        struct StarcoinMerkle has key {
+            merkle_root: vector<u8>,
+        }
+
+        struct Node has store, drop {
+            hash1: vector<u8>,
+            hash2: vector<u8>,
+        }
+
+        const HASH_LEN_IN_BIT: u64 = 32 * 8;
+        const SPARSE_MERKLE_LEAF_NODE: vector<u8> = b"SparseMerkleLeafNode";
+        const SPARSE_MERKLE_INTERNAL_NODE: vector<u8> = b"SparseMerkleInternalNode";
+        public fun create(signer: &signer, merkle_root: vector<u8>) {
+            let s = StarcoinMerkle {
+                merkle_root
+            };
+            move_to(signer, s);
+        }
+
+        public fun verify_on(merkle_address: address, account_address: vector<u8>, account_state_root_hash: vector<u8>, proofs: vector<vector<u8>>): bool
+        acquires StarcoinMerkle  {
+            let merkle = borrow_global<StarcoinMerkle>(merkle_address);
+
+            verify(*&merkle.merkle_root, account_address, account_state_root_hash, proofs)
+        }
+
+        public fun verify(expected_root: vector<u8>, account_address: vector<u8>, account_state_root_hash: vector<u8>, proofs: vector<vector<u8>>): bool {
+            let address_hash = Hash::sha3_256(account_address);
+            let leaf_node = Node { hash1: copy address_hash, hash2: account_state_root_hash};
+            let current_hash = StructuredHash::hash(SPARSE_MERKLE_LEAF_NODE, &leaf_node);
+            let i = 0;
+            let proof_length = Vector::length(&proofs);
+            while (i < proof_length) {
+                let sibling = *Vector::borrow(&proofs, i);
+                let bit = Bit::get_bit(&address_hash, proof_length - i - 1);
+                let internal_node = if (bit) {
+                    Node {hash1: sibling, hash2: current_hash}
+                } else {
+                    Node {hash1: current_hash, hash2: sibling}
+                };
+                current_hash = StructuredHash::hash(SPARSE_MERKLE_INTERNAL_NODE, &internal_node);
+                i = i+1;
+            };
+            current_hash == expected_root
+        }
+    }
+
+    module StructuredHash {
+        use 0x1::Hash;
+        use 0x1::Vector;
+        use 0x1::BCS;
+        const STARCOIN_HASH_PREFIX: vector<u8> = b"STARCOIN::";
+        public fun hash<MoveValue: store>(structure: vector<u8>, data: &MoveValue): vector<u8> {
+            let prefix_hash = Hash::sha3_256(concat(&STARCOIN_HASH_PREFIX, structure));
+            let bcs_bytes = BCS::to_bytes(data);
+            Hash::sha3_256(concat(&prefix_hash, bcs_bytes))
+        }
+
+        fun concat(v1: &vector<u8>, v2: vector<u8>): vector<u8> {
+            let data = *v1;
+            Vector::append(&mut data, v2);
+            data
+        }
+
+    }
+    module Bit {
+        use 0x1::Vector;
+        public fun get_bit(data: &vector<u8>, index: u64): bool {
+            let pos = index / 8;
+            let bit = (7 - index % 8);
+            (*Vector::borrow(data, pos) >> (bit as u8)) & 1u8 != 0
+        }
+    }
+}
---
+++ contrib-contracts/src/starcoin_merkle_test.rs
@@ -0,0 +1,113 @@
+use anyhow::Result;
+use starcoin_crypto::hash::PlainCryptoHash;
+use starcoin_executor::execute_readonly_function;
+use starcoin_state_api::{ChainStateReader, ChainStateWriter};
+use starcoin_types::account_config::account_struct_tag;
+use starcoin_types::identifier::Identifier;
+use starcoin_types::language_storage::ModuleId;
+use starcoin_vm_types::access_path::AccessPath;
+use starcoin_vm_types::account_config::association_address;
+use starcoin_vm_types::transaction::{Package, ScriptFunction, TransactionPayload};
+use starcoin_vm_types::value::MoveValue;
+use starcoin_vm_types::values::VMValueCast;
+use test_helper::executor::{association_execute, compile_modules_with_address, prepare_genesis};
+#[stest::test]
+fn test_starcoin_merkle() -> Result<()> {
+    let (chain_state, net) = prepare_genesis();
+    let ap = AccessPath::resource_access_path(association_address(), account_struct_tag());
+    let state_with_proof = chain_state.get_with_proof(&ap)?;
+    state_with_proof.proof.verify(
+        chain_state.state_root(),
+        ap,
+        state_with_proof.state.as_deref(),
+    )?;
+
+    {
+        let source = include_str!("../modules/StarcoinVerifier.move");
+        let modules = compile_modules_with_address(association_address(), source);
+
+        let package = Package::new(modules, None)?;
+        association_execute(
+            net.genesis_config(),
+            &chain_state,
+            TransactionPayload::Package(package),
+        )?;
+        chain_state.commit()?;
+        chain_state.flush()?;
+    }
+
+    let state_root = chain_state.state_root();
+
+    {
+        let script_function = ScriptFunction::new(
+            ModuleId::new(
+                association_address(),
+                Identifier::new("StarcoinVerifierScripts").unwrap(),
+            ),
+            Identifier::new("create").unwrap(),
+            vec![],
+            vec![MoveValue::vector_u8(state_root.to_vec())
+                .simple_serialize()
+                .unwrap()],
+        );
+        association_execute(
+            net.genesis_config(),
+            &chain_state,
+            TransactionPayload::ScriptFunction(script_function),
+        )?;
+        chain_state.commit()?;
+        chain_state.flush()?;
+    }
+
+    {
+        // change to previout state root.
+        let old_chain_state = chain_state.change_root(state_root);
+        // let state_root = chain_state.state_root();
+        let _expected_root = MoveValue::vector_u8(state_root.to_vec());
+
+        let ap = AccessPath::resource_access_path(association_address(), account_struct_tag());
+        let state_with_proof = old_chain_state.get_with_proof(&ap)?;
+        let account_address = MoveValue::vector_u8(association_address().to_vec());
+        let account_state_hash = MoveValue::vector_u8(
+            state_with_proof
+                .proof
+                .account_state
+                .unwrap()
+                .crypto_hash()
+                .to_vec(),
+        );
+        let proof = MoveValue::Vector(
+            state_with_proof
+                .proof
+                .account_proof
+                .siblings()
+                .iter()
+                .map(|h| MoveValue::vector_u8(h.to_vec()))
+                .collect(),
+        );
+
+        let result = execute_readonly_function(
+            &chain_state,
+            &ModuleId::new(
+                association_address(),
+                Identifier::new("StarcoinVerifier").unwrap(),
+            ),
+            &Identifier::new("verify_on").unwrap(),
+            vec![],
+            vec![
+                MoveValue::Address(association_address())
+                    .simple_serialize()
+                    .unwrap(),
+                account_address.simple_serialize().unwrap(),
+                account_state_hash.simple_serialize().unwrap(),
+                proof.simple_serialize().unwrap(),
+            ],
+        )?
+        .pop()
+        .unwrap()
+        .1;
+        let is_ok: bool = result.cast().unwrap();
+        assert!(is_ok, "verify fail");
+    }
+    Ok(())
+}
