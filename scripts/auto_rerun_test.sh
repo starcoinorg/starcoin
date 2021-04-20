4334702e4d24733d93f15a21f16fa381250c1c88
jolestar <jolestar@gmail.com>
2021-04-20 16:01:17 +0800

    [script] Use xtest to run test.

--- scripts/auto_rerun_test.sh
+++ scripts/auto_rerun_test.sh
@@ -13,10 +13,17 @@ fi
 export BOOGIE_EXE=$BOOGIE_PATH;
 export Z3_EXE=/usr/local/bin/z3;
 
-export RUSTFLAGS='-Ccodegen-units=1'
+export RUSTFLAGS='-Ccodegen-units=1 -Copt-level=0'
+export RUSTC_BOOTSTRAP=1
+export CARGO_INCREMENTAL=0
+export RUST_MIN_STACK=8388608 # 8 * 1024 * 1024
+
+echo check ulimits
+ulimit -a
+
 #pleanse ensure tow test command's argument is same.
-cargo test --no-run -- --color never --format pretty
-RUST_LOG=OFF RUST_BACKTRACE=0 cargo test --no-fail-fast -j 1 -- --color never --format pretty |tee "$TEST_RESULT_FILE" ||true
+cargo xtest --no-run -- --color never --format pretty
+RUST_LOG=OFF RUST_BACKTRACE=0 cargo xtest --no-fail-fast -j 1 -- --color never --format pretty |tee "$TEST_RESULT_FILE" ||true
 grep -e '^test[[:space:]][^[:space:]]*[[:space:]]\.\.\.[[:space:]]FAILED' "$TEST_RESULT_FILE" >"$TEST_RESULT_FAILED_FILE" ||true
 
 status=0
@@ -24,7 +31,7 @@ IFS=' '
 while read -r -a testcase; do
   case_name=${testcase[1]}
   echo "rerun failed test $case_name"
-  RUST_LOG=DEBUG RUST_BACKTRACE=full cargo test --no-fail-fast -j 1 "$case_name" -- --nocapture
+  RUST_LOG=DEBUG RUST_BACKTRACE=full cargo xtest --no-fail-fast -j 1 "$case_name" -- --nocapture
   case_status=$?
   if [ $case_status -ne 0 ]; then
     status=$case_status
