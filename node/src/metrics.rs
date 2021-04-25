16792b215cc7fd21dd4df7b5e042bdeaa040cac5
chengsuoyuan <suoyuan@gmail.com>
2021-04-25 15:31:02 +0800

    [k8s-config] remove redundant metrics and fix txfactory bug

--- node/src/metrics.rs
+++ node/src/metrics.rs
@@ -10,24 +10,6 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
 
-pub static NODE_METRICS: Lazy<NodeMetrics> = Lazy::new(|| NodeMetrics::register().unwrap());
-
-#[derive(Clone)]
-pub struct NodeMetrics {
-    pub block_latency: Histogram,
-}
-
-impl NodeMetrics {
-    pub fn register() -> Result<Self, PrometheusError> {
-        let request_block_latency =
-            register_histogram!("request_block_latency", "request_block_latency")?;
-
-        Ok(Self {
-            block_latency: request_block_latency,
-        })
-    }
-}
-
 #[derive(Clone)]
 pub struct MetricsActorService {
     push_url: String,
