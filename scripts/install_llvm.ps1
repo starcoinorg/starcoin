34f48e343d30c969a49b797ee3442e1d5c7e3a16
jolestar <jolestar@gmail.com>
2021-06-25 14:12:39 +0800

    [ci] Fix build on windows (#2630)
    * [ci]  Install LLVM and Clang on windows for fix build on windows failed.
    
    * [release] remove some binary from artifacts and add move cli to docker image.
    
    * [ci] Support upload artifact asset on action page.

--- scripts/install_llvm.ps1
+++ scripts/install_llvm.ps1
@@ -1,8 +1,8 @@
-$llvmVersion = "8.0.0"
+$llvmVersion = "12.0.0"
 Write-Host "Installing LLVM $llvmVersion ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
 $exePath = "$env:temp\LLVM-$llvmVersion-win64.exe"
-(New-Object Net.WebClient).DownloadFile("http://releases.llvm.org/$llvmVersion/LLVM-$llvmVersion-win64.exe", $exePath)
+(New-Object Net.WebClient).DownloadFile("https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvmVersion/LLVM-$llvmVersion-win64.exe", $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /S
 Write-Host "Installed" -ForegroundColor Green
