16181020d86e7c63d354dda7ff6da3c1c5c866de
nk_ysg <nk_ysg@163.com>
2021-10-21 10:27:08 +0800

    limit get_txn_info_list count size
    limit get_txn_info_list count size

--- cmd/starcoin/src/chain/get_txn_info_list_cmd.rs
+++ cmd/starcoin/src/chain/get_txn_info_list_cmd.rs
@@ -8,6 +8,9 @@ use scmd::{CommandAction, ExecContext};
 use starcoin_rpc_api::types::TransactionInfoView;
 use structopt::StructOpt;
 
+// this value reference inner_sync_task.rs do_sync function defined value
+const INFO_MAX_SIZE: u64 = 100;
+
 /// Get transaction infos list
 #[derive(Debug, StructOpt)]
 #[structopt(name = "get-txn-info-list", alias = "get_txn_info_list")]
@@ -37,10 +40,11 @@ impl CommandAction for GetTransactionInfoListCommand {
     ) -> Result<Self::ReturnItem> {
         let client = ctx.state().client();
         let opt = ctx.opt();
+        let count = std::cmp::min(opt.count, INFO_MAX_SIZE);
         let txn_infos = client.chain_get_transaction_infos(
             opt.start_index,
             opt.reverse.unwrap_or(false),
-            opt.count,
+            count,
         )?;
         Ok(txn_infos)
     }
