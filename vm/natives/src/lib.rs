881a986b167bd5741388d08a5d96b57aeeaa7d9f
jolestar <jolestar@gmail.com>
2021-11-10 11:04:35 +0800

    [vm] Implement native function ecrecover (#3015)

--- vm/natives/src/lib.rs
+++ vm/natives/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 pub mod bcs;
 pub mod debug;
 pub mod hash;
@@ -5,3 +8,5 @@ pub mod token;
 // the following two modules are copied from diem-framework. As we don't want to add deps on diem.
 pub mod account;
 pub mod signature;
+// for support ethereum compat.
+pub mod ecrecover;
