616cff42d4595d8ad5cf3c53c71cc7416027dfa7
fountainchen <fountainchen@yahoo.com>
2021-11-29 14:48:28 +0800

    [gen]add difficulty test

--- consensus/src/consensus_test.rs
+++ consensus/src/consensus_test.rs
@@ -250,3 +250,210 @@ fn test_next_target_increment_difficulty_compare() {
     assert!(next_target_1 < target0);
     assert!(next_target_2 > target0);
 }
+
+#[stest::test(timeout = 100)]
+fn test_next_target_target() {
+    let blocks_1 = vec![
+        // BlockDiffInfo {
+        //     timestamp: 1637911931937,
+        //     target: difficult_to_target("0x128dd7af".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911903653,
+        //     target: difficult_to_target("0x14a42733".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911889623,
+        //     target: difficult_to_target("0x133b86e6".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911888900,
+        //     target: difficult_to_target("0x11f54845".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911888227,
+        //     target: difficult_to_target("0x1310dccb".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911877864,
+        //     target: difficult_to_target("0x1222c9f0".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911875570,
+        //     target: difficult_to_target("0x12b92b58".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911867620,
+        //     target: difficult_to_target("0x124da70c".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911863497,
+        //     target: difficult_to_target("0x11408ac0".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911862363,
+        //     target: difficult_to_target("0x15d7bcb2".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911839267,
+        //     target: difficult_to_target("0x16317044".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911832464,
+        //     target: difficult_to_target("0x168d12d7".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911825582,
+        //     target: difficult_to_target("0x1ac09aff".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911811002,
+        //     target: difficult_to_target("0x18a2cdbd".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911809762,
+        //     target: difficult_to_target("0x184f681a".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911804871,
+        //     target: difficult_to_target("0x18115b5e".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911800093,
+        //     target: difficult_to_target("0x1afaf67a".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911789309,
+        //     target: difficult_to_target("0x19f7fe19".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911785915,
+        //     target: difficult_to_target("0x180d92a4".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911784248,
+        //     target: difficult_to_target("0x161206f7".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911782976,
+        //     target: difficult_to_target("0x15688141".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911779023,
+        //     target: difficult_to_target("0x13ec5a26".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911777159,
+        //     target: difficult_to_target("0x1431ab06".into()),
+        // },
+        // BlockDiffInfo {
+        //     timestamp: 1637911770816,
+        //     target: difficult_to_target("0x135b2bde".into()),
+        // },
+        BlockDiffInfo {
+            timestamp: 1637915608880,
+            target: difficult_to_target("0x109b3b25".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915598323,
+            target: difficult_to_target("0x107bac2f".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915593635,
+            target: difficult_to_target("0x0fc6aaf6".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915590037,
+            target: difficult_to_target("0x0e4573d9".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915589658,
+            target: difficult_to_target("0x0d9c454d".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915587412,
+            target: difficult_to_target("0x0d02072c".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915583955,
+            target: difficult_to_target("0x0c1a3c67".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915582180,
+            target: difficult_to_target("0x0b9f8d6c".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915578529,
+            target: difficult_to_target("0x0bb8e472".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915571549,
+            target: difficult_to_target("0x0b1d156a".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915569450,
+            target: difficult_to_target("0x0ad6bdde".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915565236,
+            target: difficult_to_target("0x0aa1d359".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915560700,
+            target: difficult_to_target("0x0a8a9c6b".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915555504,
+            target: difficult_to_target("0x0a55d2b8".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915551514,
+            target: difficult_to_target("0x09ff1eb6".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915548906,
+            target: difficult_to_target("0x0a9b7089".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915537703,
+            target: difficult_to_target("0x0afc8034".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915528782,
+            target: difficult_to_target("0x0b9f2116".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915518066,
+            target: difficult_to_target("0x0c551ecb".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915507321,
+            target: difficult_to_target("0x0bac0c9b".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915506330,
+            target: difficult_to_target("0x0b56064f".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915503277,
+            target: difficult_to_target("0x0ad8c0ab".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915501439,
+            target: difficult_to_target("0x0a6fc4c9".into()),
+        },
+        BlockDiffInfo {
+            timestamp: 1637915499540,
+            target: difficult_to_target("0x0ab72a30".into()),
+        },
+    ];
+
+    let next_target_1 = get_next_target_helper(blocks_1, 5962).unwrap();
+    // let difficulty = U256::from("0x0e74b15e");
+    let difficulty = U256::from("0f1ccd00");
+    let ttt: U256 = "0f1ccd00".into();
+
+    println!("{}", target_to_difficulty(next_target_1));
+    println!("{}", difficult_to_target(difficulty));
+    assert_eq!(target_to_difficulty(next_target_1), difficulty);
+}
