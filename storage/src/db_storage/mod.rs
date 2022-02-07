a7e7726aff1b98e75f4ec020091c26ae57ca0057
nk_ysg <nk_ysg@163.com>
2022-02-07 22:18:07 +0800

    update write buffer size

--- storage/src/db_storage/mod.rs
+++ storage/src/db_storage/mod.rs
@@ -193,7 +193,6 @@ impl DBStorage {
        // db_opts.enable_statistics();
         // write buffer size
         db_opts.set_max_write_buffer_number(5);
-        db_opts.set_write_buffer_size(128*1024*1024);
         db_opts.set_max_background_jobs(5);
         // cache
        // let cache = Cache::new_lru_cache(2 * 1024 * 1024 * 1024);
