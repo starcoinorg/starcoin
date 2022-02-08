7e44b6d327f1810998e92ca8a9600f1797e682f9
nk_ysg <nk_ysg@163.com>
2022-02-08 18:03:04 +0800

    update write buffer

--- storage/src/db_storage/mod.rs
+++ storage/src/db_storage/mod.rs
@@ -7,7 +7,7 @@ use crate::metrics::{record_metrics, StorageMetrics};
 use crate::storage::{ColumnFamilyName, InnerStore, KeyCodec, ValueCodec, WriteOp};
 use crate::{StorageVersion, DEFAULT_PREFIX_NAME};
 use anyhow::{ensure, format_err, Error, Result};
-use rocksdb::{Options, ReadOptions, WriteBatch as DBWriteBatch, WriteOptions, DB, Cache};
+use rocksdb::{Options, ReadOptions, WriteBatch as DBWriteBatch, WriteOptions, DB};
 use starcoin_config::RocksdbConfig;
 use std::collections::HashSet;
 use std::marker::PhantomData;
@@ -110,7 +110,8 @@ impl DBStorage {
             path,
             column_families.iter().map(|cf_name| {
                 let mut cf_opts = rocksdb::Options::default();
-                // cf_opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
+                cf_opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
+                /*
                 cf_opts.set_compression_per_level(&[
                     rocksdb::DBCompressionType::None,
                     rocksdb::DBCompressionType::None,
@@ -120,7 +121,7 @@ impl DBStorage {
                     rocksdb::DBCompressionType::Lz4,
                     rocksdb::DBCompressionType::Lz4,
                 ]);
-
+                */
                 rocksdb::ColumnFamilyDescriptor::new((*cf_name).to_string(), cf_opts)
             }),
         )?;
@@ -190,14 +191,13 @@ impl DBStorage {
         db_opts.set_max_total_wal_size(config.max_total_wal_size);
         db_opts.set_wal_bytes_per_sync(config.wal_bytes_per_sync);
         db_opts.set_bytes_per_sync(config.bytes_per_sync);
-       // db_opts.enable_statistics();
+        // db_opts.enable_statistics();
         // write buffer size
         db_opts.set_max_write_buffer_number(5);
         db_opts.set_max_background_jobs(5);
         // cache
-       // let cache = Cache::new_lru_cache(2 * 1024 * 1024 * 1024);
+        // let cache = Cache::new_lru_cache(2 * 1024 * 1024 * 1024);
         // db_opts.set_row_cache(&cache.unwrap());
-        // compress
         db_opts
     }
     fn iter_with_direction<K, V>(
