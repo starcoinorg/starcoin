59cfe240119d7679ffa0544550a62b15aa97b7b4
nk_ysg <nk_ysg@163.com>
2022-04-12 15:48:38 +0800

    add statetree  test
    1. add compare continue commit and batch flush test
    2. add remove test

--- state/state-tree/src/state_tree_test.rs
+++ state/state-tree/src/state_tree_test.rs
@@ -24,77 +24,156 @@ pub fn update_nibble(original_key: &HashValueKey, n: usize, nibble: u8) -> HashV
 }
 
 #[test]
-pub fn test_put_blob() -> Result<()> {
-    let s = MockStateNodeStore::new();
-    let state = StateTree::<HashValueKey>::new(Arc::new(s), None);
-    assert_eq!(state.root_hash(), *SPARSE_MERKLE_PLACEHOLDER_HASH);
+pub fn test_put_blob_continue_commit_flush_same() -> Result<()> {
+    let s1 = MockStateNodeStore::new();
+    let state1 = StateTree::<HashValueKey>::new(Arc::new(s1), None);
+    assert_eq!(state1.root_hash(), *SPARSE_MERKLE_PLACEHOLDER_HASH);
+
+    let s2 = MockStateNodeStore::new();
+    let state2 = StateTree::<HashValueKey>::new(Arc::new(s2), None);
+    assert_eq!(state2.root_hash(), *SPARSE_MERKLE_PLACEHOLDER_HASH);
 
     let hash_value = HashValue::random().into();
+    let account11 = update_nibble(&hash_value, 0, 1);
+    let account11 = update_nibble(&account11, 2, 2);
+    let account21 = account11.clone();
 
-    let account1 = update_nibble(&hash_value, 0, 1);
-    let account1 = update_nibble(&account1, 2, 2);
-    state.put(account1, vec![0, 0, 0]);
+    state1.put(account11, vec![0, 0, 0]);
+    assert_eq!(state1.get(&account11)?, Some(vec![0, 0, 0]));
+    assert_eq!(state1.get(&update_nibble(&hash_value, 0, 8))?, None);
 
-    assert_eq!(state.get(&account1)?, Some(vec![0, 0, 0]));
-    assert_eq!(state.get(&update_nibble(&hash_value, 0, 8))?, None);
+    state2.put(account21, vec![0, 0, 0]);
+    assert_eq!(state2.get(&account21)?, Some(vec![0, 0, 0]));
+    assert_eq!(state2.get(&update_nibble(&hash_value, 0, 8))?, None);
 
-    let new_root_hash = state.commit()?;
-    assert_eq!(state.root_hash(), new_root_hash);
-    assert_eq!(state.get(&account1)?, Some(vec![0, 0, 0]));
-    assert_eq!(state.get(&update_nibble(&hash_value, 0, 8))?, None);
+    let new_root_hash1 = state1.commit()?;
+    assert_eq!(state1.root_hash(), new_root_hash1);
+    assert_eq!(state1.get(&account11)?, Some(vec![0, 0, 0]));
+    assert_eq!(state1.get(&update_nibble(&hash_value, 0, 8))?, None);
 
-    let (root, updates) = state.last_change_sets().unwrap();
-    assert_eq!(root, new_root_hash);
-    assert_eq!(updates.num_stale_leaves, 0);
-    assert_eq!(updates.num_new_leaves, 1);
-    assert_eq!(updates.node_batch.len(), 1);
-    assert_eq!(updates.stale_node_index_batch.len(), 1);
+    let new_root_hash2 = state2.commit()?;
+    assert_eq!(state2.root_hash(), new_root_hash2);
+    assert_eq!(state2.get(&account11)?, Some(vec![0, 0, 0]));
+    assert_eq!(state2.get(&update_nibble(&hash_value, 0, 8))?, None);
 
-    let account2 = update_nibble(&account1, 0, 2);
-    state.put(account2, vec![0, 0, 0]);
-    assert_eq!(state.get(&account2)?, Some(vec![0, 0, 0]));
-    let new_root_hash = state.commit()?;
-    assert_eq!(state.root_hash(), new_root_hash);
-    assert_eq!(state.get(&account2)?, Some(vec![0, 0, 0]));
-    let (root, updates) = state.last_change_sets().unwrap();
-    assert_eq!(root, new_root_hash);
-    assert_eq!(updates.num_stale_leaves, 0);
-    assert_eq!(updates.num_new_leaves, 1);
-    assert_eq!(updates.node_batch.len(), 2);
-    assert_eq!(updates.stale_node_index_batch.len(), 0);
+    let (root1, updates1) = state1.last_change_sets().unwrap();
+    assert_eq!(root1, new_root_hash1);
+    assert_eq!(updates1.num_stale_leaves, 0);
+    assert_eq!(updates1.num_new_leaves, 1);
+    assert_eq!(updates1.node_batch.len(), 1);
+    assert_eq!(updates1.stale_node_index_batch.len(), 1);
+    state1.flush()?;
+
+    let (root2, updates2) = state2.last_change_sets().unwrap();
+    assert_eq!(root2, new_root_hash2);
+    assert_eq!(updates2.num_stale_leaves, 0);
+    assert_eq!(updates2.num_new_leaves, 1);
+    assert_eq!(updates2.node_batch.len(), 1);
+    assert_eq!(updates2.stale_node_index_batch.len(), 1);
+    assert_eq!(root1, root2);
+
+    let account12 = update_nibble(&account11, 0, 2);
+    state1.put(account12, vec![0, 0, 0]);
+    assert_eq!(state1.get(&account12)?, Some(vec![0, 0, 0]));
+    let new_root_hash1 = state1.commit()?;
+    assert_eq!(state1.root_hash(), new_root_hash1);
+    assert_eq!(state1.get(&account12)?, Some(vec![0, 0, 0]));
+    let (root1, updates1) = state1.last_change_sets().unwrap();
+    assert_eq!(root1, new_root_hash1);
+    assert_eq!(updates1.num_stale_leaves, 0);
+    assert_eq!(updates1.num_new_leaves, 1);
+    assert_eq!(updates1.node_batch.len(), 2);
+    assert_eq!(updates1.stale_node_index_batch.len(), 0);
+    state1.flush()?;
+
+    let account22 = update_nibble(&account21, 0, 2);
+    state2.put(account22, vec![0, 0, 0]);
+    assert_eq!(state2.get(&account12)?, Some(vec![0, 0, 0]));
+    let new_root_hash2 = state2.commit()?;
+    assert_eq!(state2.root_hash(), new_root_hash2);
+    assert_eq!(state2.get(&account22)?, Some(vec![0, 0, 0]));
+    let (root2, updates2) = state2.last_change_sets().unwrap();
+    assert_eq!(root2, new_root_hash2);
+    assert_eq!(updates2.num_stale_leaves, 0);
+    assert_eq!(updates2.num_new_leaves, 1);
+    assert_eq!(updates2.node_batch.len(), 2);
+    assert_eq!(updates2.stale_node_index_batch.len(), 0);
+    assert_eq!(root1, root2);
 
     // modify existed account
-    state.put(account1, vec![1, 1, 1]);
-    assert_eq!(state.get(&account1)?, Some(vec![1, 1, 1]));
-    let new_root_hash = state.commit()?;
-    assert_eq!(state.root_hash(), new_root_hash);
-    assert_eq!(state.get(&account1)?, Some(vec![1, 1, 1]));
-    let (root, updates) = state.last_change_sets().unwrap();
-    assert_eq!(root, new_root_hash);
-    assert_eq!(updates.num_stale_leaves, 1);
-    assert_eq!(updates.num_new_leaves, 1);
-    assert_eq!(updates.node_batch.len(), 2);
-    assert_eq!(updates.stale_node_index_batch.len(), 2);
+    state1.put(account11, vec![1, 1, 1]);
+    assert_eq!(state1.get(&account11)?, Some(vec![1, 1, 1]));
+    let new_root_hash1 = state1.commit()?;
+    assert_eq!(state1.root_hash(), new_root_hash1);
+    assert_eq!(state1.get(&account11)?, Some(vec![1, 1, 1]));
+    let (root1, updates1) = state1.last_change_sets().unwrap();
+    assert_eq!(root1, new_root_hash1);
+    assert_eq!(updates1.num_stale_leaves, 1);
+    assert_eq!(updates1.num_new_leaves, 1);
+    assert_eq!(updates1.node_batch.len(), 2);
+    assert_eq!(updates1.stale_node_index_batch.len(), 2);
+    state1.flush()?;
 
-    let account3 = update_nibble(&account1, 2, 3);
-    for (k, v) in vec![(account1, vec![1, 1, 0]), (account3, vec![0, 0, 0])] {
-        state.put(k, v);
+    // modify existed account
+    state2.put(account21, vec![1, 1, 1]);
+    assert_eq!(state2.get(&account21)?, Some(vec![1, 1, 1]));
+    let new_root_hash2 = state2.commit()?;
+    assert_eq!(state2.root_hash(), new_root_hash2);
+    assert_eq!(state2.get(&account21)?, Some(vec![1, 1, 1]));
+    let (root2, updates2) = state2.last_change_sets().unwrap();
+    assert_eq!(root2, new_root_hash2);
+    assert_eq!(updates2.num_stale_leaves, 1);
+    assert_eq!(updates2.num_new_leaves, 1);
+    assert_eq!(updates2.node_batch.len(), 2);
+    assert_eq!(updates2.stale_node_index_batch.len(), 2);
+
+    let account13 = update_nibble(&account11, 2, 3);
+    for (k, v) in vec![(account11, vec![1, 1, 0]), (account13, vec![0, 0, 0])] {
+        state1.put(k, v);
     }
-    assert_eq!(state.get(&account1)?, Some(vec![1, 1, 0]));
-    assert_eq!(state.get(&account2)?, Some(vec![0, 0, 0]));
-    assert_eq!(state.get(&account3)?, Some(vec![0, 0, 0]));
+    assert_eq!(state1.get(&account11)?, Some(vec![1, 1, 0]));
+    assert_eq!(state1.get(&account12)?, Some(vec![0, 0, 0]));
+    assert_eq!(state1.get(&account13)?, Some(vec![0, 0, 0]));
+    let new_root_hash1 = state1.commit()?;
+    assert_eq!(state1.root_hash(), new_root_hash1);
+    assert_eq!(state1.get(&account11)?, Some(vec![1, 1, 0]));
+    assert_eq!(state1.get(&account12)?, Some(vec![0, 0, 0]));
+    assert_eq!(state1.get(&account13)?, Some(vec![0, 0, 0]));
+    let (root1, updates1) = state1.last_change_sets().unwrap();
+    assert_eq!(updates1.num_stale_leaves, 1);
+    assert_eq!(updates1.num_new_leaves, 2);
+    assert_eq!(updates1.node_batch.len(), 5);
+    assert_eq!(updates1.stale_node_index_batch.len(), 2);
 
-    let new_root_hash = state.commit()?;
-    assert_eq!(state.root_hash(), new_root_hash);
-    assert_eq!(state.get(&account1)?, Some(vec![1, 1, 0]));
-    assert_eq!(state.get(&account2)?, Some(vec![0, 0, 0]));
-    assert_eq!(state.get(&account3)?, Some(vec![0, 0, 0]));
+    let account23 = update_nibble(&account21, 2, 3);
+    for (k, v) in vec![(account21, vec![1, 1, 0]), (account23, vec![0, 0, 0])] {
+        state2.put(k, v);
+    }
+    assert_eq!(state2.get(&account21)?, Some(vec![1, 1, 0]));
+    assert_eq!(state2.get(&account22)?, Some(vec![0, 0, 0]));
+    assert_eq!(state2.get(&account23)?, Some(vec![0, 0, 0]));
+    let new_root_hash2 = state2.commit()?;
+    assert_eq!(state2.root_hash(), new_root_hash2);
+    assert_eq!(state2.get(&account11)?, Some(vec![1, 1, 0]));
+    assert_eq!(state2.get(&account12)?, Some(vec![0, 0, 0]));
+    assert_eq!(state2.get(&account13)?, Some(vec![0, 0, 0]));
+    let (root2, updates2) = state2.last_change_sets().unwrap();
+    assert_eq!(updates2.num_stale_leaves, 1);
+    assert_eq!(updates2.num_new_leaves, 2);
+    assert_eq!(updates2.node_batch.len(), 5);
+    assert_eq!(updates2.stale_node_index_batch.len(), 2);
+    assert_eq!(root1, root2);
 
-    let (_, updates) = state.last_change_sets().unwrap();
-    assert_eq!(updates.num_stale_leaves, 1);
-    assert_eq!(updates.num_new_leaves, 2);
-    assert_eq!(updates.node_batch.len(), 5);
-    assert_eq!(updates.stale_node_index_batch.len(), 2);
+    // test delete account
+    state1.remove(&account11);
+    state1.commit()?;
+    state1.flush()?;
+    assert_eq!(state1.get(&account11)?, None);
+
+    state2.remove(&account21);
+    state2.commit()?;
+    assert_eq!(state2.get(&account21)?, None);
+
     Ok(())
 }
 
