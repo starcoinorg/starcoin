8bee59b8a6f255d5afcf15e96ac8810669fffc6f
LemonHX.eth <lemonhx@lemonhx.tech>
2022-04-15 13:29:24 +0800

    [Dev] add powershell for setup (#3343)
    * add powershell for setup
    testing: @WGB5445

--- scripts/install_llvm.ps1
+++
@@ -1,8 +0,0 @@
-$llvmVersion = "12.0.0"
-Write-Host "Installing LLVM $llvmVersion ..." -ForegroundColor Cyan
-Write-Host "Downloading..."
-$exePath = "$env:temp\LLVM-$llvmVersion-win64.exe"
-(New-Object Net.WebClient).DownloadFile("https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvmVersion/LLVM-$llvmVersion-win64.exe", $exePath)
-Write-Host "Installing..."
-cmd /c start /wait $exePath /S
-Write-Host "Installed" -ForegroundColor Green
