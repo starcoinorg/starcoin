49221d46e892d879e2ed1517a41575258d254c32
jiangying <krivergo3@gmail.com>
2022-08-31 12:50:50 +0800

    fix

--- rpc/api/src/tests.rs
+++ rpc/api/src/tests.rs
@@ -55,7 +55,7 @@ fn assert_that_version_control_has_no_unstaged_changes() {
 }
 
 // temporally disable cov, need to be enabled
-#[cfg_attr(coverage, no_coverage)]
+#[cfg_attr(coverage_nightly, no_coverage)]
 #[test]
 fn test_generated_schema_are_up_to_date_in_git() {
     // Better not run the `stdlib` tool when the repository is not in a clean state.
