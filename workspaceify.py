fa4691033f8095978b3ec6efdc5f1128f61a9a0e
welbon <libo.weng@gmail.com>
2023-01-04 15:42:05 +0800

    commit scripts

--- workspaceify.py
+++ workspaceify.py
@@ -1,3 +1,5 @@
+import collections
+import copy
 from abc import abstractmethod
 
 import toml
@@ -10,41 +12,50 @@ def merge_two_dicts(x, y):
     return z
 
 
-def normalized_relative_path(work_dir, input_path):
+def normalized_relative_path(input_path):
     """
-    :param self:
-    :param work_dir:
     :param input_path:
     :return: new path relative workdir
     """
-    return input_path
+
+    return input_path.replace('../', '')
 
 
 class TomlObj:
-    def __init__(self, file_path):
+    def __init__(self, work_dir, file_path):
         self.toml_dict = dict()
         self.file_path = file_path
+        self.work_dir = work_dir
 
     def load(self):
-        self.toml_dict = toml.load(self.file_path)
+        self.toml_dict = toml.load(self.file_path, decoder=toml.decoder.TomlDecoder())
 
     def save(self):
         with open(self.file_path, mode="w") as f:
-            toml.dump(self.toml_dict, f, encoder=toml.TomlPreserveInlineDictEncoder())
+            toml.dump(collections.OrderedDict(sorted(self.toml_dict.items(), key=lambda item: item[0])),
+                      f,
+                      encoder=toml.TomlPreserveInlineDictEncoder())
+        # print("Save filepath: " + self.file_path)
+        # print(toml.dumps(self.toml_dict, encoder=toml.TomlPreserveInlineDictEncoder()))
 
-    def get_origin_dependencies(self, work_dir, is_dev=False):
+    def get_item(self, key):
+        return self.toml_dict[key]
+
+    def get_origin_dependencies(self, is_dev=False):
         depends = self.get_dependencies(is_dev)
         origin_depends = dict()
+        if depends is None:
+            return dict()
+
         # Ignore dependencies item with workspace
         for i, (k, v) in enumerate(depends.items()):
-            if isinstance(v, dict) and 'workspace' in v:
-                continue
-
-            if k == 'path':
-                origin_depends['path'] = normalized_relative_path(work_dir, v)
-                continue
-
-            origin_depends[k] = v
+            new_v = v
+            if isinstance(new_v, dict):
+                if 'workspace' in new_v:
+                    continue
+                if 'path' in new_v:
+                    new_v['path'] = normalized_relative_path(new_v['path'])
+            origin_depends[k] = new_v
         return origin_depends
 
     @abstractmethod
@@ -60,6 +71,9 @@ class TomlObj:
             return
 
         dependencies = self.get_dependencies()
+        if dependencies is None:
+            return
+
         new_dependencies = merge_two_dicts(dependencies, input_depends)
         self.put_dependencies(new_dependencies)
 
@@ -107,6 +121,9 @@ class TomlObj:
 
     def replace_dependencies(self, is_dev=False):
         depends = self.get_dependencies(is_dev)
+        if depends is None:
+            return
+
         decoder = toml.TomlDecoder()
         dict_key = ['path', 'version', 'git', 'rev']
         for i, (k, v) in enumerate(depends.items()):
@@ -125,44 +142,113 @@ class TomlObj:
                 inline_table['workspace'] = True
                 depends[k] = inline_table
 
+    @abstractmethod
+    def replace_fake_package_name(self):
+        pass
+
 
 class RootCargoToml(TomlObj):
-    def __init__(self, file_path):
-        super().__init__(file_path)
+    def __init__(self, work_dir, file_path):
+        super().__init__(work_dir, file_path)
 
     def get_dependencies(self, is_dev=False):
+        if 'workspace' not in self.toml_dict:
+            self.toml_dict['workspace'] = dict()
+
+        if 'dependencies' not in self.toml_dict['workspace']:
+            self.toml_dict['workspace'] = dict()
+
         return self.toml_dict['workspace']['dependencies']
 
     def put_dependencies(self, depends):
-        self.toml_dict['workspace']['dependencies'] = depends
+        self.toml_dict['workspace']['dependencies'] = copy.deepcopy(depends)
+
+    def replace_fake_package_name(self):
+        depends = self.get_dependencies()
+        remove_keys = []
+        for i, (k, v) in enumerate(depends.items()):
+            if isinstance(v, dict) and 'path' in v:
+                path = v['path']
+                subtoml = SubCargoToml(self.work_dir,
+                                       self.work_dir + path + "/Cargo.toml")
+                subtoml.load()
+                real_name = subtoml.get_item('package')['name']
+                if real_name != k and real_name in depends:
+                    remove_keys.append(k)
+
+        for k in remove_keys:
+            del depends[k]
 
 
 class SubCargoToml(TomlObj):
-    def __init__(self, file_path):
-        super().__init__(file_path)
+    def __init__(self, work_dir, file_path):
+        super().__init__(work_dir, file_path)
 
     def get_dependencies(self, is_dev=False):
-        return self.toml_dict['dependencies' if is_dev is False else 'dev-dependencies']
+        key = 'dependencies' if is_dev is False else 'dev-dependencies'
+        return self.toml_dict[key] if key in self.toml_dict else None
 
     def put_dependencies(self, depends):
-        self.toml_dict['dependencies'] = depends
+        self.toml_dict['dependencies'] = copy.deepcopy(depends)
+
+    def replace_fake_package_name(self):
+        decoder = toml.decoder.TomlDecoder()
+        add_dicts = dict()
+        remove_keys = []
+        depends = self.get_dependencies()
+        for i, (k, v) in enumerate(depends.items()):
+            if isinstance(v, dict) and 'package' in v:
+                new_key = v['package']
+                if k == new_key:
+                    continue
+
+                inline_table = decoder.get_empty_inline_table()
+                inline_table['workspace'] = True
+                add_dicts[new_key] = inline_table
+                remove_keys.append(k)
+        for rk in remove_keys:
+            del depends[rk]
+
+
+def replace_fake_package_names_sub_proj(root_toml, work_dir):
+    root_workspace = root_toml.get_item('workspace')
+    members = root_workspace["members"]
+    for m in members:
+        sub_toml = SubCargoToml(work_dir, work_dir + m + "/Cargo.toml")
+        sub_toml.load()
+        sub_toml.replace_fake_package_name()
+        sub_toml.save()
 
 
 def main(work_dir):
-    root_toml = RootCargoToml(work_dir + "/Cargo.toml")
+    root_toml = RootCargoToml(work_dir, work_dir + "Cargo.toml")
     root_toml.load()
 
-    test_toml = SubCargoToml(work_dir + "/consensus/Cargo.toml")
-    test_toml.load()
-    root_toml.combine_dependencies(test_toml.get_origin_dependencies(work_dir, is_dev=False))
-    root_toml.combine_dependencies(test_toml.get_origin_dependencies(work_dir, is_dev=True))
-    test_toml.replace_package()
-    test_toml.replace_dependencies(is_dev=False)
-    test_toml.replace_dependencies(is_dev=True)
-    test_toml.save()
+    replace_fake_package_names_sub_proj(root_toml, work_dir)
+
+    # root_workspace = root_toml.get_item('workspace')
+    # members = root_workspace["members"]
+    # for m in members:
+    #     sub_cargo_item = work_dir + m + "/Cargo.toml"
+    #     test_toml = SubCargoToml(sub_cargo_item)
+    #     test_toml.load()
+    #
+    #     release_dep = test_toml.get_origin_dependencies(is_dev=False)
+    #     root_toml.combine_dependencies(release_dep)
+    #     debug_dep = test_toml.get_origin_dependencies(is_dev=True)
+    #     root_toml.combine_dependencies(debug_dep)
+    #
+    #     test_toml.replace_package()
+    #     test_toml.replace_dependencies(is_dev=False)
+    #     test_toml.replace_dependencies(is_dev=True)
+    #     test_toml.save()
 
     # Save root toml
-    root_toml.save()
+    # sorted_dict = collections.OrderedDict(sorted(root_workspace['dependencies'].items(), key=lambda item: item[0]))
+    # trim_repeatly_items(sorted_dict, work_dir)
+    # root_workspace['dependencies'] = sorted_dict
+
+    # root_toml.save()
 
 
 if __name__ == "__main__":
