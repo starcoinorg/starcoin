19fbef1cf5e4aee94a521c29dd7a9ffb0731d98a
jackzhhuang <jackzhhuang@gmail.com>
2023-04-05 01:55:55 +0800

    fix some type styles

--- scripts/upload_file_any_size.py
+++ scripts/upload_file_any_size.py
@@ -3,7 +3,7 @@ from botocore.exceptions import ClientError
 import os
 import sys
         
-per_uploading = 104857600 ## 100 MB per uploading
+PER_UPLOADING = 104857600 ## up to 100 MB per uploading action
 
 def upload_file(file_name, bucket, key):
     object_name = os.path.basename(file_name)
@@ -25,7 +25,7 @@ def upload_file_big_file(file_name, bucket):
         response = client.create_multipart_upload(Bucket = bucket, Key = object_name)
         upload_id = response["UploadId"]
         file_obj = open(file_name, "rb")
-        content = file_obj.read(per_uploading) 
+        content = file_obj.read(PER_UPLOADING) 
         count = 1
         parts = []
         while content:
@@ -33,7 +33,7 @@ def upload_file_big_file(file_name, bucket):
             response = client.upload_part(Body = content, Bucket = bucket, Key = object_name, PartNumber = count, UploadId = upload_id)
             parts.append({'ETag': response["ETag"], 'PartNumber': count})
             count += 1
-            content = file_obj.read(per_uploading) ## 100 MB per uploading
+            content = file_obj.read(PER_UPLOADING)
         response = client.complete_multipart_upload(Bucket = bucket, Key = object_name, MultipartUpload = {'Parts': parts,}, UploadId = upload_id)
         print(str(response))
     except ClientError as e:
@@ -43,7 +43,7 @@ def upload_file_big_file(file_name, bucket):
 
 def upload_file_any_size(file_name, bucket, key):
     file_size = os.path.getsize(file_name)
-    if file_size > per_uploading:
+    if file_size > PER_UPLOADING:
         upload_file_big_file(file_name, bucket, key)
     else:
         upload_file(file_name, bucket, key)
@@ -52,11 +52,11 @@ def create_bucket(bucket_name, region=None):
     try:
         if region is None:
             s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
+            s3_client.create_bucket(Bucket = bucket_name)
         else:
-            s3_client = boto3.client('s3', region_name=region)
+            s3_client = boto3.client('s3', region_name = region)
             location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
+            s3_client.create_bucket(Bucket = bucket_name,
                                     CreateBucketConfiguration=location)
     except ClientError as e:
         print(str(e))
@@ -83,7 +83,7 @@ def main():
     test_file = sys.argv[3]
 
     upload_file_any_size(test_file, bucket_name, key)
-    list_bucket(bucket_name, 10)
+    # list_bucket(bucket_name, 10)
 
 if __name__ == '__main__':
     main()
