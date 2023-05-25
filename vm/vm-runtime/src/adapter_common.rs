e547edfbe8eb7eb63a103151af03b51b211a80a2
nk_ysg <nk_ysg@163.com>
2023-05-25 18:53:07 +0800

    update vm-runtime

--- vm/vm-runtime/src/adapter_common.rs
+++ vm/vm-runtime/src/adapter_common.rs
@@ -1,35 +1,26 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{counters::*, data_cache::StateViewCache};
-use anyhow::Result;
-use starcoin_state_view::StateView;
-use aptos_types::{
-    transaction::{SignatureCheckedTransaction, SignedTransaction, VMValidatorResult},
-    vm_status::{StatusCode, VMStatus},
-};
+/// XXX FIXME YSG notice SignedUserTransaction is Aptos SignedTransaction
+use crate::{data_cache::{AsMoveResolver, StateViewCache}, move_vm_ext::{SessionId, MoveResolverExt}};
 use move_core_types::resolver::MoveResolver;
-
-use crate::{
-    data_cache::AsMoveResolver,
-    logging::AdapterLogSchema,
-    move_vm_ext::{SessionExt, SessionId},
-};
+use move_core_types::vm_status::{StatusCode, VMStatus};
+use move_vm_runtime::session::Session;
+use rayon::prelude::*;
 use starcoin_logger::prelude::*;
+use starcoin_vm_types::state_view::StateView;
 use starcoin_vm_types::{
     access_path::AccessPath,
     block_metadata::BlockMetadata,
     state_store::state_key::StateKey,
     transaction::{
-        Transaction, TransactionArgument, TransactionOutput, TransactionPayload, TransactionStatus,
-        WriteSetPayload,SignedUserTransaction,
+        SignatureCheckedTransaction, SignedUserTransaction, Transaction, TransactionArgument,
+        TransactionOutput, TransactionPayload, TransactionStatus, WriteSetPayload,
     },
     write_set::WriteSet,
 };
-use rayon::prelude::*;
 use std::collections::HashSet;
-use move_core_types::vm_status::{StatusCode, VMStatus};
-use starcoin_vm_types::state_view::StateView;
+use anyhow::Result;
 
 /// This trait describes the VM adapter's interface.
 /// TODO: bring more of the execution logic in aptos_vm into this file.
@@ -37,46 +28,33 @@ pub trait VMAdapter {
     /// Creates a new Session backed by the given storage.
     /// TODO: this doesn't belong in this trait. We should be able to remove
     /// this after redesigning cache ownership model.
-    fn new_session<'r, R: MoveResolver>(
+    fn new_session<'r, R: MoveResolverExt>(
         &self,
         remote: &'r R,
         session_id: SessionId,
-    ) -> SessionExt<'r, '_, R>;
+        // XXX FIXME YSG, this place we use Session, we don't have move_vm_ext::SessionExt
+    ) -> Session<'r, '_, R>;
 
     /// Checks the signature of the given signed transaction and returns
     /// `Ok(SignatureCheckedTransaction)` if the signature is valid.
-    fn check_signature(txn: SignedTransaction) -> Result<SignatureCheckedTransaction>;
-
-    /// Check if the transaction format is supported.
-    fn check_transaction_format(&self, txn: &SignedTransaction) -> Result<(), VMStatus>;
-
-    /// Get the gas price for the given transaction.
-    /// TODO: remove this after making mempool interface more generic so
-    /// it ranks transactions using a provided ranker that implements PartialOrd
-    /// instead of using governance roles and gas prices.
-    fn get_gas_price<S: MoveResolver>(
-        &self,
-        txn: &SignedTransaction,
-        remote_cache: &S,
-    ) -> Result<u64, VMStatus>;
+    fn check_signature(txn: SignedUserTransaction) -> Result<SignatureCheckedTransaction>;
 
     /// Runs the prologue for the given transaction.
-    fn run_prologue<S: MoveResolver>(
+    fn run_prologue<S: MoveResolverExt>(
         &self,
-        session: &mut SessionExt<S>,
+        // XXX FIXME YSG, this place we use Session, we don't have move_vm_ext::SessionExt
+        session: &mut Session<S>,
         transaction: &SignatureCheckedTransaction,
-        log_context: &AdapterLogSchema,
     ) -> Result<(), VMStatus>;
 
     /// TODO: maybe remove this after more refactoring of execution logic.
     fn should_restart_execution(output: &TransactionOutput) -> bool;
 
     /// Execute a single transaction.
-    fn execute_single_transaction<S: MoveResolver + StateView>(
+    fn execute_single_transaction<S: MoveResolverExt + StateView>(
         &self,
         txn: &PreprocessedTransaction,
         data_cache: &S,
-        log_context: &AdapterLogSchema,
     ) -> Result<(VMStatus, TransactionOutput, Option<String>), VMStatus>;
 }
 
@@ -88,11 +66,9 @@ pub trait VMAdapter {
 /// and `Some(DiscardedVMStatus)` otherwise.
 pub fn validate_signed_transaction<A: VMAdapter>(
     adapter: &A,
-    transaction: SignedTransaction,
+    transaction: SignedUserTransaction,
     state_view: &impl StateView,
 ) -> VMValidatorResult {
-    let _timer = TXN_VALIDATION_SECONDS.start_timer();
-    let log_context = AdapterLogSchema::new(state_view.id(), 0);
 
     let txn = match A::check_signature(transaction) {
         Ok(t) => t,
@@ -111,32 +87,28 @@ pub fn validate_signed_transaction<A: VMAdapter>(
     };
 
     let validation_result =
-        validate_signature_checked_transaction(adapter, &mut session, &txn, true, &log_context);
-
-    let (status, gas_price) = match (status, validation_result) {
-        (Some(_), _) => (status, 0),
-        (None, Ok(())) => (None, gas_price),
-        (None, Err(err)) => (Some(err.status_code()), 0),
-    };
+        validate_signature_checked_transaction(adapter, &mut session, &txn, true);
 
     // Increment the counter for transactions verified.
-    let counter_label = match status {
-        None => "success",
-        Some(_) => "failure",
+    let (_counter_label, result) = match validation_result {
+        Ok(_) => (
+            "success",
+            VMValidatorResult::new(None, txn.gas_unit_price()),
+        ),
+        Err(err) => (
+            "failure",
+            VMValidatorResult::new(Some(err.status_code()), 0),
+        ),
     };
-    TRANSACTIONS_VALIDATED
-        .with_label_values(&[counter_label])
-        .inc();
-
-    VMValidatorResult::new(status, gas_price)
+    result
 }
 
-pub(crate) fn validate_signature_checked_transaction<S: MoveResolver, A: VMAdapter>(
+pub(crate) fn validate_signature_checked_transaction<S: MoveResolverExt, A: VMAdapter>(
     adapter: &A,
-    session: &mut SessionExt<S>,
+    // XXX FIXME YSG, this place we use Session, we don't have move_vm_ext::SessionExt
+    session: &mut Session<S>,
     transaction: &SignatureCheckedTransaction,
     allow_too_new: bool,
-    log_context: &AdapterLogSchema,
 ) -> Result<(), VMStatus> {
     adapter.check_transaction_format(transaction)?;
 
@@ -187,7 +159,6 @@ pub(crate) fn execute_block_impl<A: VMAdapter, S: StateView>(
     let mut should_restart = false;
 
     info!(
-        AdapterLogSchema::new(data_cache.id(), 0),
         "Executing block, transaction count: {}",
         transactions.len()
     );
@@ -210,7 +181,6 @@ pub(crate) fn execute_block_impl<A: VMAdapter, S: StateView>(
     });
 
     for (idx, txn) in signature_verified_block.into_iter().enumerate() {
-        let log_context = AdapterLogSchema::new(data_cache.id(), idx);
         if should_restart {
             let txn_output =
                 TransactionOutput::new(WriteSet::default(), vec![], 0, TransactionStatus::Retry);
@@ -221,7 +191,6 @@ pub(crate) fn execute_block_impl<A: VMAdapter, S: StateView>(
         let (vm_status, output, sender) = adapter.execute_single_transaction(
             &txn,
             &data_cache.as_move_resolver(),
-            &log_context,
         )?;
         if !output.status().is_discarded() {
             data_cache.push_write_set(output.write_set());
@@ -239,7 +208,6 @@ pub(crate) fn execute_block_impl<A: VMAdapter, S: StateView>(
 
         if A::should_restart_execution(&output) {
             info!(
-                AdapterLogSchema::new(data_cache.id(), 0),
                 "Reconfiguration occurred: restart required",
             );
             should_restart = true;
@@ -258,7 +226,7 @@ pub(crate) fn execute_block_impl<A: VMAdapter, S: StateView>(
 /// but a user transaction transformed to a SignatureCheckedTransaction.
 #[derive(Debug)]
 pub enum PreprocessedTransaction {
-    UserTransaction(Box<SignedUserTransaction>),
+    UserTransaction(Box<SignatureCheckedTransaction>),
     BlockMetadata(BlockMetadata),
 }
 
