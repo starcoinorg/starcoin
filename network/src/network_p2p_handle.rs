20aaf517b83239dfae611dd97dc8b84d131f3442
jackzhhuang <jackzhhuang@gmail.com>
2023-05-26 00:21:34 +0800

    process the unwrap

--- network/src/network_p2p_handle.rs
+++ network/src/network_p2p_handle.rs
@@ -1,5 +1,6 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
+use anyhow::anyhow;
 use bcs_ext::BCSCodec;
 use network_p2p_types::business_layer_handle::BusinessLayerHandle;
 use starcoin_types::startup_info::{ChainInfo, ChainStatus};
@@ -16,32 +17,70 @@ impl Networkp2pHandle {
 
 impl BusinessLayerHandle for Networkp2pHandle {
     fn handshake(&self, peer_info: &[u8]) -> Result<(), (&'static str, String)> {
-        let other_chain_info = ChainInfo::decode(peer_info).unwrap();
-        if self.chain_info.genesis_hash() == other_chain_info.genesis_hash() {
-            return std::result::Result::Ok(());
+        match ChainInfo::decode(peer_info) {
+            Ok(other_chain_info) => {
+                if self.chain_info.genesis_hash() == other_chain_info.genesis_hash() {
+                    std::result::Result::Ok(())
+                } else {
+                    return Err((
+                        "the genesis hash is different",
+                        format!(
+                            "the genesis hash from other peer is different, self: {}, remote: {}",
+                            self.chain_info.genesis_hash(),
+                            other_chain_info.genesis_hash()
+                        ),
+                    ));
+                }
+            }
+            Err(error) => {
+                return Err((
+                    "failed to decode the generic data",
+                    format!(
+                        "failed to decode the generic data for the reason: {}",
+                        error
+                    ),
+                ))
+            }
         }
-        return Err((
-            "the genesis hash is different",
-            format!(
-                "the genesis hash from other peer is different, self: {}, remote: {}",
-                self.chain_info.genesis_hash(),
-                other_chain_info.genesis_hash()
-            ),
-        ));
     }
 
     fn get_generic_data(&self) -> Result<Vec<u8>, anyhow::Error> {
-        Ok(self.chain_info.encode().unwrap())
+        match self.chain_info.encode() {
+            Ok(generic) => Ok(generic),
+            Err(error) => Err(anyhow!(format!(
+                "failed to encode chain info for the reason: {}",
+                error
+            ))),
+        }
     }
 
     fn update_generic_data(&mut self, peer_info: &[u8]) -> Result<(), anyhow::Error> {
-        self.chain_info = ChainInfo::decode(peer_info).unwrap();
-        Ok(())
+        match ChainInfo::decode(peer_info) {
+            Ok(other_chain_info) => {
+                self.chain_info = other_chain_info;
+                Ok(())
+            }
+            Err(error) => {
+                return Err(anyhow!(
+                    "failed to decode the generic data for the reason: {}",
+                    error
+                ))
+            }
+        }
     }
 
     fn update_status(&mut self, peer_status: &[u8]) -> Result<(), anyhow::Error> {
-        self.chain_info
-            .update_status(ChainStatus::decode(peer_status).unwrap());
-        Ok(())
+        match ChainStatus::decode(peer_status) {
+            Ok(status) => {
+                self.chain_info.update_status(status);
+                Ok(())
+            }
+            Err(error) => {
+                return Err(anyhow!(
+                    "failed to decode the generic data for the reason: {}",
+                    error
+                ))
+            }
+        }
     }
 }
