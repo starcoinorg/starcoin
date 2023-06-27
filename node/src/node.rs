84c3b69f808222269bf317f1016a3bbe76943b92
nk_ysg <nk_ysg@163.com>
2023-06-27 13:29:35 +0800

    set concurrency level

--- node/src/node.rs
+++ node/src/node.rs
@@ -234,7 +234,7 @@ impl NodeService {
         }
 
         // XXX FIXME YSG add execute_config
-        StarcoinVM::set_concurrency_level_once(4);
+        StarcoinVM::set_concurrency_level_once(1);
         let (start_sender, start_receiver) = oneshot::channel();
         let join_handle = timeout_join_handler::spawn(move || {
             let system = System::with_tokio_rt(|| {
