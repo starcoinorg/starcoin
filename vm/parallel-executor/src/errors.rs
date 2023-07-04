f10341231101573eb33dfadb7a2074c0ae2d03c5
nk_ysg <nk_ysg@163.com>
2023-07-04 17:25:47 +0800

    fix should_restart_execution logic

--- vm/parallel-executor/src/errors.rs
+++ vm/parallel-executor/src/errors.rs
@@ -6,14 +6,11 @@ pub enum Error<E> {
     /// Invariant violation that happens internally inside of scheduler, usually an indication of
     /// implementation error.
     InvariantViolation,
-    /// The inference can't get the read/write set of a transaction, abort the entire execution pipeline.
-    InferencerError,
-    /// A transaction write to a key that wasn't estimated by the inferencer, abort the execution
-    /// because we don't have a good way of handling read-after-write dependency. Will relax this limitation later.
-    UnestimatedWrite,
     /// Execution of a thread yields a non-recoverable error, such error will be propagated back to
     /// the caller.
     UserError(E),
+
+    BlockRestart,
 }
 
 pub type Result<T, E> = ::std::result::Result<T, Error<E>>;
