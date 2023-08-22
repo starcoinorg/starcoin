7e281e01343e4ec803fc0059f49d086d100a0b64
simonjiao <simonjiaoh@gmail.com>
2023-08-22 15:43:14 +0800

    move iterator to single file

--- storage/src/db_storage/mod.rs
+++ storage/src/db_storage/mod.rs
@@ -1,20 +1,23 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+mod iterator;
+
+pub use iterator::*;
+
 use crate::{
     batch::WriteBatch,
     errors::StorageInitError,
     metrics::{record_metrics, StorageMetrics},
-    storage::{ColumnFamilyName, InnerStore, KeyCodec, RawDBStorage, ValueCodec, WriteOp},
+    storage::{ColumnFamilyName, InnerStore, KeyCodec, ValueCodec, WriteOp},
     StorageVersion, DEFAULT_PREFIX_NAME,
 };
 use anyhow::{ensure, format_err, Error, Result};
 use rocksdb::{
-    DBIterator, DBPinnableSlice, IteratorMode, Options, ReadOptions, WriteBatch as DBWriteBatch,
-    WriteOptions, DB,
+    DBIterator, IteratorMode, Options, ReadOptions, WriteBatch as DBWriteBatch, WriteOptions, DB,
 };
 use starcoin_config::{check_open_fds_limit, RocksdbConfig};
-use std::{collections::HashSet, iter, marker::PhantomData, path::Path};
+use std::{collections::HashSet, iter, path::Path};
 
 const RES_FDS: u64 = 4096;
 
@@ -304,87 +307,6 @@ impl DBStorage {
     }
 }
 
-pub enum ScanDirection {
-    Forward,
-    Backward,
-}
-
-pub struct SchemaIterator<'a, K, V> {
-    db_iter: rocksdb::DBRawIterator<'a>,
-    direction: ScanDirection,
-    phantom_k: PhantomData<K>,
-    phantom_v: PhantomData<V>,
-}
-
-impl<'a, K, V> SchemaIterator<'a, K, V>
-where
-    K: KeyCodec,
-    V: ValueCodec,
-{
-    fn new(db_iter: rocksdb::DBRawIterator<'a>, direction: ScanDirection) -> Self {
-        SchemaIterator {
-            db_iter,
-            direction,
-            phantom_k: PhantomData,
-            phantom_v: PhantomData,
-        }
-    }
-
-    /// Seeks to the first key.
-    pub fn seek_to_first(&mut self) {
-        self.db_iter.seek_to_first();
-    }
-
-    /// Seeks to the last key.
-    pub fn seek_to_last(&mut self) {
-        self.db_iter.seek_to_last();
-    }
-
-    /// Seeks to the first key whose binary representation is equal to or greater than that of the
-    /// `seek_key`.
-    pub fn seek(&mut self, seek_key: Vec<u8>) -> Result<()> {
-        self.db_iter.seek(&seek_key);
-        Ok(())
-    }
-
-    /// Seeks to the last key whose binary representation is less than or equal to that of the
-    /// `seek_key`.
-    pub fn seek_for_prev(&mut self, seek_key: Vec<u8>) -> Result<()> {
-        self.db_iter.seek_for_prev(&seek_key);
-        Ok(())
-    }
-
-    fn next_impl(&mut self) -> Result<Option<(K, V)>> {
-        if !self.db_iter.valid() {
-            self.db_iter.status()?;
-            return Ok(None);
-        }
-
-        let raw_key = self.db_iter.key().expect("Iterator must be valid.");
-        let raw_value = self.db_iter.value().expect("Iterator must be valid.");
-        let key = K::decode_key(raw_key)?;
-        let value = V::decode_value(raw_value)?;
-        match self.direction {
-            ScanDirection::Forward => self.db_iter.next(),
-            ScanDirection::Backward => self.db_iter.prev(),
-        }
-
-        Ok(Some((key, value)))
-    }
-}
-
-impl<'a, K, V> Iterator for SchemaIterator<'a, K, V>
-where
-    K: KeyCodec,
-    V: ValueCodec,
-{
-    type Item = Result<(K, V)>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_impl().transpose()
-    }
-}
-
 impl InnerStore for DBStorage {
     fn get(&self, prefix_name: &str, key: Vec<u8>) -> Result<Option<Vec<u8>>> {
         record_metrics("db", prefix_name, "get", self.metrics.as_ref()).call(|| {
@@ -486,24 +408,5 @@ impl InnerStore for DBStorage {
             }
             Ok(res)
         })
-    }
-}
-
-impl RawDBStorage for DBStorage {
-    fn raw_get_pinned_cf<K: AsRef<[u8]>>(
-        &self,
-        prefix: &str,
-        key: K,
-    ) -> Result<Option<DBPinnableSlice>> {
-        let cf = self.get_cf_handle(prefix)?;
-        let res = self
-            .db
-            .get_pinned_cf_opt(cf, key, &ReadOptions::default())?;
-        Ok(res)
-    }
-
-    fn raw_write_batch(&self, batch: DBWriteBatch) -> Result<()> {
-        self.db.write(batch)?;
-        Ok(())
     }
 }
