7e2b3dcf032621366ba2e5d0615f11ef2a5e9856
welbon <libo.weng@gmail.com>
2023-09-07 19:10:44 +0800

    [benchmark] Add flamegraph config for transaction_benches

--- vm/transaction-benchmarks/benches/transaction_benches.rs
+++ vm/transaction-benchmarks/benches/transaction_benches.rs
@@ -1,18 +1,21 @@
 // Copyright (c) Starcoin
 // SPDX-License-Identifier: Apache-2.0
 
+use std::fs::File;
 use criterion::{criterion_group, criterion_main, measurement::Measurement, Criterion};
 use proptest::prelude::*;
 use starcoin_language_e2e_tests::account_universe::P2PTransferGen;
 use starcoin_transaction_benchmarks::{
-    measurement::wall_time_measurement, transactions::TransactionBencher,
+    transactions::TransactionBencher,
 };
+use pprof::criterion::{Output, PProfProfiler};
 
 //
 // Transaction benchmarks
 //
+
 fn peer_to_peer<M: Measurement + 'static>(c: &mut Criterion<M>) {
-    let default_num_accounts = 10_000;
+    let default_num_accounts = 1_000;
     let default_num_transactions = 10_000;
     c.bench_function("peer_to_peer", |b| {
         let bencher = TransactionBencher::new(
@@ -20,22 +23,24 @@ fn peer_to_peer<M: Measurement + 'static>(c: &mut Criterion<M>) {
             default_num_accounts,
             default_num_transactions,
         );
-        bencher.bench(b)
+        bencher.bench(b);
     });
 
     c.bench_function("peer_to_peer_parallel", |b| {
+
         let bencher = TransactionBencher::new(
             any_with::<P2PTransferGen>((10_000, 10_000_000)),
             default_num_accounts,
             default_num_transactions,
         );
-        bencher.bench_parallel(b)
+        bencher.bench_parallel(b);
     });
 }
 
 criterion_group!(
     name = txn_benches;
-    config = wall_time_measurement().sample_size(10);
+    // config = wall_time_measurement().sample_size(10);
+    config = Criterion::default().with_profiler(PProfProfiler::new(10, Output::Flamegraph(None)));
     targets = peer_to_peer
 );
 
