1a4e540abab5cd2491997bf717c542da6f2aa456
welbon <libo.weng@gmail.com>
2023-09-12 15:19:26 +0800

    [benchmark] append txn numbers and concurrecy level as command line

--- vm/transaction-benchmarks/src/main.rs
+++ vm/transaction-benchmarks/src/main.rs
@@ -6,9 +6,12 @@ use starcoin_transaction_benchmarks::transactions::TransactionBencher;
 
 #[derive(Debug, Parser)]
 pub struct ConcurrencyLevelOpt {
-    #[clap(long, short = 'n')]
+    #[clap(long, short = 'n', use_delimiter = true)]
     /// concurrency level
-    pub concurrency_level: usize,
+    pub concurrency_level: Vec<usize>,
+    #[clap(long, short = 't', use_delimiter = true)]
+    /// Transaction numbers
+    pub txn_nums: Vec<usize>,
     #[clap(long, short = 'p')]
     /// run parallel
     pub run_par: bool,
@@ -21,13 +24,17 @@ fn main() {
     let opt: ConcurrencyLevelOpt = ConcurrencyLevelOpt::parse();
     let default_num_accounts = 100;
     let default_num_transactions = 1_000;
-    let concurrency_level = opt.concurrency_level;
+    let concurrency_levels = opt.concurrency_level;
+    let txns = opt.txn_nums;
     let mut run_par = opt.run_par;
     let run_seq = true;
 
-    if concurrency_level > 0 {
-        run_par = true;
-    }
+    assert!(!concurrency_levels.is_empty(), "Concurrcy level array is empty!");
+    assert!(!txns.is_empty(),  "Transaction numbers level array is empty!");
+
+    // if !concurrency_levels.is_empty() {
+    //     run_par = true;
+    // }
 
     let bencher = TransactionBencher::new(
         any_with::<P2PTransferGen>((1_000, 1_000_000)),
@@ -36,7 +43,7 @@ fn main() {
     );
 
     let acts = [1000];
-    let txns = [100000];
+    //let txns = [10000, 50000, 100000];
     let num_warmups = 2;
     let num_runs = 10;
 
@@ -45,32 +52,34 @@ fn main() {
     let mut par_measurements = Vec::new();
     let mut seq_measurements = Vec::new();
 
-    for block_size in txns {
-        for num_accounts in acts {
-            let (mut par_tps, mut seq_tps) = bencher.blockstm_benchmark(
-                num_accounts,
-                block_size,
-                run_par,
-                run_seq,
-                num_warmups,
-                num_runs,
-                concurrency_level,
-            );
-            par_tps.sort();
-            seq_tps.sort();
-            par_measurements.push(par_tps);
-            seq_measurements.push(seq_tps);
+    for concurrency_level in concurrency_levels {
+        for block_size in &txns {
+            for num_accounts in acts {
+                let (mut par_tps, mut seq_tps) = bencher.blockstm_benchmark(
+                    num_accounts,
+                    *block_size,
+                    run_par || (concurrency_level > 1),
+                    run_seq,
+                    num_warmups,
+                    num_runs,
+                    concurrency_level,
+                );
+                par_tps.sort();
+                seq_tps.sort();
+                par_measurements.push(par_tps);
+                seq_measurements.push(seq_tps);
+            }
         }
+        println!("\nconcurrency_level = {}\n", concurrency_level);
     }
 
-    println!("\nconcurrency_level = {}\n", concurrency_level);
 
     let mut i = 0;
-    for block_size in txns {
+    for block_size in &txns {
         for num_accounts in acts {
             println!(
                 "PARAMS: num_account = {}, block_size = {}",
-                num_accounts, block_size
+                num_accounts, *block_size
             );
 
             let mut seq_tps = 1;
