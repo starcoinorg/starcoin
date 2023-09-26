fa59bc26ec9f91d526c10dfb03fb1ed2e1dc4eef
nk_ysg <nk_ysg@163.com>
2023-09-26 17:41:11 +0800

    update nextest.sh

--- scripts/nextest.sh
+++ scripts/nextest.sh
@@ -14,7 +14,7 @@ ulimit -a
 
 # install cargo-nextest
 echo "Setup cargo-nextest."
-cargo nextest -V >/dev/null 2>&1 || cargo install cargo-nextest --locked
+cargo nextest -V >/dev/null 2>&1 || cargo install cargo-nextest
 
 # following options are tuned for current self hosted CI machine
 # --test-threads 12, proper test concurrency level, balance failure rate and test speed
