8749892e42a8b038f1330e442e8f55a10d061076
nk_ysg <nk_ysg@163.com>
2023-09-26 19:06:24 +0800

    update nextest.sh

--- scripts/nextest.sh
+++ scripts/nextest.sh
@@ -14,7 +14,7 @@ ulimit -a
 
 # install cargo-nextest
 echo "Setup cargo-nextest."
-cargo install clap --version "4.3.19" --locked
+cargo install nextest-runner --version "0.45.0" --locked
 cargo nextest -V >/dev/null 2>&1 || cargo install cargo-nextest --locked
 
 # following options are tuned for current self hosted CI machine
