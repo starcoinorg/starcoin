bd24b14af89125af0817e518910b17da64bcfd5e
nk_ysg <nk_ysg@163.com>
2023-10-26 20:38:57 +0800

    add hash_extra_jh_r rust function and unit test

--- consensus/cryptonight-rs/src/lib.rs
+++ consensus/cryptonight-rs/src/lib.rs
@@ -11,6 +11,10 @@ extern "C" {
     ) -> c_void;
 }
 
+#[link(name = "cryptonight", kind = "static")]
+extern "C" {
+    fn hash_extra_jh(data: *const c_char, length: size_t, hash: *const c_char) -> c_void;
+}
 const VARIANT: i32 = 4;
 const HEIGHT: u64 = 0;
 
@@ -21,6 +25,19 @@ pub fn cryptonight_r(data: &[u8], size: usize) -> Vec<u8> {
     let hash_ptr: *const c_char = hash.as_ptr() as *const c_char;
     let mut hash = unsafe {
         cryptonight_hash(data_ptr, hash_ptr, size, VARIANT, HEIGHT);
+        std::mem::transmute::<Vec<i8>, Vec<u8>>(hash)
+    };
+    hash.reverse();
+    hash
+}
+
+#[allow(clippy::unsound_collection_transmute)]
+pub fn hash_extra_jh_r(data: &[u8], size: usize) -> Vec<u8> {
+    let hash: Vec<i8> = vec![0i8; 32];
+    let data_ptr: *const c_char = data.as_ptr() as *const c_char;
+    let hash_ptr: *const c_char = hash.as_ptr() as *const c_char;
+    let mut hash = unsafe {
+        hash_extra_jh(data_ptr, size, hash_ptr);
         std::mem::transmute::<Vec<i8>, Vec<u8>>(hash)
     };
     hash.reverse();
