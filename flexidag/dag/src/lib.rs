c149b8481eb0b2c05a32310211a808fb130c1460
jackzhhuang <jackzhhuang@gmail.com>
2024-03-15 13:04:10 +0800

    mv test and remove warnning libs

--- flexidag/dag/src/lib.rs
+++ flexidag/dag/src/lib.rs
@@ -5,7 +5,6 @@ pub mod consensusdb;
 pub mod ghostdag;
 pub mod reachability;
 pub mod types;
-mod tests;
 
 pub fn process_key_already_error(result: Result<(), StoreError>) -> Result<(), StoreError> {
     if let Err(StoreError::KeyAlreadyExists(_)) = result {
