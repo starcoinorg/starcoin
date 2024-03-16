661faff2df5337e0d9f4ddd3f60ed1faef376165
nk_ysg <nk_ysg@163.com>
2024-03-16 09:56:37 +0800

    add transaction_arg_verify

--- executor/tests/script_function_test.rs
+++ executor/tests/script_function_test.rs
@@ -11,8 +11,10 @@ use starcoin_transaction_builder::{
 use starcoin_types::account::Account;
 use starcoin_types::account_config::association_address;
 use starcoin_types::transaction::Transaction;
+use starcoin_vm_types::account_config::stc_type_tag;
 use starcoin_vm_types::identifier::Identifier;
 use starcoin_vm_types::language_storage::ModuleId;
+use starcoin_vm_types::state_view::StateReaderExt;
 use starcoin_vm_types::transaction::{
     Package, Script, ScriptFunction, TransactionPayload, TransactionStatus,
 };
@@ -383,3 +385,76 @@ fn test_struct_republish_backward_incompatible() -> Result<()> {
 
     Ok(())
 }
+
+#[stest::test]
+fn test_transaction_arg_verify() -> Result<()> {
+    let (chain_state, net) = prepare_genesis();
+    let account1 = Account::new();
+    let txn1 = Transaction::UserTransaction(create_account_txn_sent_as_association(
+        &account1, 0, 5_000_000, 1, &net,
+    ));
+    let output1 = execute_and_apply(&chain_state, txn1);
+    assert_eq!(KeptVMStatus::Executed, output1.status().status().unwrap());
+    let module_source = r#"
+    module {{sender}}::test {
+    use StarcoinFramework::Token::{Token};
+    use StarcoinFramework::Account;
+
+    public entry fun deposit_token<T: store>(account: signer, coin: Token<T>) {
+        Account::deposit_to_self<T>(&account, coin);
+        }
+    } "#;
+    let module = compile_modules_with_address(*account1.address(), module_source)
+        .pop()
+        .unwrap();
+
+    let package = Package::new_with_module(module)?;
+
+    let txn1 = Transaction::UserTransaction(account1.create_signed_txn_impl(
+        *account1.address(),
+        TransactionPayload::Package(package),
+        0,
+        1000_000,
+        1,
+        1,
+        net.chain_id(),
+    ));
+    let output1 = execute_and_apply(&chain_state, txn1);
+    assert_eq!(
+        KeptVMStatus::MiscellaneousError,
+        output1.status().status().unwrap()
+    );
+    if output1.status().status().is_err() {
+        return Ok(());
+    }
+
+    let balance = chain_state.get_balance(*account1.address())?;
+    println!("balance 1 {:#?}", balance);
+
+    let money = 100_000;
+    let num: u128 = 50_000_000;
+    let payload = TransactionPayload::ScriptFunction(ScriptFunction::new(
+        ModuleId::new(*account1.address(), Identifier::new("test").unwrap()),
+        Identifier::new("deposit_token").unwrap(),
+        vec![stc_type_tag()],
+        vec![bcs_ext::to_bytes(&num).unwrap()],
+    ));
+    let txn = Transaction::UserTransaction(account1.create_signed_txn_impl(
+        *account1.address(),
+        payload,
+        1,
+        money,
+        1,
+        1,
+        net.chain_id(),
+    ));
+
+    let output = execute_and_apply(&chain_state, txn);
+    println!("output this");
+    assert_eq!(
+        KeptVMStatus::MiscellaneousError,
+        output.status().status().unwrap()
+    );
+    Ok(())
+}
+
