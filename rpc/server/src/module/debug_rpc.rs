c6aee22087fba7935c20a343ef0e1861677df20b
jackzhhuang <jackzhhuang@gmail.com>
2024-06-12 08:48:30 +0800

    fix test_generated_schema_are_up_to_date_in_git

--- rpc/server/src/module/debug_rpc.rs
+++ rpc/server/src/module/debug_rpc.rs
@@ -63,9 +63,9 @@ impl DebugApi for DebugRpcImpl {
     }
 
     fn panic(&self) -> Result<()> {
-        if !self.config.net().is_test()
+        if !(self.config.net().is_test()
+            && self.config.net().is_dag_test())
             || self.config.net().is_dev()
-            || self.config.net().is_dag_test()
         {
             return Err(jsonrpc_core::Error::invalid_request());
         }
