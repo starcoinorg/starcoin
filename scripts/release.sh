bed53240b9268a14ae1dabccf75b45975dc36b57
0xa <hit.cs.lijun@gmail.com>
2024-07-02 13:42:41 +0800

    [Scripts] Fix release (#4147)
    * Revert "release only starcoin"
    
    This reverts commit 62ebf79f5654eaa573ff58fd99db5dcab089f91f.
    
    * kube: Update loadbalance manifest
    
    * Fix node selector
    
    * Update argument

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,13 @@
 rm -rf starcoin-artifacts/*
 mkdir -p starcoin-artifacts/
 cp -v target/release/starcoin starcoin-artifacts/
+cp -v target/release/starcoin_miner starcoin-artifacts/
+cp -v target/release/starcoin_generator starcoin-artifacts/
+cp -v target/release/mpm starcoin-artifacts/
+cp -v target/release/starcoin_txfactory starcoin-artifacts/
+cp -v target/release/starcoin_db_exporter starcoin-artifacts/
+cp -v scripts/import_block.sh starcoin-artifacts/
+cp -v scripts/verify_header.sh starcoin-artifacts/
 cp -v README.md starcoin-artifacts/
 if [ "$1" == "windows-latest" ]; then
   7z a -r starcoin-$1.zip starcoin-artifacts
