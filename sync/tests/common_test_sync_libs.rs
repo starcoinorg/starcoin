88b88fb2b6d400d0589517ac5341f9b339c74f9b
simonjiao <simonjiaoh@gmail.com>
2024-07-12 10:22:05 +0800

    remove unused codes

--- sync/tests/common_test_sync_libs.rs
+++ sync/tests/common_test_sync_libs.rs
@@ -1,24 +1,21 @@
 use anyhow::{Ok, Result};
-use futures::executor::block_on;
-use network_api::PeerId;
-use starcoin_chain::BlockChain;
-use starcoin_chain_api::{ChainAsyncService, ChainReader};
-use starcoin_chain_service::ChainReaderService;
+
+
+
+
+
 use starcoin_config::{temp_dir, NetworkConfig, NodeConfig};
 use starcoin_crypto::HashValue;
-use starcoin_dag::blockdag::BlockDAG;
+
 use starcoin_logger::prelude::*;
-use starcoin_miner::MinedBlock;
+
 use starcoin_node::NodeHandle;
-use starcoin_service_registry::{
-    bus::{Bus, BusService},
-    RegistryAsyncService, RegistryService, ServiceRef,
-};
-use starcoin_storage::Storage;
-use starcoin_types::block::{BlockHeader, BlockNumber};
-use starcoin_vm_types::on_chain_config::FlexiDagConfig;
-use std::{sync::Arc, time::Duration};
-use test_helper::Account;
+
+
+use starcoin_types::block::{BlockHeader};
+
+use std::{sync::Arc};
+
 
 #[derive(Debug, Clone)]
 pub struct DagBlockInfo {
@@ -72,124 +69,4 @@ pub fn generate_block(handle: &NodeHandle, count: usize) -> Result<()> {
         let _ = handle.generate_block()?;
     }
     Ok(())
-}
-
-#[allow(unused)]
-pub fn generate_dag_block(handle: &NodeHandle, count: usize) -> Result<Vec<DagBlockInfo>> {
-    let mut result = vec![];
-    let dag = handle.get_dag()?;
-    while result.len() < count {
-        let (block, is_dag) = handle.generate_block()?;
-        if is_dag {
-            result.push(block);
-        }
-    }
-    Ok(result
-        .into_iter()
-        .map(|block| DagBlockInfo {
-            header: block.header().clone(),
-            children: dag.get_children(block.header().id()).unwrap(),
-        })
-        .collect::<Vec<DagBlockInfo>>())
-}
-
-// fixme: remove unused
-#[allow(unused)]
-pub fn init_two_node() -> Result<(NodeHandle, NodeHandle, PeerId)> {
-    // network1 initialization
-    let (local_handle, local_net_addr) = {
-        let local_config = NodeConfig::random_for_test();
-        let net_addr = local_config.network.self_address();
-        debug!("Local node address: {:?}", net_addr);
-        (gen_chain_env(local_config).unwrap(), net_addr)
-    };
-
-    // network2 initialization
-    let (target_handle, target_peer_id) = {
-        let mut target_config = NodeConfig::random_for_test();
-        target_config.network.seeds = vec![local_net_addr].into();
-        let target_peer_id = target_config.network.self_peer_id();
-        (gen_chain_env(target_config).unwrap(), target_peer_id)
-    };
-    Ok((local_handle, target_handle, target_peer_id))
-}
-
-/// Just for test
-#[allow(unused)]
-pub fn execute_dag_poll_block(
-    registry: ServiceRef<RegistryService>,
-    fork_number: BlockNumber,
-) -> Result<u64> {
-    let timestamp = block_on(async move {
-        let node_config = registry
-            .get_shared::<Arc<NodeConfig>>()
-            .await
-            .expect("Failed to get node config");
-        let time_service = node_config.net().time_service();
-        let chain_service = registry
-            .service_ref::<ChainReaderService>()
-            .await
-            .expect("failed to get chain reader service");
-        let header_hash = chain_service
-            .main_head_header()
-            .await
-            .expect("failed to get header hash")
-            .id();
-        let storage = registry
-            .get_shared::<Arc<Storage>>()
-            .await
-            .expect("failed to get storage");
-        let dag = registry
-            .get_shared::<BlockDAG>()
-            .await
-            .expect("failed to get dag");
-        let mut chain = BlockChain::new(time_service, header_hash, storage, None, dag)
-            .expect("failed to get new the chain");
-        let net = node_config.net();
-        let current_number = chain.status().head().number();
-        chain = test_helper::dao::modify_on_chain_config_by_dao_block(
-            Account::new(),
-            chain,
-            net,
-            test_helper::dao::vote_flexi_dag_config(net, fork_number),
-            test_helper::dao::on_chain_config_type_tag(FlexiDagConfig::type_tag()),
-            test_helper::dao::execute_script_on_chain_config(net, FlexiDagConfig::type_tag(), 0u64),
-        )
-        .expect("failed to execute script for poll");
-
-        let bus = registry
-            .service_ref::<BusService>()
-            .await
-            .expect("failed to get bus service");
-        // broadcast poll blocks
-        for block_number in current_number + 1..=chain.status().head().number() {
-            let block = chain
-                .get_block_by_number(block_number)
-                .expect("failed to get block by number")
-                .unwrap();
-            let block_info = chain
-                .get_block_info(Some(block.id()))
-                .expect("failed to get block info")
-                .unwrap();
-            bus.broadcast(MinedBlock(Arc::new(block)))
-                .expect("failed to broadcast new head block");
-        }
-
-        loop {
-            if chain_service
-                .main_head_block()
-                .await
-                .expect("failed to get main head block")
-                .header()
-                .number()
-                == chain.status().head().number()
-            {
-                break;
-            } else {
-                async_std::task::sleep(Duration::from_millis(500)).await;
-            }
-        }
-        chain.time_service().now_millis()
-    });
-    Ok(timestamp)
 }
