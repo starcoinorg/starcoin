a3791d12d78009dde2a393cfa0ff50be6c2062e2
simonjiao <simonjiaoh@gmail.com>
2024-08-06 11:29:30 +0800

    fix some warnings

--- flexidag/src/consensusdb/consensus_relations.rs
+++ flexidag/src/consensusdb/consensus_relations.rs
@@ -67,7 +67,7 @@ impl ValueCodec<RelationChildren> for Hash {
     }
 
     fn decode_value(data: &[u8]) -> Result<Self, StoreError> {
-        Hash::from_slice(data).map_err(|e| StoreError::DecodeError(e.to_string()))
+        Self::from_slice(data).map_err(|e| StoreError::DecodeError(e.to_string()))
     }
 }
 
