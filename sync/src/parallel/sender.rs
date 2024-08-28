11102f9d79efe5862b27b450a09aa1010ca323ce
jackzhhuang <jackzhhuang@gmail.com>
2024-08-29 03:13:28 +0800

    set executing state in sender

--- sync/src/parallel/sender.rs
+++ sync/src/parallel/sender.rs
@@ -110,7 +110,6 @@ impl<'a> DagBlockSender<'a> {
 
             // Finding the executing state is the priority
             if self.dispatch_to_worker(&block).await? {
-                self.flush_executor_state().await?;
                 continue;
             }
 
