1616233cf6f2459e95b863be6694ce4018c5fd66
nk_ysg <nk_ysg@163.com>
2024-09-03 02:11:24 +0800

    update starcoin-config

--- config/src/genesis_config.rs
+++ config/src/genesis_config.rs
@@ -14,7 +14,8 @@ use starcoin_crypto::{
     multi_ed25519::{genesis_multi_key_pair, MultiEd25519PublicKey},
     HashValue, ValidCryptoMaterialStringExt,
 };
-use starcoin_gas_algebra::{CostTable, FromOnChainGasSchedule};
+use starcoin_gas_algebra::{CostTable};
+use starcoin_gas_schedule::FromOnChainGasSchedule;
 use starcoin_gas_meter::StarcoinGasParameters;
 use starcoin_time_service::{TimeService, TimeServiceType};
 use starcoin_uint::U256;
