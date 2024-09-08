1f68c4111384afe7abed9a1cfb390f28cdc61891
nk_ysg <nk_ysg@163.com>
2024-09-08 22:54:19 +0800

    update starcoin-gas-schedule

--- vm/gas-schedule/src/gas_schedule/starcoin_framework.rs
+++ vm/gas-schedule/src/gas_schedule/starcoin_framework.rs
@@ -3,6 +3,7 @@
 
 use crate::gas_schedule::NativeGasParameters;
 use crate::traits::EXECUTION_GAS_MULTIPLIER as MUL;
+use move_core_types::gas_algebra::InternalGasPerArg;
 use starcoin_gas_algebra::{InternalGas, InternalGasPerByte};
 
 // see starcoin/vm/types/src/on_chain_config/genesis_gas_schedule.rs
@@ -11,21 +12,21 @@ use starcoin_gas_algebra::{InternalGas, InternalGasPerByte};
 crate::gas_schedule::macros::define_gas_parameters!(StarcoinFrameworkGasParameters, "starcoin_framework",
     NativeGasParameters => .starcoin_framework,
     [
-    // [signature_ed25519_verify_base,  "signature.ed25519_verify.base", 0 * MUL],
-    [signature_ed25519_verify_per_byte: InternalGasPerByte,  "signature.ed25519_verify.per_byte", (61 + 1)* MUL],
-   // [signature_ed25519_validate_key_base,  "signature.ed25519_validate_key.base", 0 * MUL],
-    [signature_ed25519_validate_key_per_byte: InternalGasPerByte,  "signature.ed25519_validate_key.per_byte",(26 + 1) * MUL],
+    [signature_ed25519_pubkey_base: InternalGas,  "signature.ed25519.pubkey.base", 0 * MUL],
+    [signature_ed25519_pubkey_per_byte: InternalGasPerByte,  "signature.ed25519.pubkey.per_byte", (61 + 1)* MUL],
+     [signature_ed25519_verify_base : InternalGas,  "signature.ed25519.verify.base", 0 * MUL],
+    [signature_ed25519_verify_per_byte: InternalGasPerByte,  "signature.ed25519.verify.per_byte",(26 + 1) * MUL],
 
     [account_create_signer_base: InternalGas, "account.create_signer.base", (24 + 1) * MUL],
     [account_destroy_signer_base: InternalGas, "account.destroy_signer.base", (212 + 1)* MUL],
 
     [token_name_of_base: InternalGas,  "token.name_of.base", (2002 + 1) * MUL],
 
-   // [.hash.keccak256.base, optional "hash.keccak256.base",  0 * MUL],
+    [hash_keccak256_base: InternalGas, "hash.keccak256.base",  0 * MUL],
     [hash_keccak256_per_byte: InternalGasPerByte, "hash.keccak256.per_byte",  (64 + 1) *MUL],
-  //  [hash_ripemd160_base:InternalGasPerByte ,   "hash.ripemd160.base", 0 * MUL],
+        [hash_ripemd160_base:InternalGas ,   "hash.ripemd160.base", 0 * MUL],
     [hash_ripemd160_per_byte: InternalGasPerByte,  "hash.ripemd160.per_byte", (64 + 1) * MUL],
-   // [signature_ec_recover_base: InternalGasPerByte,   "signature.ec_recover.base",  0 * MUL],
+     [signature_ec_recover_base: InternalGas,   "signature.ec_recover.base",  0 * MUL],
     [signature_ec_recover_per_byte: InternalGasPerByte,   "signature.ec_recover.per_byte", (128 + 1) * MUL],
 
        // XXX FIXME YSG, need to remove?
@@ -39,8 +40,10 @@ crate::gas_schedule::macros::define_gas_parameters!(StarcoinFrameworkGasParamete
     [u256_pow_base: InternalGas,   "u256.pow.base",  (8 + 1) * MUL],
         // XXX FIXME YSG, need to remove?
 
-    [from_bcs_base: InternalGas,  "frombcs.base", (4 + 1)  * MUL],
+    [util_from_bytes_base: InternalGas,  "util.from_bytes.base", (4 + 1)  * MUL],
+          [util_from_bytes_per_byte: InternalGasPerByte, "util.from_bytes.per_byte", 0],
     [secp256k1_base: InternalGas,  "secp256k1.base", (4 + 1)  * MUL],
+                [secp256k1_ecdsa_recover: InternalGasPerArg, "secp256k1.ecdsa_recover", 5918360],
 
         [code_request_publish_base: InternalGas, "code.request_publish.base", 1838],
         [code_request_publish_per_byte: InternalGasPerByte, "code.request_publish.per_byte", 7],
