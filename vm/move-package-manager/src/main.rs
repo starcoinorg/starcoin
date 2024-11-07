ad02c9922952d66bc137bf984c3c0456446902cf
nk_ysg <nk_ysg@163.com>
2024-11-07 12:21:44 +0800

    fix move-package-manager errors

--- vm/move-package-manager/src/main.rs
+++ vm/move-package-manager/src/main.rs
@@ -4,7 +4,7 @@
 use anyhow::Result;
 use clap::Parser;
 // use move_cli::package::cli::handle_package_commands;
-use move_cli::{experimental, sandbox, Move, DEFAULT_STORAGE_DIR};
+use move_cli::Move;
 use move_core_types::errmap::ErrorMapping;
 use move_package_manager::compatibility_check_cmd::{
     handle_compatibility_check, CompatibilityCheckCommand,
@@ -16,10 +16,7 @@ use move_package_manager::{run_integration_test, IntegrationTestCommand};
 use move_vm_test_utils::gas_schedule::CostTable;
 use starcoin_config::genesis_config::G_LATEST_GAS_PARAMS;
 use starcoin_vm_runtime::natives::starcoin_natives;
-use starcoin_vm_types::on_chain_config::{
-    TimedFeatures, TimedFeaturesBuilder, G_LATEST_INSTRUCTION_TABLE,
-};
-use std::path::PathBuf;
+use starcoin_vm_types::on_chain_config::{TimedFeaturesBuilder, G_LATEST_INSTRUCTION_TABLE};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -43,17 +40,6 @@ pub enum Commands {
     /// Release the package.
     #[clap(name = "release")]
     Release(Release),
-
-    /// (Experimental) Run static analyses on Move source or bytecode.
-    #[clap(name = "experimental")]
-    Experimental {
-        /// Directory storing Move resources, events, and module bytecodes produced by module publishing
-        /// and script execution.
-        #[arg(long, default_value = DEFAULT_STORAGE_DIR, value_parser = value_parser!(std::ffi::OsString))]
-        storage_dir: PathBuf,
-        #[clap(subcommand)]
-        cmd: experimental::cli::ExperimentalCommand,
-    },
     /// Run integration tests in tests dir.
     #[clap(name = "integration-test", alias = "spectest")]
     IntegrationTest(IntegrationTestCommand),
@@ -68,7 +54,7 @@ pub enum Commands {
 }
 
 fn main() -> Result<()> {
-    let error_descriptions: ErrorMapping =
+    let _error_descriptions: ErrorMapping =
         bcs_ext::from_bytes(stdlib::ERROR_DESCRIPTIONS).expect("Decode err map failed");
     let args: CliOptions = CliOptions::parse();
 
@@ -82,20 +68,12 @@ fn main() -> Result<()> {
         TimedFeaturesBuilder::enable_all().build(),
         Default::default(),
     );
-    let cost_table = CostTable {
+    let _cost_table = CostTable {
         instruction_table: G_LATEST_INSTRUCTION_TABLE.clone(),
     };
     match args.cmd {
         Commands::IntegrationTest(cmd) => run_integration_test(args.move_args, cmd),
         Commands::Package { cmd } => handle_package_commands(natives, args.move_args, cmd),
-        Commands::Sandbox { storage_dir, cmd } => cmd.handle_command(
-            natives,
-            &cost_table,
-            &error_descriptions,
-            move_args,
-            &storage_dir,
-        ),
-        Commands::Experimental { storage_dir, cmd } => cmd.handle_command(move_args, &storage_dir),
         Commands::Release(release) => handle_release(move_args, release),
         Commands::CompatibilityCheck(cmd) => handle_compatibility_check(move_args, cmd),
         Commands::Deploy(cmd) => handle_deployment(move_args, cmd),
