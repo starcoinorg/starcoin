de7b6d9490dfb8c5060d6a3234fc964d7ab6abc1
simonjiao <simonjiaoh@gmail.com>
2024-11-12 12:14:33 +0800

    fix testsuite building errors (#4277)

--- testsuite/tests/steps/state.rs
+++ testsuite/tests/steps/state.rs
@@ -2,20 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 use crate::MyWorld;
 use cucumber::{Steps, StepsBuilder};
+use move_core_types::move_resource::MoveStructType;
 use starcoin_types::access_path::AccessPath;
-use starcoin_vm_types::{account_config::AccountResource, move_resource::MoveResource};
+use starcoin_vm_types::{account_config::AccountResource, state_store::state_key::StateKey};
 
 pub fn steps() -> Steps<MyWorld> {
     let mut builder: StepsBuilder<MyWorld> = Default::default();
     builder.then("state proof", |world: &mut MyWorld, _step| {
         let client = world.default_rpc_client.as_ref().take().unwrap();
         let account = world.default_account.as_ref().take().unwrap();
-        let access_path =
-            AccessPath::new(account.address.clone(), AccountResource::resource_path());
-        let proof = client
-            .clone()
-            .state_get_with_proof(access_path.clone())
-            .unwrap();
+        let access_path = AccessPath::resource_access_path(
+            account.address.clone(),
+            AccountResource::struct_tag(),
+        );
+        let key = StateKey::resource(account.address(), &AccountResource::struct_tag()).unwrap();
+        let proof = client.clone().state_get_with_proof(key).unwrap();
         let state_root = client.clone().state_get_state_root().unwrap();
         proof
             .into_state_proof()
