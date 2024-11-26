1a4f9196e8253a3984a21ed83eecbdd5c161e406
jackzhhuang <jackzhhuang@gmail.com>
2024-11-26 15:39:18 +0800

    add transaction db

--- flexidag/tests/tests.rs
+++ flexidag/tests/tests.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{bail, format_err, Ok, Result};
+use rocksdb::{OptimisticTransactionDB, SingleThreaded};
+use starcoin_config::temp_dir;
 use starcoin_crypto::HashValue as Hash;
 use starcoin_dag::{
     blockdag::{BlockDAG, MineNewDagBlockInfo},
@@ -16,6 +18,7 @@ use starcoin_dag::{
     types::{ghostdata::GhostdagData, interval::Interval},
 };
 use starcoin_logger::prelude::debug;
+use starcoin_storage::storage::ColumnFamilyName;
 use starcoin_types::{
     block::{BlockHeader, BlockHeaderBuilder, BlockNumber},
     blockhash::{BlockHashMap, HashKTypeMap, KType},
@@ -1308,4 +1311,148 @@ fn test_verification_blue_block() -> anyhow::Result<()> {
         .check_ghostdata_blue_block(&together_ghost_data)?;
 
     anyhow::Result::Ok(())
+}
+
+fn init_transaction_db(
+    columns: &[ColumnFamilyName],
+) -> anyhow::Result<OptimisticTransactionDB<SingleThreaded>> {
+    // db config
+    let mut cf_opts = rocksdb::Options::default();
+    cf_opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
+    cf_opts.create_if_missing(true);
+    cf_opts.create_missing_column_families(true);
+
+    let temp_path = temp_dir();
+    let db: OptimisticTransactionDB<SingleThreaded> = OptimisticTransactionDB::open_cf_descriptors(
+        &cf_opts,
+        temp_path,
+        columns.iter().map(|col| {
+            let mut cf_opts = rocksdb::Options::default();
+            cf_opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
+            rocksdb::ColumnFamilyDescriptor::new((*col).to_string(), cf_opts)
+        }),
+    )?;
+
+    anyhow::Ok(db)
+}
+
+fn init_columns() -> anyhow::Result<Vec<ColumnFamilyName>> {
+    const TEST_FOR_TRANSACTION1_DATA_CF: &str = "test-for-transaction1-data";
+    const TEST_FOR_TRANSACTION2_DATA_CF: &str = "test-for-transaction2-data";
+    const TEST_FOR_TRANSACTION3_DATA_CF: &str = "test-for-transaction3-data";
+
+    let columns: Vec<ColumnFamilyName> = vec![
+        TEST_FOR_TRANSACTION1_DATA_CF,
+        TEST_FOR_TRANSACTION2_DATA_CF,
+        TEST_FOR_TRANSACTION3_DATA_CF,
+    ];
+    anyhow::Ok(columns)
+}
+
+#[test]
+fn test_transaction_write_and_commit() -> anyhow::Result<()> {
+    let columns = init_columns()?;
+    let db = init_transaction_db(&columns)?;
+
+    let column1 = db.cf_handle(columns[0]).unwrap_or_else(|| {
+        panic!(
+            "check the column name: {0} that maybe not defined",
+            columns[0]
+        )
+    });
+    let column2 = db.cf_handle(columns[1]).unwrap_or_else(|| {
+        panic!(
+            "check the column name: {0} that maybe not defined",
+            columns[1]
+        )
+    });
+    let column3 = db.cf_handle(columns[2]).unwrap_or_else(|| {
+        panic!(
+            "check the column name: {0} that maybe not defined",
+            columns[2]
+        )
+    });
+
+    // write and commit
+    let transaction = db.transaction();
+
+    transaction.put_cf(column1, b"jack_key", b"jack")?;
+    transaction.put_cf(column2, b"loves_key", b"loves")?;
+    transaction.put_cf(column3, b"rose_key", b"rose")?;
+
+    transaction.commit()?;
+
+    if let Some(column_value) = db.get_cf(column1, b"jack_key")? {
+        assert_eq!(column_value, b"jack");
+    } else {
+        bail!("failed to get the value from the column1");
+    }
+
+    if let Some(column_value) = db.get_cf(column2, b"loves_key")? {
+        assert_eq!(column_value, b"loves");
+    } else {
+        bail!("failed to get the value from the column2");
+    }
+
+    if let Some(column_value) = db.get_cf(column3, b"rose_key")? {
+        assert_eq!(column_value, b"rose");
+    } else {
+        bail!("failed to get the value from the column3");
+    }
+
+    // write and rollback
+    let transaction = db.transaction();
+
+    transaction.put_cf(column1, b"jack_key", b"jacky")?;
+    transaction.put_cf(column2, b"loves_key", b"loves eternally")?;
+    transaction.put_cf(column3, b"rose_key", b"claire")?;
+
+    transaction.rollback()?;
+
+    if let Some(column_value) = db.get_cf(column1, b"jack_key")? {
+        assert_eq!(column_value, b"jack");
+    } else {
+        bail!("failed to get the value from the column1");
+    }
+
+    if let Some(column_value) = db.get_cf(column2, b"loves_key")? {
+        assert_eq!(column_value, b"loves");
+    } else {
+        bail!("failed to get the value from the column2");
+    }
+
+    if let Some(column_value) = db.get_cf(column3, b"rose_key")? {
+        assert_eq!(column_value, b"rose");
+    } else {
+        bail!("failed to get the value from the column3");
+    }
+
+    // write again and commit this time
+    let transaction = db.transaction();
+
+    transaction.put_cf(column1, b"jack_key", b"jacky")?;
+    transaction.put_cf(column2, b"loves_key", b"loves eternally")?;
+    transaction.put_cf(column3, b"rose_key", b"claire")?;
+
+    transaction.commit()?;
+
+    if let Some(column_value) = db.get_cf(column1, b"jack_key")? {
+        assert_eq!(column_value, b"jacky");
+    } else {
+        bail!("failed to get the value from the column1");
+    }
+
+    if let Some(column_value) = db.get_cf(column2, b"loves_key")? {
+        assert_eq!(column_value, b"loves eternally");
+    } else {
+        bail!("failed to get the value from the column2");
+    }
+
+    if let Some(column_value) = db.get_cf(column3, b"rose_key")? {
+        assert_eq!(column_value, b"claire");
+    } else {
+        bail!("failed to get the value from the column3");
+    }
+
+    anyhow::Ok(())
 }
