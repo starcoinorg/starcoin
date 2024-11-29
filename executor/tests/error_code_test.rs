ce17ece70cae300c17f04a8d7f6d2b3c5ba50111
simonjiao <simonjiaoh@gmail.com>
2024-11-29 10:16:25 +0800

    ignore some Tests
    * NFT, sip flag, module upgrade, dummy token

--- executor/tests/error_code_test.rs
+++ executor/tests/error_code_test.rs
@@ -151,6 +151,7 @@ fn test_execute_transfer_txn_with_wrong_token_code() -> Result<()> {
     Ok(())
 }
 
+#[ignore]
 #[stest::test]
 fn test_execute_transfer_txn_with_dummy_gas_token_code() -> Result<()> {
     let (chain_state, net) = prepare_genesis();
