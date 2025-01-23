0b8baf15ecf216348c030cd5f500905ea048d9a4
welbon <2261238+welbon@users.noreply.github.com>
2025-01-23 12:06:30 +0800

    [asset-mapping] change test case using test heper functions

--- asset-mapping/src/lib.rs
+++ asset-mapping/src/lib.rs
@@ -1,5 +1,4 @@
 use std::str::FromStr;
-use std::sync::Arc;
 
 use anyhow::Result;
 use starcoin_crypto::{hash::PlainCryptoHash, HashValue};
@@ -8,29 +7,27 @@ use forkable_jellyfish_merkle::{blob::Blob, node_type::SparseMerkleLeafNode, Raw
 use starcoin_cached_packages::starcoin_framework_sdk_builder::{
     asset_mapping_assign_to_account_test, asset_mapping_assign_to_account_with_proof,
 };
-
-use starcoin_chain::{BlockChain, ChainReader, ChainWriter};
-use starcoin_config::{ChainNetwork, NodeConfig, G_TEST_CONFIG};
+use starcoin_cached_packages::starcoin_stdlib::{
+    starcoin_account_create_account, transfer_scripts_peer_to_peer_v2,
+};
+use starcoin_chain::ChainReader;
+use starcoin_config::{ChainNetwork, G_TEST_CONFIG};
 use starcoin_consensus::Consensus;
 use starcoin_state_api::ChainStateReader;
-use starcoin_transaction_builder::{
-    create_signed_txn_with_association_account, peer_to_peer_txn_sent_as_association,
-    DEFAULT_MAX_GAS_AMOUNT,
-};
 use starcoin_types::{
-    account::Account, account::DEFAULT_EXPIRATION_TIME, account_address::AccountAddress,
+    account_address::AccountAddress,
     account_config::CORE_CODE_ADDRESS, identifier::Identifier, language_storage::StructTag,
 };
-use starcoin_vm_types::account_config::association_address;
 use starcoin_vm_types::{
     access_path::AccessPath,
-    account_config::{self, genesis_address, stc_type_tag},
+    account_config::{genesis_address, stc_type_tag},
     genesis_config::ChainId,
     state_store::state_key::StateKey,
     state_view::StateReaderExt,
-    transaction::SignedUserTransaction,
 };
-use test_helper::executor::prepare_genesis;
+use test_helper::executor::{
+    association_execute_should_success, prepare_customized_genesis, prepare_genesis,
+};
 
 #[test]
 fn test_get_chain_id_after_genesis_with_proof_verify() -> Result<()> {
@@ -136,67 +133,40 @@ fn test_asset_mapping_for_specified_coin_type() -> Result<()> {
 fn test_simple_asset_mapping_without_proof() -> Result<()> {
     starcoin_logger::init_for_test();
 
-    // let (chain_state, net) = prepare_genesis();
-    let test_net = ChainNetwork::new_test();
-    let mut block_chain = test_helper::gen_blockchain_with_blocks_for_test(0, &test_net)?;
-    let associal_sequence_number = block_chain
-        .chain_state_reader()
-        .get_sequence_number(association_address())?;
+    let (chain_state, net) = prepare_genesis();
 
     // Build alice and transfer STC to alice
     let amount = 1000000000;
     let alice = AccountAddress::from_str("0xd0c5a06ae6100ce115cad1600fe59e96").unwrap();
-    local_block_chain_excecute_txn(
-        &mut block_chain,
-        peer_to_peer_txn_sent_as_association(
-            alice,
-            associal_sequence_number,
-            amount,
-            DEFAULT_EXPIRATION_TIME,
-            &test_net,
-        ),
+
+    association_execute_should_success(
+        &net,
+        &chain_state,
+        transfer_scripts_peer_to_peer_v2(stc_type_tag(), alice, amount),
     )?;
 
     // Check alice's balance
-    {
-        assert_eq!(
-            block_chain.chain_state_reader().get_balance(alice)?,
-            amount,
-            "alice balance not expect"
-        );
-    }
+    assert_eq!(
+        chain_state.get_balance(alice)?,
+        amount,
+        "alice balance not expect"
+    );
 
-    let genesis_sequence_number = {
-        block_chain
-            .chain_state_reader()
-            .get_sequence_number(genesis_address())?
-    };
-    // Execute assign asset mapping to alice
-    local_block_chain_excecute_txn(
-        &mut block_chain,
-        create_signed_txn_with_association_account(
-            asset_mapping_assign_to_account_test(
-                alice,
-                "0x1::STC::STC".as_bytes().to_vec(),
-                amount as u64,
-            ),
-            genesis_sequence_number,
-            DEFAULT_MAX_GAS_AMOUNT,
-            1,
-            DEFAULT_EXPIRATION_TIME,
-            &test_net,
+    association_execute_should_success(
+        &net,
+        &chain_state,
+        asset_mapping_assign_to_account_test(
+            alice,
+            "0x1::STC::STC".as_bytes().to_vec(),
+            amount as u64,
         ),
     )?;
 
-    // Check alice's balance with 2 * amount
-    {
-        let balance = block_chain.chain_state_reader().get_balance(alice)?;
-        assert_eq!(
-            balance,
-            amount * 2,
-            "alice balance not expect after asset-mapping"
-        );
-    }
+    assert_eq!(
+        chain_state.get_balance(alice)?,
+        amount * 2,
+        "alice balance not expect after asset-mapping"
+    );
 
     Ok(())
 }
@@ -205,49 +175,38 @@ fn test_simple_asset_mapping_without_proof() -> Result<()> {
 fn test_asset_mapping_whole_process() -> Result<()> {
     starcoin_logger::init_for_test();
 
-    let _block_gas_limit: u64 = 10000000;
+    // let _block_gas_limit: u64 = 10000000;
     let initial_balance: u128 = 100000000000; // 1000 STC
-    let receiver = AccountAddress::from_str("0xd0c5a06ae6100ce115cad1600fe59e96").unwrap();
+    let alice = AccountAddress::from_str("0xd0c5a06ae6100ce115cad1600fe59e96").unwrap();
 
     // Create a source BlockChain
     let (proof_root_hash, proof_path_hash, proof_value_hash, proof_siblings) = {
-        // construct a test network
-        let config = Arc::new(NodeConfig::random_for_test());
-        let mut block_chain = test_helper::gen_blockchain_with_blocks_for_test(0, config.net())?;
+        let (chain_state_1, net_1) = prepare_genesis();
 
-        let peer_to_peer_txn = {
-            local_build_peer_to_peer_from_association(
-                &block_chain,
-                receiver,
-                initial_balance,
-                config.net(),
-            )?
-        };
-        local_block_chain_excecute_txn(&mut block_chain, peer_to_peer_txn)?;
+        association_execute_should_success(
+            &net_1,
+            &chain_state_1,
+            transfer_scripts_peer_to_peer_v2(stc_type_tag(), alice, initial_balance),
+        )?;
+        // Check balance is initial_balance
+        let balance = chain_state_1.get_balance(alice)?;
+        assert_eq!(balance, initial_balance);
 
-        {
-            let chain_state = block_chain.chain_state_reader();
-
-            // Check balance is initial_balance
-            let balance = block_chain.chain_state_reader().get_balance(receiver)?;
-            assert_eq!(balance, initial_balance);
-
-            let proof = chain_state.get_with_proof(&StateKey::resource(
-                &CORE_CODE_ADDRESS,
-                &StructTag {
-                    address: CORE_CODE_ADDRESS,
-                    module: Identifier::new("coin").unwrap(),
-                    name: Identifier::new("CoinStore").unwrap(),
-                    type_args: vec![stc_type_tag()],
-                },
-            )?)?;
-            (
-                chain_state.state_root(),
-                proof.proof.account_state_proof.leaf.unwrap().0,
-                proof.proof.account_state_proof.leaf.unwrap().1,
-                proof.proof.account_state_proof.siblings,
-            )
-        }
+        let proof = chain_state_1.get_with_proof(&StateKey::resource(
+            &CORE_CODE_ADDRESS,
+            &StructTag {
+                address: CORE_CODE_ADDRESS,
+                module: Identifier::new("coin").unwrap(),
+                name: Identifier::new("CoinStore").unwrap(),
+                type_args: vec![stc_type_tag()],
+            },
+        )?)?;
+        (
+            chain_state_1.state_root(),
+            proof.proof.account_state_proof.leaf.unwrap().0,
+            proof.proof.account_state_proof.leaf.unwrap().1,
+            proof.proof.account_state_proof.siblings,
+        )
     };
 
     println!(
@@ -267,7 +226,7 @@ fn test_asset_mapping_whole_process() -> Result<()> {
             genesis_config,
         )?;
 
-        let mut block_chain = test_helper::gen_blockchain_with_blocks_for_test(0, &net)?;
+        let chain_state_2 = prepare_customized_genesis(&net);
 
         let mut proof_encoded_siblings: Vec<u8> = Vec::new();
         proof_siblings.iter().for_each(|hash| {
@@ -275,109 +234,31 @@ fn test_asset_mapping_whole_process() -> Result<()> {
             proof_encoded_siblings.push(0x7c);
         });
 
-        let genesis_sequence_number = {
-            block_chain
-                .chain_state_reader()
-                .get_sequence_number(genesis_address())?
-        };
+        // Create account
+        association_execute_should_success(
+            &net,
+            &chain_state_2,
+            starcoin_account_create_account(alice),
+        )?;
 
-        {
-            // Transfer STC from association account to receiver account
-            let peer_to_peer_txn =
-                { local_build_peer_to_peer_from_association(&block_chain, receiver, 1, &net)? };
-            local_block_chain_excecute_txn(&mut block_chain, peer_to_peer_txn)?;
-        }
+        assert_eq!(chain_state_2.get_balance(alice)?, initial_balance + 1);
 
-        {
-            // Transfer STC from association account to framework account for gas fee
-            let peer_to_peer_txn = {
-                local_build_peer_to_peer_from_association(
-                    &block_chain,
-                    AccountAddress::from_hex_literal("0x1").unwrap(),
-                    1000000000,
-                    &net,
-                )?
-            };
-            local_block_chain_excecute_txn(&mut block_chain, peer_to_peer_txn)?;
-        }
-
-        // Verify proof and assign asset mapping to receiver account
-        local_block_chain_excecute_txn(
-            &mut block_chain,
-            Account::new_genesis_account(genesis_address()).create_signed_txn_with_args(
-                asset_mapping_assign_to_account_with_proof(
-                    receiver,
-                    "0x1::STC::STC".as_bytes().to_vec(),
-                    proof_path_hash.to_vec(),
-                    proof_value_hash.to_vec(),
-                    proof_encoded_siblings,
-                    initial_balance as u64,
-                ),
-                genesis_sequence_number,
-                DEFAULT_MAX_GAS_AMOUNT,
-                1,
-                DEFAULT_EXPIRATION_TIME,
-                net.chain_id(),
+        // Asset mapping for alice
+        association_execute_should_success(
+            &net,
+            &chain_state_2,
+            asset_mapping_assign_to_account_with_proof(
+                alice,
+                "0x1::STC::STC".as_bytes().to_vec(),
+                proof_path_hash.to_vec(),
+                proof_value_hash.to_vec(),
+                proof_encoded_siblings,
+                initial_balance as u64,
             ),
         )?;
 
-        {
-            let balance = block_chain.chain_state_reader().get_balance(receiver)?;
-            println!("The verify blockchain receiver balance is: {:?}", balance);
-            assert_eq!(balance, initial_balance + 1);
-        }
+        assert_eq!(chain_state_2.get_balance(alice)?, initial_balance + 1);
     }
-
-    Ok(())
-}
-
-fn local_build_peer_to_peer_from_association(
-    chain: &BlockChain,
-    receiver: AccountAddress,
-    amount: u128,
-    net: &ChainNetwork,
-) -> Result<SignedUserTransaction> {
-    let association_sequence_num = {
-        chain
-            .chain_state_reader()
-            .get_sequence_number(account_config::association_address())?
-    };
-
-    Ok(
-        starcoin_transaction_builder::build_transfer_from_association(
-            receiver,
-            association_sequence_num,
-            amount,
-            net.time_service().now_secs() + DEFAULT_EXPIRATION_TIME,
-            net,
-        )
-        .try_into()?,
-    )
-}
-
-fn local_block_chain_excecute_txn(
-    chain: &mut BlockChain,
-    txn: SignedUserTransaction,
-) -> Result<()> {
-    let (block_template, excluded) = chain
-        .create_block_template(
-            account_config::association_address(),
-            None,
-            vec![txn],
-            vec![],
-            Some(DEFAULT_MAX_GAS_AMOUNT),
-            None,
-        )
-        .unwrap();
-
-    assert!(excluded.discarded_txns.is_empty(), "Execute txn failed!");
-    assert!(excluded.untouched_txns.is_empty(), "Execute txn failed!");
-
-    let block = chain
-        .consensus()
-        .create_block(block_template, chain.time_service().as_ref())?;
-
-    chain.apply(block)?;
 
     Ok(())
 }
