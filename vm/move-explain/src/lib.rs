6eeb917c333f330694dcf56d6dd27d4f41ac65d6
simonjiao <simonjiaoh@gmail.com>
2025-03-21 14:01:46 +0800

    update move-explain

--- vm/move-explain/src/lib.rs
+++ vm/move-explain/src/lib.rs
@@ -3,10 +3,13 @@
 
 use move_core_types::errmap::{ErrorDescription, ErrorMapping};
 
+pub const ERROR_DESCRIPTIONS: &[u8] =
+    std::include_bytes!("../compiled/latest/error_descriptions/error_descriptions.errmap");
+
 /// Given the module ID and the abort code raised from that module, returns the human-readable
 /// explanation of that abort if possible.
 pub fn get_explanation(module_name: &str, abort_code: u64) -> Option<ErrorDescription> {
     let error_descriptions: ErrorMapping =
-        bcs_ext::from_bytes(stdlib::ERROR_DESCRIPTIONS).expect("Decode err map failed");
+        bcs_ext::from_bytes(ERROR_DESCRIPTIONS).expect("Decode err map failed");
     error_descriptions.get_explanation(module_name, abort_code)
 }
