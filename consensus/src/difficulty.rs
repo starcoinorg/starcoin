09e68bad3e5dd89a19f1c16bb587afab13ac3ec4
sanlee42 <hit.cs.lijun@gmail.com>
2025-03-25 16:46:43 +0800

    chore(consensus): set block target to 200ms (BPS=5) for high-frequency mining

--- consensus/src/difficulty.rs
+++ consensus/src/difficulty.rs
@@ -44,7 +44,7 @@ pub fn get_next_work_required(chain: &dyn ChainReader) -> Result<U256> {
             "block difficulty count should eq block_difficulty_window"
         );
     }
-    let target = get_next_target_helper(blocks, 5_00)?;
+    let target = get_next_target_helper(blocks, 2_00)?;
     debug!(
         "get_next_work_required current_number: {}, epoch: {:?}, target: {}",
         current_header.number(),
