b8bd2df28b87455e4d5856043796d7ccca120908
simonjiao <simonjiaoh@gmail.com>
2025-03-25 10:22:35 +0800

    add statedb to workspace
    * update rust-toolchain to 1.82.0
    * remove chain-state-service from statedb

--- state/api/src/mock/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The Starcoin Core Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-mod mock_chain_state_service;
-
-pub use mock_chain_state_service::*;
-pub use starcoin_state_tree::mock::*;
