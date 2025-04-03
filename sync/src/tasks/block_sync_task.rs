4655c428ae428678df1c3ff158bfd183a74ea1cd
jackzhhuang <jackzhhuang@gmail.com>
2025-04-03 20:35:20 +0800

    fetch block in batch

--- sync/src/tasks/block_sync_task.rs
+++ sync/src/tasks/block_sync_task.rs
@@ -428,7 +428,7 @@ where
             if absent_blocks.is_empty() {
                 return Ok(());
             }
-            block_headers = self.fetch_blocks(absent_blocks).await?;
+            block_headers = self.fetch_blocks_in_batch(absent_blocks).await?;
         }
     }
 
@@ -543,7 +543,6 @@ where
         Ok(result)
     }
 
-    #[allow(dead_code)]
     async fn fetch_blocks_in_batch(
         &self,
         mut block_ids: Vec<HashValue>,
