5edf3727da27584709b31a0ec0ebb5784673179e
simonjiao <simonjiaoh@gmail.com>
2025-04-10 21:45:26 +0800

    fix unclean git status errors

--- vm/stdlib/tests/generated_files.rs
+++ vm/stdlib/tests/generated_files.rs
@@ -15,8 +15,8 @@ fn assert_that_version_control_has_no_unstaged_changes() {
         .output()
         .unwrap();
     let output_string = String::from_utf8(output.stdout).unwrap();
-    // remove .cargo/config from output
-    let output_string = output_string.replace("M .cargo/config", "");
+    // remove .cargo/config.toml from output
+    let output_string = output_string.replace("M .cargo/config.toml", "");
     let output_string = output_string.trim().to_string();
     if !output_string.is_empty() {
         println!("git status output:\n {}", output_string)
