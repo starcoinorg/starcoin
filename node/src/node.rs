3e1cfeca830e828a9412b1b60bb16c4cd97829ac
simonjiao <simonjiaoh@gmail.com>
2025-04-14 12:17:49 +0800

    incress wait time for test

--- node/src/node.rs
+++ node/src/node.rs
@@ -360,9 +360,11 @@ impl NodeService {
         let txpool_service = registry.register::<TxPoolActorService>().await?;
 
         //wait TxPoolService put shared..
+        #[cfg(not(test))]
         Delay::new(Duration::from_millis(200)).await;
+        #[cfg(test)]
+        Delay::new(Duration::from_secs(1)).await;
         // TxPoolActorService auto put shared TxPoolService,
-
         registry.register::<ChainReaderService>().await?;
 
         registry.register::<ChainNotifyHandlerService>().await?;
