36210a648fc9460e814872eaea46b4fa2f2988b3
simonjiao <simonjiaoh@gmail.com>
2025-04-14 17:44:39 +0800

    add storage2 to txpoolservice

--- test-helper/src/txpool.rs
+++ test-helper/src/txpool.rs
@@ -69,7 +69,7 @@ pub async fn start_txpool_with_miner(
     }
     //registry.register::<MinerService>().await.unwrap();
     let pool_actor = registry.register::<TxPoolActorService>().await.unwrap();
-    Delay::new(Duration::from_millis(200)).await;
+    Delay::new(Duration::from_secs(1)).await;
     let txpool_service = registry.get_shared::<TxPoolService>().await.unwrap();
 
     (
