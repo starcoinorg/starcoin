40254a63d5c7a09fd27f6bff3208b7475f47bdb0
jackzhhuang <jackzhhuang@gmail.com>
2025-04-17 11:13:49 +0800

    fix info for bps

--- chain/src/chain.rs
+++ chain/src/chain.rs
@@ -50,6 +50,7 @@ use std::collections::HashSet;
 use std::iter::Extend;
 use std::option::Option::{None, Some};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::{collections::HashMap, sync::Arc};
 
 static OUTPUT_BLOCK: AtomicBool = AtomicBool::new(false);
@@ -1674,15 +1675,14 @@ impl BlockChain {
                 U256::MAX
             };
 
-            let eclapse_time = u64::try_from(
-                std::time::Duration::from_millis(self.epoch.start_time()).as_millis(),
-            )?;
-            let bps = if let Some(bps) = total_blocks.checked_div(eclapse_time) {
-                bps
-            } else {
-                info!("BPS overflow");
-                0
+            let eclapse_time = {
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis();
+                now.saturating_sub(self.epoch.start_time() as u128) as f64 / 1000.0
             };
+            let bps = (total_blocks as f64) / eclapse_time;
 
             info!("the epoch data will be updated, this epoch data: total blue blocks: {:?}, total difficulty: {:?}, avg total difficulty: {:?}, BPS: {:?}, eclapse time: {:?}, start block id: {:?}, end block id: {:?}", 
                 total_blocks, total_difficulty, avg_total_difficulty, bps, eclapse_time, start_block_id, end_block_id);
