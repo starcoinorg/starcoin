b6e42520ba56aaf90dea2ad08218fddd55d4e521
simonjiao <simonjiaoh@gmail.com>
2025-04-20 11:24:31 +0800

    remove deuplicate functions

--- vm2/executor/src/lib.rs
+++ vm2/executor/src/lib.rs
@@ -5,7 +5,7 @@ pub mod executor;
 
 pub mod block_executor;
 
-pub use block_executor::{block_execute, execute_genesis_transaction};
+pub use block_executor::block_execute;
 pub use executor::{
     do_execute_block_transactions, execute_readonly_function, validate_transaction,
 };
