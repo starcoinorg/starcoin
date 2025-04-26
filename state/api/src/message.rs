aa296dfcfdc5c0fa2ceb6e16ebbfeccb21797a78
simonjiao <simonjiaoh@gmail.com>
2025-04-26 09:17:58 +0800

    add back chain-state-service

---
+++ state/api/src/message.rs
@@ -0,0 +1,45 @@
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{StateWithProof, StateWithTableItemProof};
+use anyhow::Result;
+use bytes::Bytes;
+use starcoin_crypto::HashValue;
+use starcoin_service_registry::ServiceRequest;
+use starcoin_types::state_set::AccountStateSet;
+use starcoin_types::{account_address::AccountAddress, account_state::AccountState};
+use starcoin_vm_types::state_store::state_key::StateKey;
+use starcoin_vm_types::state_store::table::{TableHandle, TableInfo};
+
+#[derive(Debug, Clone)]
+pub enum StateRequest {
+    Get(StateKey),
+    GetWithProof(StateKey),
+    GetWithProofByRoot(StateKey, HashValue),
+    GetAccountState(AccountAddress),
+    GetAccountStateSet {
+        address: AccountAddress,
+        state_root: Option<HashValue>,
+    },
+    GetAccountStateByRoot(AccountAddress, HashValue),
+    StateRoot(),
+    GetWithTableItemProof(TableHandle, Vec<u8>),
+    GetWithTableItemProofByRoot(TableHandle, Vec<u8>, HashValue),
+    GetTableInfo(AccountAddress),
+}
+
+impl ServiceRequest for StateRequest {
+    type Response = Result<StateResponse>;
+}
+
+#[derive(Debug, Clone)]
+pub enum StateResponse {
+    State(Option<Bytes>),
+    StateWithProof(Box<StateWithProof>),
+    StateRoot(HashValue),
+    AccountState(Option<AccountState>),
+    AccountStateSet(Option<AccountStateSet>),
+    None,
+    StateWithTableItemProof(Box<StateWithTableItemProof>),
+    TableInfo(Option<TableInfo>),
+}
