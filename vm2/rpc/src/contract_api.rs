8ba03ab7c61e98985b6985c84befd7ad8e5ab3da
nk_ysg <nk_ysg@163.com>
2025-05-02 13:07:21 +0800

    WIP add contract_api2

---
+++ vm2/rpc/src/contract_api.rs
@@ -0,0 +1,63 @@
+
+use crate::types::{
+    AnnotatedMoveStructView, AnnotatedMoveValueView, ContractCall, DryRunOutputView,
+    DryRunTransactionRequest, FunctionIdView, ModuleIdView, StrView, StructTagView,
+};
+use crate::FutureResult;
+use openrpc_derive::openrpc;
+use starcoin_vm2_abi_decoder::DecodedMoveValue;
+use starcoin_abi_types::{FunctionABI, ModuleABI, StructInstantiation};
+use starcoin_vm2_vm_types::account_address::AccountAddress;
+use starcoin_vm2_vm_types::language_storage::{ModuleId, StructTag};
+use starcoin_vm2_vm_types::transaction::authenticator::AccountPublicKey;
+#[openrpc]
+pub trait ContractApi {
+    /// get code of module
+    #[rpc(name = "contract2.get_code")]
+    fn get_code(&self, module_id: StrView<ModuleId>) -> FutureResult<Option<StrView<Vec<u8>>>>;
+
+    /// get resource data of `addr`
+    #[rpc(name = "contract2.get_resource")]
+    fn get_resource(
+        &self,
+        addr: AccountAddress,
+        resource_type: StrView<StructTag>,
+    ) -> FutureResult<Option<AnnotatedMoveStructView>>;
+
+    /// Call a move contract, return returned move values.
+    #[rpc(name = "contract2.call")]
+    fn call(&self, call: ContractCall) -> FutureResult<Vec<AnnotatedMoveValueView>>;
+
+    /// Call a move contract, return move values.
+    #[rpc(name = "contract2.call_v2")]
+    fn call_v2(&self, call: ContractCall) -> FutureResult<Vec<DecodedMoveValue>>;
+
+    #[rpc(name = "contract2.dry_run")]
+    fn dry_run(&self, txn: DryRunTransactionRequest) -> FutureResult<DryRunOutputView>;
+
+    /// Dry run RawUserTransaction, the raw_txn parameter is RawUserTransaction's hex
+    #[rpc(name = "contract2.dry_run_raw")]
+    fn dry_run_raw(
+        &self,
+        raw_txn: String,
+        sender_public_key: StrView<AccountPublicKey>,
+    ) -> FutureResult<DryRunOutputView>;
+    #[rpc(name = "contract2.resolve_function")]
+    fn resolve_function(&self, function_id: FunctionIdView) -> FutureResult<FunctionABI>;
+    #[rpc(name = "contract2.resolve_module_function_index")]
+    fn resolve_module_function_index(
+        &self,
+        module_id: ModuleIdView,
+        function_index: u16,
+    ) -> FutureResult<FunctionABI>;
+    #[rpc(name = "contract2.resolve_struct")]
+    fn resolve_struct(&self, struct_tag: StructTagView) -> FutureResult<StructInstantiation>;
+    #[rpc(name = "contract2.resolve_module")]
+    fn resolve_module(&self, module_id: ModuleIdView) -> FutureResult<ModuleABI>;
+}
+#[test]
+fn test() {
+    let schema = self::gen_schema();
+    let j = serde_json::to_string_pretty(&schema).unwrap();
+    println!("{}", j);
+}
