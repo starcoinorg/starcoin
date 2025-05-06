f791b7686972b65cca89615d33b6b9159a94263e
nk_ysg <nk_ysg@163.com>
2025-05-06 17:48:32 +0800

    update msg

--- vm2/rpc/src/contract_api.rs
+++ vm2/rpc/src/contract_api.rs
@@ -11,7 +11,7 @@ use starcoin_vm2_types::view::{
 use starcoin_vm2_vm_types::account_address::AccountAddress;
 use starcoin_vm2_vm_types::language_storage::{ModuleId, StructTag};
 use starcoin_vm2_vm_types::transaction::authenticator::AccountPublicKey;
-// copy from https://github.com/starcoinorg/starcoin/blob/bf5ec6e44a242e9dff5ac177c1565c64c6e4b0d0/rpc/api/src/contract_api.rs#L14
+// copy from https://github.com/starcoinorg/starcoin/blob/bf5ec6e44a242e9dff5ac177c1565c64c6e4b0d0/rpc/api/src/contract_api.rs#L14 etc
 #[openrpc]
 pub trait ContractApi {
     /// get code of module
