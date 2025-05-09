fe695c7794f00775ea74813e87cd8933665fc647
simonjiao <simonjiaoh@gmail.com>
2025-05-09 10:30:54 +0800

    update vm2-rpc lib

--- vm2/rpc/src/lib.rs
+++ vm2/rpc/src/lib.rs
@@ -1,34 +1,7 @@
-use crate::account_api::AccountApiClient;
-/// this is in cargo expand contract_api.rs
-use crate::contract_api::ContractApiClient;
-use crate::state_api::StateApiClient;
-use jsonrpc_core_client::RpcChannel;
+// Copyright (c) The Starcoin Core Contributors
+// SPDX-License-Identifier: Apache-2
 
 pub use starcoin_vm2_abi_decoder::DecodedMoveValue;
-
-#[allow(dead_code)]
-#[derive(Clone)]
-pub struct RpcClientInner {
-    account_client: AccountApiClient,
-    contract_client: ContractApiClient,
-    state_client: StateApiClient,
-}
-
-impl RpcClientInner {
-    pub fn new(channel: RpcChannel) -> Self {
-        Self {
-            account_client: channel.clone().into(),
-            contract_client: channel.clone().into(),
-            state_client: channel.clone().into(),
-        }
-    }
-}
-
-impl From<RpcChannel> for RpcClientInner {
-    fn from(channel: RpcChannel) -> Self {
-        Self::new(channel)
-    }
-}
 
 pub mod account_api;
 pub mod contract_api;
