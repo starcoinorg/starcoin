c29e78a8450ae9e16115a7c765d645a7087ede2b
simonjiao <simonjiaoh@gmail.com>
2025-05-13 10:34:30 +0800

    register vm2 apis to rpc service (#4505)
    * register vm2 apis to rpc service
    
    * fix typos

--- node/src/rpc_service_factory.rs
+++ node/src/rpc_service_factory.rs
@@ -6,7 +6,7 @@ use anyhow::Result;
 use starcoin_account_service::AccountService;
 use starcoin_chain_service::ChainReaderService;
 use starcoin_config::NodeConfig;
-use starcoin_dev::playground::PlaygroudService;
+use starcoin_dev::playground::PlaygroundService;
 use starcoin_executor::VMMetrics;
 use starcoin_genesis::Genesis;
 use starcoin_logger::LoggerHandle;
@@ -24,6 +24,11 @@ use starcoin_storage::Storage;
 use starcoin_sync::sync::SyncService;
 use starcoin_txpool::TxPoolService;
 use starcoin_vm2_account_service::AccountService as AccountService2;
+use starcoin_vm2_dev::playground::PlaygroudService as PlaygroundService2;
+use starcoin_vm2_rpc_server::{
+    account_rpc::AccountRpcImpl as AccountRpcImpl2,
+    contract_rpc::ContractRpcImpl as ContractRpcImpl2, state_rpc::StateRpcImpl as StateRpcImpl2,
+};
 use starcoin_vm2_state_service::ChainStateService as ChainStateService2;
 use starcoin_vm2_storage::Storage as Storage2;
 use std::sync::Arc;
@@ -64,16 +69,27 @@ impl ServiceFactory<RpcService> for RpcServiceFactory {
         let state_api = ctx
             .service_ref_opt::<ChainStateService>()?
             .map(|service_ref| StateRpcImpl::new(service_ref.clone(), storage.clone()));
+        let state_api2 = ctx
+            .service_ref_opt::<ChainStateService2>()?
+            .map(|service_ref| StateRpcImpl2::new(service_ref.clone(), storage2.clone()));
         let chain_state_service = ctx.service_ref::<ChainStateService>()?.clone();
-        let _chain_state_service2 = ctx.service_ref_opt::<ChainStateService2>()?.cloned();
+        let chain_state_service2 = ctx.service_ref::<ChainStateService2>()?.clone();
         let account_service = ctx.service_ref_opt::<AccountService>()?.cloned();
-        let _account_service2 = ctx.service_ref_opt::<AccountService2>()?.cloned();
+        let account_service2 = ctx.service_ref_opt::<AccountService2>()?.cloned();
         let account_api = account_service.clone().map(|service_ref| {
             AccountRpcImpl::new(
                 config.clone(),
                 service_ref,
                 txpool_service.clone(),
                 chain_state_service.clone(),
+            )
+        });
+        let account_api2 = account_service2.clone().map(|service_ref| {
+            AccountRpcImpl2::new(
+                config.clone(),
+                service_ref,
+                txpool_service.clone(),
+                chain_state_service2.clone(),
             )
         });
         let pubsub_service = ctx.service_ref::<PubSubService>()?.clone();
@@ -89,14 +105,26 @@ impl ServiceFactory<RpcService> for RpcServiceFactory {
 
         let contract_api = {
             let vm_metrics = ctx.get_shared_opt::<VMMetrics>()?;
-            let dev_playground = PlaygroudService::new(storage.clone(), vm_metrics);
+            let dev_playground = PlaygroundService::new(storage.clone(), vm_metrics);
             ContractRpcImpl::new(
                 config.clone(),
                 account_service,
-                txpool_service,
+                txpool_service.clone(),
                 chain_state_service,
                 dev_playground,
                 storage,
+            )
+        };
+        let contract_api2 = {
+            let vm_metrics = ctx.get_shared_opt::<VMMetrics>()?;
+            let dev_playground = PlaygroundService2::new(storage2.clone(), vm_metrics);
+            ContractRpcImpl2::new(
+                config.clone(),
+                account_service2,
+                txpool_service,
+                chain_state_service2,
+                dev_playground,
+                storage2,
             )
         };
 
@@ -114,6 +142,9 @@ impl ServiceFactory<RpcService> for RpcServiceFactory {
             debug_api,
             miner_api,
             Some(contract_api),
+            account_api2,
+            state_api2,
+            Some(contract_api2),
         ))
     }
 }
