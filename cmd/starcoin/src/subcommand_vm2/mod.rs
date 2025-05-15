7b6326a201f3e0669cfae48e55d5a45a352c35ce
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-05-15 16:08:01 +0800

    [dual-vm] vm2 command: fixed todo

--- cmd/starcoin/src/subcommand_vm2/mod.rs
+++ cmd/starcoin/src/subcommand_vm2/mod.rs
@@ -9,6 +9,10 @@ pub fn add_command_vm2(
 ) -> CmdContext<CliState, StarcoinOpt> {
     context.command(
         CustomCommand::with_name("account")
-            .subcommand(account::accept_token_cmd::AcceptTokenCommand),
+            .subcommand(account::accept_token_cmd::AcceptTokenCommand)
+            .subcommand(account::show_cmd::ShowCommand)
+            .subcommand(account::execute_script_cmd::ExecuteScriptCommand)
+            .subcommand(account::execute_script_function_cmd::ExecuteScriptFunctionCmd)
+            .subcommand(account::unlock_cmd::UnlockCommand),
     )
 }
