2fb8aa0e4577d0b2d7f46d6f453485f734f80838
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-05-15 08:59:49 +0800

    [starcoin-vm2] add ResourceView missing decode function (#4512)
    * [dual-vm] add ResourceView missing decode function
    
    * [starcoin-vm2] Add documentation for impl ResourceView

--- types/src/view/resource_view.rs
+++ types/src/view/resource_view.rs
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::view::{function_arg_type_view::StructTagView, str_view::StrView};
+use move_core_types::move_resource::MoveResource;
 use schemars::JsonSchema;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use starcoin_abi_decoder::DecodedMoveValue;
 use std::collections::BTreeMap;
@@ -12,6 +14,23 @@ pub struct ResourceView {
     pub raw: StrView<Vec<u8>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub json: Option<DecodedMoveValue>,
+}
+
+impl ResourceView {
+    /// # Parameters
+    /// - Generic parameter `R`: The target deserialization type, which must satisfy:
+    ///   - Implements `MoveResource` (indicating it's a standard Move resource)
+    ///   - Implements `DeserializeOwned` (ensuring complete ownership of deserialized data)
+    ///
+    /// # Returns
+    /// - `Ok(R)`: Successfully deserialized instance of type R
+    /// - `Err(anyhow::Error)`: Contains error details if deserialization fails, which could occur because:
+    ///   - Byte data doesn't match BCS structure of type R
+    ///   - Byte data is incomplete or malformed
+    ///
+    pub fn decode<R: MoveResource + DeserializeOwned>(&self) -> anyhow::Result<R> {
+        bcs_ext::from_bytes(self.raw.0.as_slice())
+    }
 }
 
 impl From<Vec<u8>> for ResourceView {
