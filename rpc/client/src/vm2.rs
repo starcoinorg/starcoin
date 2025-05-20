c369cb86bc82a649fafbcd862190e8398b8ce5e9
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-05-20 21:53:12 +0800

    [dual-vm] remove useless code

--- rpc/client/src/vm2.rs
+++ rpc/client/src/vm2.rs
@@ -3,7 +3,6 @@
 
 use crate::{map_err, remote_state_reader2::RemoteStateReader, RpcClient, StateRootOption};
 use bcs_ext::BCSCodec;
-use starcoin_types::account_address::AccountAddress as AccountAddressV1;
 use starcoin_vm2_abi_types::{FunctionABI, ModuleABI, StructInstantiation};
 use starcoin_vm2_account_api::AccountInfo;
 use starcoin_vm2_crypto::HashValue;
