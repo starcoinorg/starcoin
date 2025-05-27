3e409ec2a3bbeb18c69ec0baefc5e80a7d0aa2e9
simonjiao <simonjiaoh@gmail.com>
2025-05-27 14:29:05 +0800

    save vm1/vm2 txn events to same columnfamily

--- chain/src/chain.rs
+++ chain/src/chain.rs
@@ -635,12 +635,9 @@ impl BlockChain {
         // save transaction relationship and save transaction to storage2
         starcoin_vm2_chain::save_executed_transactions(
             block_id,
-            header.number(),
             storage2,
             transactions2,
-            executed_data2,
-            // todo: how to track vm2 transaction global index?
-            transaction_global_index + executed_data.txn_infos.len() as u64,
+            executed_data2.clone(),
         )?;
 
         watch(CHAIN_WATCH_NAME, "n25");
@@ -660,8 +657,29 @@ impl BlockChain {
             "events' length should be equal to txn infos' length"
         );
         let txn_info_ids: Vec<_> = txn_infos.iter().map(|info| info.id()).collect();
-        for (info_id, events) in txn_info_ids.iter().zip(txn_events.into_iter()) {
-            storage.save_contract_events(*info_id, events)?;
+        for (info_id, events) in txn_info_ids.iter().zip(
+            txn_events
+                .into_iter()
+                .map(|events| events.into_iter().map(Into::into).collect::<Vec<_>>()),
+        ) {
+            storage.save_contract_events_v2(*info_id, events)?;
+        }
+
+        // save vm2 txn events
+        let vm2_txn_info_ids: Vec<_> = vm2_txn_infos.iter().map(|info| info.id()).collect();
+        {
+            debug_assert!(
+                executed_data2.txn_events.len() == vm2_txn_infos.len(),
+                "vm2 events' length should be equal to txn infos' length"
+            );
+            for (info_id, events) in vm2_txn_info_ids.iter().zip(
+                executed_data2
+                    .txn_events
+                    .into_iter()
+                    .map(|events| events.into_iter().map(Into::into).collect::<Vec<_>>()),
+            ) {
+                storage.save_contract_events_v2(*info_id, events)?;
+            }
         }
 
         storage.save_transaction_infos(
@@ -692,7 +710,10 @@ impl BlockChain {
 
         // save block's transactions
         storage.save_block_transaction_ids(block_id, txn_id_vec)?;
-        storage.save_block_txn_info_ids(block_id, txn_info_ids)?;
+        storage.save_block_txn_info_ids(
+            block_id,
+            txn_info_ids.into_iter().chain(vm2_txn_info_ids).collect(),
+        )?;
         storage.commit_block(block.clone())?;
 
         storage.save_block_info(block_info.clone())?;
--- types/src/lib.rs
+++ types/src/lib.rs
@@ -103,7 +103,7 @@ pub mod language_storage {
         ModuleId, ResourceKey, StructTag, TypeTag, CODE_TAG, CORE_CODE_ADDRESS, RESOURCE_TAG,
     };
 
-    use starcoin_vm2_vm_types::language_storage::TypeTag as TypeTag2;
+    pub use starcoin_vm2_vm_types::language_storage::TypeTag as TypeTag2;
 
     #[derive(Serialize, Deserialize, Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
     pub enum StcTypeTag {
