f06a96bc21bfa9df25ed67913a92143cec9995c2
jackzhhuang <jackzhhuang@gmail.com>
2025-06-04 17:08:27 +0800

    force to mint if the dag block comes from peers

--- miner/src/generate_block_event_pacemaker.rs
+++ miner/src/generate_block_event_pacemaker.rs
@@ -128,6 +128,6 @@ impl EventHandler<Self, SyncStatusChangeEvent> for GenerateBlockEventPacemaker {
 
 impl EventHandler<Self, NewDagBlockFromPeer> for GenerateBlockEventPacemaker {
     fn handle_event(&mut self, _msg: NewDagBlockFromPeer, ctx: &mut ServiceContext<Self>) {
-        self.send_event(false, ctx);
+        self.send_event(true, ctx);
     }
 }
