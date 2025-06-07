9b26fe87c7a32a7a371579ebcadf6cb94c9e5bba
jackzhhuang <jackzhhuang@gmail.com>
2025-06-07 23:01:54 +0800

    sort the tips by the work type and blue score and fetch the previous 10 tip stored in the dag state

--- flexidag/src/blockdag.rs
+++ flexidag/src/blockdag.rs
@@ -689,7 +689,6 @@ impl BlockDAG {
                     .collect::<HashSet<_>>()
                     .into_iter()
                     .collect::<Vec<_>>();
-
                 let mut tips_in_order = merged_tips
                     .into_iter()
                     .map(|hash: Hash| {
