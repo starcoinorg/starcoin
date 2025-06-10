7c175f3a09bfa89d5316bc0e38f47a6cd7a68633
jackzhhuang <jackzhhuang@gmail.com>
2025-06-10 17:48:32 +0800

    no mining until synced

--- miner/src/generate_block_event_pacemaker.rs
+++ miner/src/generate_block_event_pacemaker.rs
@@ -75,7 +75,7 @@ impl ActorService for GenerateBlockEventPacemaker {
 impl EventHandler<Self, NewDagBlock> for GenerateBlockEventPacemaker {
     fn handle_event(&mut self, _msg: NewDagBlock, ctx: &mut ServiceContext<Self>) {
         if self.is_synced() {
-            self.send_event(false, ctx)
+            self.send_event(true, ctx)
         } else {
             debug!("[pacemaker] Ignore NewDagBlock event because the node has not been synchronized yet.")
         }
