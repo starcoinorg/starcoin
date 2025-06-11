98aec3828a26c912b31268d73903e04c139bc552
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-06-11 16:14:54 +0800

    [multi move] fixed ci deploy (#4560)
    * [CI] fixed deploy CI
    
    * [CI] fixed deploy CI
    
    * [CI] fixed docker file formater

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,7 @@ rm -rf starcoin-artifacts/*
 mkdir -p starcoin-artifacts/
 cp -v target/release/starcoin starcoin-artifacts/
 cp -v target/release/starcoin_miner starcoin-artifacts/
-cp -v target/release/starcoin_generator starcoin-artifacts/
 cp -v target/release/mpm starcoin-artifacts/
-cp -v target/release/starcoin_db_exporter starcoin-artifacts/
 cp -v scripts/import_block.sh starcoin-artifacts/
 cp -v scripts/import_snapshot.sh starcoin-artifacts/
 cp -v scripts/verify_header.sh starcoin-artifacts/
