ad9cbc22de5bd42dee20cd0ba025127ba748dfe0
sanlee42 <hit.cs.lijun@gmail.com>
2025-07-04 13:37:07 +0800

    network-p2p: Init peerset with notif_protocols

--- network-p2p/src/service.rs
+++ network-p2p/src/service.rs
@@ -1136,7 +1136,7 @@ impl<T: BusinessLayerHandle + Send> Future for NetworkWorker<T> {
                     this.network_service
                         .behaviour_mut()
                         .user_protocol_mut()
-                        .add_default_set_discovered_nodes(iter::once(peer_id));
+                        .add_set_discovered_nodes(iter::once(peer_id));
                 }
 
                 Poll::Ready(SwarmEvent::Behaviour(BehaviourOut::ReputationChanges {
@@ -1525,7 +1525,7 @@ impl<T: BusinessLayerHandle + Send> Future for NetworkWorker<T> {
                     this.network_service
                         .behaviour_mut()
                         .user_protocol_mut()
-                        .add_default_set_discovered_nodes(iter::once(peer_id));
+                        .add_set_discovered_nodes(iter::once(peer_id));
                 }
             };
         }
