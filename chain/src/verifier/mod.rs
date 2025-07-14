86e592ee5a082dff4d3efaf5ea3a67bb5c623a6a
jackzhhuang <jackzhhuang@gmail.com>
2025-07-14 15:21:12 +0800

    remove the black list verification

--- chain/src/verifier/mod.rs
+++ chain/src/verifier/mod.rs
@@ -10,7 +10,6 @@ use starcoin_consensus::{Consensus, ConsensusVerifyError};
 use starcoin_crypto::HashValue;
 use starcoin_dag::types::ghostdata::GhostdagData;
 use starcoin_logger::prelude::warn;
-use starcoin_open_block::AddressFilter;
 use starcoin_types::{
     block::{Block, BlockHeader, ALLOWED_FUTURE_BLOCKTIME},
     consensus_header::ConsensusHeader,
@@ -75,7 +74,6 @@ pub trait BlockVerifier {
         watch(CHAIN_WATCH_NAME, "n11");
         //verify header
         let new_block_header = new_block.header();
-        // Self::verify_blacklisted_txns(&new_block)?;
         Self::verify_header(current_chain, new_block_header)?;
         watch(CHAIN_WATCH_NAME, "n12");
         StaticVerifier::verify_body_hash(&new_block)?;
@@ -92,18 +90,6 @@ pub trait BlockVerifier {
             ghostdata,
         })
     }
-
-    // fn verify_blacklisted_txns(new_block: &Block) -> Result<()> {
-    //     let block_number = new_block.header().number();
-    //     for txn in new_block.transactions() {
-    //         verify_block!(
-    //             VerifyBlockField::Body,
-    //             !AddressFilter::is_blacklisted(txn, block_number),
-    //             "Invalid block: the sender of transaction in block must be not blacklisted"
-    //         );
-    //     }
-    //     Ok(())
-    // }
 
     fn verify_blue_blocks<R>(
         current_chain: &R,
