8f37a30d309e08bf119dcfe00a7a022165ca3586
jackzhhuang <jackzhhuang@gmail.com>
2025-07-14 20:57:57 +0800

    Set the block time target from 500ms to 2000ms

--- miner/src/create_block_template/new_header_service.rs
+++ miner/src/create_block_template/new_header_service.rs
@@ -144,10 +144,6 @@ impl NewHeaderService {
             })?
             .as_ref()
             .clone();
-        info!(
-            "resolve_header: new ghostdata: {:?}, current ghostdata: {:?}",
-            new_ghostdata, self.ghostdag_data
-        );
         let update = match new_ghostdata.blue_work.cmp(&self.ghostdag_data.blue_work) {
             std::cmp::Ordering::Less => false,
             std::cmp::Ordering::Equal => {
