3f145b73d06415c5f381647004b9a459df029568
jackzhhuang <jackzhhuang@gmail.com>
2025-08-01 23:48:43 +0800

    add log to find the slow code

--- miner/src/generate_block_event_pacemaker.rs
+++ miner/src/generate_block_event_pacemaker.rs
@@ -121,6 +121,7 @@ impl EventHandler<Self, SyncStatusChangeEvent> for GenerateBlockEventPacemaker {
 
 impl EventHandler<Self, DeterminedDagBlock> for GenerateBlockEventPacemaker {
     fn handle_event(&mut self, _msg: DeterminedDagBlock, ctx: &mut ServiceContext<Self>) {
+        info!("jacktest: handle DeterminedDagBlock event");
         if self.is_synced() {
             self.send_event(true, ctx)
         } else {
