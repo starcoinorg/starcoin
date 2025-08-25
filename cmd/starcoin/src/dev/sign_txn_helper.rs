0c0335c8a296002a8b46f302a4a880611bdd5f18
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-08-25 19:13:47 +0800

    [command] fixed `dev vm-config-proposal`
    # Conflicts:
    #	cmd/starcoin/src/lib.rs

--- cmd/starcoin/src/dev/sign_txn_helper.rs
+++ cmd/starcoin/src/dev/sign_txn_helper.rs
@@ -4,13 +4,12 @@
 use crate::cli_state::CliState;
 use anyhow::{format_err, Result};
 use starcoin_rpc_client::StateRootOption;
-use starcoin_state_api::StateReaderExt;
-use starcoin_vm_types::on_chain_config::DaoConfig;
+use starcoin_vm2_vm_types::{on_chain_config::DaoConfig, state_view::StateReaderExt};
 
 pub fn get_dao_config(cli_state: &CliState) -> Result<DaoConfig> {
     let client = cli_state.client();
-    let chain_state_reader = client.state_reader(StateRootOption::Latest)?;
+    let chain_state_reader = client.state_reader2(StateRootOption::Latest)?;
     chain_state_reader
-        .get_on_chain_config::<DaoConfig>()?
+        .get_on_chain_config::<DaoConfig>()
         .ok_or_else(|| format_err!("DaoConfig not exist on chain."))
 }
