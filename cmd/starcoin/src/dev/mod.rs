a315bc6c4bab58a452e90413f6c1b06a84eeaa33
Bob Ong <2261238+welbon@users.noreply.github.com>
2025-08-27 20:00:53 +0800

    [command] remove unused command

--- cmd/starcoin/src/dev/mod.rs
+++ cmd/starcoin/src/dev/mod.rs
@@ -3,7 +3,6 @@
 
 pub(crate) mod call_api_cmd;
 mod call_contract_cmd;
-mod compile_cmd;
 mod concurrency_level_cmd;
 mod deploy_cmd;
 pub mod dev_helper;
@@ -26,7 +25,7 @@ pub mod upgrade_module_queue_cmd;
 pub mod upgrade_vm_config_proposal_cmd;
 
 pub use {
-    call_api_cmd::*, call_contract_cmd::*, compile_cmd::*, concurrency_level_cmd::*, deploy_cmd::*,
+    call_api_cmd::*, call_contract_cmd::*, concurrency_level_cmd::*, deploy_cmd::*,
     gen_block_cmd::*, get_coin_cmd::*, log_cmd::*, logger_balance_amount_cmd::*, move_explain::*,
     package_cmd::*, panic_cmd::*, resolve_cmd::*, sign_txn_helper::*, sleep_cmd::*,
     subscribe_cmd::*, upgrade_module_exe_cmd::*, upgrade_module_plan_cmd::*,
