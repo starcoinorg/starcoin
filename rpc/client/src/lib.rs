b544fdad9cc7fc2d5f16afc7ecf60e8a8e54cc98
jackzhhuang <jackzhhuang@gmail.com>
2025-10-09 17:44:52 +0800

    fix fmt

--- rpc/client/src/lib.rs
+++ rpc/client/src/lib.rs
@@ -858,14 +858,6 @@ impl RpcClient {
             .map_err(map_err)
     }
 
-    pub fn chain_get_block_info_by_hash(
-        &self,
-        id: HashValue,
-    ) -> anyhow::Result<Option<BlockInfoView>> {
-        self.call_rpc_blocking(|inner| inner.chain_client.get_block_info_by_hash(id))
-            .map_err(map_err)
-    }
-
     pub fn chain_get_blocks_by_number(
         &self,
         number: Option<BlockNumber>,
--- rpc/server/src/module/chain_rpc.rs
+++ rpc/server/src/module/chain_rpc.rs
@@ -218,18 +218,6 @@ where
         Box::pin(fut.boxed())
     }
 
-    fn get_block_info_by_hash(&self, id: HashValue) -> FutureResult<Option<BlockInfoView>> {
-        let service = self.service.clone();
-
-        let fut = async move {
-            let result = service.get_block_info_by_hash(&id).await?.map(Into::into);
-            Ok(result)
-        }
-        .map_err(map_err);
-
-        Box::pin(fut.boxed())
-    }
-
     fn get_block_info_by_number2(&self, number: u64) -> FutureResult<Option<BlockInfoView2>> {
         let service = self.service.clone();
 
