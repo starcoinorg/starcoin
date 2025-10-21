8525d4cb6ae7ad350134a83950cd96a858d0bb0a
BobOng <2261238+welbon@users.noreply.github.com>
2025-10-21 17:33:04 +0800

    [fa] mpm integration-test restore the replace process of precompiled files, but filter 0x1 module

--- vm2/starcoin-transactional-test-harness/src/lib.rs
+++ vm2/starcoin-transactional-test-harness/src/lib.rs
@@ -39,7 +39,7 @@ use move_transactional_test_runner::vm_test_harness::{PrecompiledFilesModules, T
 
 use move_command_line_common::values::ParsableValue;
 use move_compiler::compiled_unit::{AnnotatedCompiledUnit, CompiledUnitEnum};
-use move_core_types::resolver::ResourceResolver;
+use move_core_types::resolver::{ModuleResolver, ResourceResolver};
 use move_transactional_test_runner::tasks::{
     PrintBytecodeCommand, PublishCommand, RunCommand, ViewCommand,
 };
@@ -54,6 +54,7 @@ use std::{
     str::FromStr,
     sync::Mutex,
 };
+use log::{info, warn};
 use tempfile::{NamedTempFile, TempDir};
 
 use starcoin_config::BuiltinNetworkID;
@@ -95,6 +96,8 @@ use starcoin_vm2_vm_types::{
 use starcoin_gas_schedule::{FromOnChainGasSchedule, StarcoinGasParameters};
 use starcoin_vm2_resource_viewer::MoveValueAnnotator;
 use starcoin_vm2_statedb::ChainStateDB;
+use starcoin_vm2_vm_types::state_store::state_key::StateKey;
+use starcoin_vm2_vm_types::write_set::{WriteOp, WriteSetMut};
 use starcoin_vm_types::genesis_config::ChainId as ChainId1;
 
 pub mod context;
@@ -1131,53 +1134,74 @@ impl<'a> MoveTestAdapter<'a> for StarcoinTestAdapter<'a> {
             )
         };
 
-        // // add pre compiled modules
-        // if let Some(pre_compiled_lib) = pre_compiled_deps_v1 {
-        //     let mut writes = WriteSetMut::default();
-        //     for c in &pre_compiled_lib.0.compiled {
-        //         if let CompiledUnitEnum::Module(m) = c {
-        //             // update named_address_mapping
-        //             if let Some(named_address) = &m.address_name {
-        //                 let name = named_address.value.to_string();
-        //                 let already_assigned_with_different_value = named_address_mapping
-        //                     .get(&name)
-        //                     .filter(|existed| {
-        //                         existed.into_inner() != m.named_module.address.into_inner()
-        //                     })
-        //                     .is_some();
-        //                 if already_assigned_with_different_value {
-        //                     panic!(
-        //                         "Invalid init. The named address '{}' is already assigned with {}",
-        //                         name,
-        //                         named_address_mapping.get(&name).unwrap(),
-        //                     )
-        //                 }
-        //                 named_address_mapping.insert(name, m.named_module.address);
-        //             }
-        //
-        //             let state_key = StateKey::module_id(&m.named_module.module.self_id());
-        //             info!("MoveAdapter::init | write pre_compiled_lib, write: {:?}", state_key);
-        //             writes.insert((
-        //                 state_key,
-        //                 WriteOp::legacy_modification({
-        //                     let mut bytes = vec![];
-        //                     m.named_module.module.serialize(&mut bytes).unwrap();
-        //                     bytes.into()
-        //                 }),
-        //             ));
-        //         }
-        //     }
-        //     context.apply_write_set(writes.freeze().unwrap()).unwrap();
-        // }
+        
+        // add pre compiled modules
+        if let Some(pre_compiled_lib) = pre_compiled_deps_v1 {
+            let mut writes = WriteSetMut::default();
+            // Filter out modules that already exist in context.storage
+            let compiled_filtered: Vec<_> = pre_compiled_lib.0.compiled.iter()
+                .filter(|c| {
+                    if let CompiledUnitEnum::Module(m) = c {
+                        let module_id = &m.named_module.module.self_id();
+                        // Check if module already exists in storage
+                        match context.storage.as_move_resolver().get_module(module_id) {
+                            Ok(Some(_)) => {
+                                false
+                            },
+                            Ok(None) => {
+                                true
+                            },
+                            Err(e) => {
+                                warn!("MoveTestAdapter::init | Error checking module {:?} in storage: {:?}, will be added", module_id, e);
+                                true
+                            }
+                        }
+                    } else {
+                        true // Include non-module compiled units
+                    }
+                })
+                .collect();
 
-        // let metadata = context
-        //     .storage
-        //     .as_move_resolver()
-        //     .get_module_metadata(&ModuleId::new(
-        //         AccountAddress::ONE,
-        //         ident_str!("account").to_owned(),
-        //     ));
-        // assert!(metadata.len() > 0);
+            for c in &compiled_filtered {
+                info!("MoveTestAdapter::init | pre_compiled_deps_v1, compiled unit: {:?}", c);
+                if let CompiledUnitEnum::Module(m) = c {
+
+                    // update named_address_mapping
+                    if let Some(named_address) = &m.address_name {
+                        let name = named_address.value.to_string();
+                        let already_assigned_with_different_value = named_address_mapping
+                            .get(&name)
+                            .filter(|existed| {
+                                existed.into_inner() != m.named_module.address.into_inner()
+                            })
+                            .is_some();
+                        if already_assigned_with_different_value {
+                            panic!(
+                                "Invalid init. The named address '{}' is already assigned with {}",
+                                name,
+                                named_address_mapping.get(&name).unwrap(),
+                            )
+                        }
+                        named_address_mapping.insert(name, m.named_module.address);
+                    }
+
+                    let state_key = StateKey::module_id(&m.named_module.module.self_id());
+                    info!("MoveAdapter::init | write pre_compiled_lib, write: {:?}", state_key);
+                    writes.insert((
+                        state_key,
+                        WriteOp::legacy_modification({
+                            let mut bytes = vec![];
+                            m.named_module.module.serialize(&mut bytes).unwrap();
+                            bytes.into()
+                        }),
+                    ));
+                }
+            }
+
+            if !writes.is_empty() {
+                context.apply_write_set(writes.freeze().unwrap()).unwrap();
+            }
+        }
 
         let mut me = Self {
             compiled_state: CompiledState::new(
