c3b4bfa47178f019a419379be7a51ab27db0f990
simonjiao <simonjiaoh@gmail.com>
2025-11-17 22:10:43 +0800

    fix block-txn-infos' order

--- chain/src/chain.rs
+++ chain/src/chain.rs
@@ -844,7 +844,7 @@ impl BlockChain {
         storage.save_block_transaction_ids(block_id, txn_id_vec)?;
         storage.save_block_txn_info_ids(
             block_id,
-            txn_info_ids.into_iter().chain(vm2_txn_info_ids).collect(),
+            vm2_txn_info_ids.into_iter().chain(txn_info_ids).collect(),
         )?;
         storage.commit_block(block.clone())?;
 
@@ -1597,10 +1597,10 @@ impl ChainReader for BlockChain {
                 MultiAccessPath::VM2(_) => {
                     let statedb2 = statedb2.fork_at(final_state_root_hash);
                     let state_key = final_access_path.to_state_key()?.ok_or_else(|| {
-                                format_err!(
+                        format_err!(
                                     "the transaction to be verified is vm version 2 but the access path is not"
                                 )
-                            })?;
+                    })?;
                     Some(MultiStateProof::VM2(statedb2.get_with_proof(&state_key)?))
                 }
             }
@@ -2536,7 +2536,7 @@ impl BlockChain {
         storage.save_block_transaction_ids(block_id, txn_id_vec)?;
         storage.save_block_txn_info_ids(
             block_id,
-            txn_info_ids.into_iter().chain(vm2_txn_info_ids).collect(),
+            vm2_txn_info_ids.into_iter().chain(txn_info_ids).collect(),
         )?;
 
         // Save table infos
