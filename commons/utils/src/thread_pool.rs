d9a13a42e94fa7f80723b6ba4e7a9f27d8efbd0d
jackzhhuang <jackzhhuang@gmail.com>
2025-12-04 20:05:46 +0800

    use 1 thread to produce and execute the block

--- commons/utils/src/thread_pool.rs
+++ commons/utils/src/thread_pool.rs
@@ -2,7 +2,7 @@ use once_cell::sync::Lazy;
 
 pub static RAYON_EXEC_POOL: Lazy<rayon::ThreadPool> = Lazy::new(|| {
     rayon::ThreadPoolBuilder::new()
-        .num_threads(num_cpus::get())
+        .num_threads(1)
         .thread_name(|index| format!("parallel_executor_{}", index))
         .build()
         .expect("failed to build global rayon thread pool for parallel execution")
